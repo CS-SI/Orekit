82dda8e7194c98537c239af02939d5d48c765823
Luc Maisonobe <luc@orekit.org>
2006-09-07 01:55:29 +0000

    added tests

---
+++ tests-src/fr/cs/aerospace/orekit/frames/ITRF2000Test.java
@@ -0,0 +1,17 @@
+package fr.cs.aerospace.orekit.frames;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ITRF2000Test extends TestCase {
+
+  public void testToDo() {
+    fail("TODO: implement tests for ITRF2000");
+  }
+
+  public static Test suite() {
+    return new TestSuite(ITRF2000Test.class);
+  }
+  
+}
