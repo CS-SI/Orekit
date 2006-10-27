3851ad113399542347fe31959ccd8f90f6ee9921
Fabien Maussion <fabien@orekit.org>
2006-10-27 11:33:39 +0000

    cunningham

--- src/fr/cs/aerospace/orekit/perturbations/CunninghamAttractionModel.java
+++ src/fr/cs/aerospace/orekit/perturbations/CunninghamAttractionModel.java
@@ -1,7 +1,7 @@
 package fr.cs.aerospace.orekit.perturbations;
 
-import fr.cs.aerospace.orekit.bodies.RotatingBody;
 import fr.cs.aerospace.orekit.errors.OrekitException;
+import fr.cs.aerospace.orekit.frames.SynchronizedFrame;
 import fr.cs.aerospace.orekit.propagation.EquinoctialGaussEquations;
 import fr.cs.aerospace.orekit.time.AbsoluteDate;
 import fr.cs.aerospace.orekit.utils.PVCoordinates;
@@ -22,32 +22,58 @@ import org.spaceroots.mantissa.geometry.Vector3D;
  */
 
 public class CunninghamAttractionModel implements ForceModel {
-
+  
   /**
-   * Create a new instance of CentralBodyPotential.
+   * Creates a new instance of CentralBodyPotential.
    * 
    * @param mu central body attraction coefficient
-   * @param body rotating body
+   * @param centralBodyFrame rotating body frame
    * @param equatorialRadius reference equatorial radius of the potential
    * @param C normalized coefficients array (cosine part)
    * @param S normalized coefficients array (sine part)
    */
-  public CunninghamAttractionModel(double mu, RotatingBody body,
-                                   double equatorialRadius, double[][] C,
-                                   double[][] S) {
-
-    this.body = body;
+  public CunninghamAttractionModel(double mu, SynchronizedFrame centralBodyFrame,
+                                      double equatorialRadius, double[][] C,
+                                      double[][] S) {
+    
+    
+    this.bodyFrame = centralBodyFrame;
     this.equatorialRadius = equatorialRadius;
-    muOnAe = mu / equatorialRadius;
+    this.mu = mu;
     this.C = C;
     this.S = S;
+    ndeg = C.length - 1;
+    mord = C[ndeg].length - 1;
+    
+    Vi = new double[C.length][];
+    Vr = new double[C.length][];
+    
+    ViderX= new double[C.length][];
+    ViderY= new double[C.length][];
+    ViderZ= new double[C.length][];
+    
+    VrderX= new double[C.length][];
+    VrderY= new double[C.length][];
+    VrderZ= new double[C.length][]; 
+    
+    int j = 0;
+    
+    for(int i =0; i< C.length ; i++) { 
 
-    V = new double[C.length + 3 + 1][C.length + 3 + 1];
-    ndd = Math.min(Math.max(ndeg + 1, 4), C.length + 2);
-    nod = Math.min(Math.max(nord + 1, 4), C.length + 2);
-
+      Vi[i]= new double[i+1];
+      Vr[i]= new double[i+1];
+      
+      ViderX[i]= new double[i+1];
+      ViderY[i]= new double[i+1];
+      ViderZ[i]= new double[i+1];
+      
+      VrderX[i]= new double[i+1];
+      VrderY[i]= new double[i+1];
+      VrderZ[i]= new double[i+1]; 
+    }
+    
   }
-
+  
   /**
    * Compute the contribution of the central body potential to the perturbing
    * acceleration.
@@ -62,218 +88,186 @@ public class CunninghamAttractionModel implements ForceModel {
    */
   public void addContribution(AbsoluteDate date, PVCoordinates pvCoordinates,
                               EquinoctialGaussEquations adder)
-      throws OrekitException {
-
+  throws OrekitException {
+    
     // Construction of the potential array V(n,m)
-    buildArray(ndd, nod, date, pvCoordinates.getPosition(), V);
-
-    double aX = 0.0;
-    double aY = 0.0;
-    double aZ = 0.0;
-    for (int nn = 1; nn <= (ndeg + 1); nn++) {
-      int n = ndeg + 1 - nn;
-      int npu = n + 1;
-      int mpu = Math.min(npu, (nord + 1));
-      double sumX = 0.0;
-      double sumY = 0.0;
-      double sumZ = 0.0;
-      double[] cn = C[n];
-      double[] sn = S[n];
-      for (int m = 0; m < mpu; ++m) {
-
-        // compute the first derivative of the Vnm function
-        buildDerivatives(n, m, V);
-
-        double cnm = cn[m];
-        double snm = sn[m];
-        sumX += realDerX * cnm + imaginaryDerX * snm;
-        sumY += realDerY * cnm + imaginaryDerY * snm;
-        sumZ += realDerZ * cnm + imaginaryDerZ * snm;
-
-      }
-
-      // Acceleration calculation
-      aX += sumX * muOnAe;
-      aY += sumY * muOnAe;
-      aZ += sumZ * muOnAe;
-
-    }
-
-    // provide the perturbing acceleration to the derivatives adder
-    adder.addXYZAcceleration(aX, aY, aZ);
-
-  }
-
-  /**
-   * Potential builder.
-   * 
-   * @param ndeg maximum degree of potential to take into account
-   * @param nord maximum order of the potential to take into account
-   * @param date current date
-   * @param position current position (m)
-   * @param V matrix containing all the elementary potentials
-   */
-  private void buildArray(int ndeg, int nord, AbsoluteDate date,
-                          Vector3D position, double[][] V)
-      throws OrekitException {
-
-    // Retrieval of cartesian coordinates
-    Vector3D relative = body.getOrientation(date).applyTo(position);
+    Vector3D relative = bodyFrame.getTransformTo(adder.getFrame() , date)
+    .getInverse().transformPosition(pvCoordinates.getPosition());
+    
     double x = relative.getX();
     double y = relative.getY();
     double z = relative.getZ();
-
+    
     // Definition of intermediate variables
-    double r = Math.sqrt(x * x + y * y + z * z);
-    if (r == 0) {
+    double r2 =x * x + y * y + z * z;
+    
+    double r = Math.sqrt(r2);
+    if (r <= equatorialRadius) {
       throw new OrekitException("underground trajectory (r = {0})",
                                 new String[] {
-                                  Double.toString(r)
-                                });
+          Double.toString(r)
+      });
     }
-    double cosphicosl = x / r;
-    double cosphisinl = y / r;
-    double sinphi = z / r;
-    double aeOnr = equatorialRadius / r;
-    double aeOnr2 = aeOnr * aeOnr;
-    int noo = Math.min(C.length + 3, Math.max(2, nord + 2));
-
-    // Construction of the two first columns of matrix V
-    V[1][1] = 1.0 / r;
-    for (int i = 2; i <= noo; i++) {
-      V[i][1] = V[i - 1][1] * (2 * i - 3) * aeOnr;
-      V[i][2] = V[i][1] * sinphi;
+    double onr2 = 1/r2;
+    double xonr2 = x / r2;
+    double yonr2 = y / r2;
+    double zonr2 = z / r2;
+    
+    // diagonal and diagonal + 1 
+    Vr[0][0] = 1.0 / r;
+    Vi[0][0] = 0;
+    for (int n = 1; n<= mord; n++) {
+      Vr[n][n] = ( 2*n - 1 )  * ( xonr2*Vr[n-1][n-1] - yonr2*Vi[n-1][n-1] );
+      Vi[n][n] = ( 2*n - 1 )  * ( yonr2*Vr[n-1][n-1] + xonr2*Vi[n-1][n-1] ); 
+      Vr[n][n-1] = ( 2*n - 1 ) * zonr2 * Vr[n-1][n-1];
+      Vi[n][n-1] = ( 2*n - 1 ) * zonr2 * Vi[n-1][n-1];
+      
     }
 
-    if (ndeg > 1) {
-      int mm = Math.min((nord + 1), (ndeg - 1));
-      for (int j = 1; j <= mm; j++) {
-        for (int i = (j + 2); i <= (ndeg + 1); i++) {
-          V[i][i - j + 1] = (V[i - 1][i - j] * sinphi * aeOnr * (2 * i - 3) - V[i - 2][i
-              - j - 1]
-              * aeOnr2 * (i + j - 3))
-              / (i - j);
-        }
+    // columns
+    
+    for (int m = 0; m<= mord; m++) {
+      for (int n = m+2; n<= ndeg; n++) {
+        Vr[n][m] = ((2*n - 1)*zonr2*Vr[n-1][m] - (n+m-1)* onr2 *Vr[n-2][m])/(n-m);
+        Vi[n][m] = ((2*n - 1)*zonr2*Vi[n-1][m] - (n+m-1)* onr2 *Vi[n-2][m])/(n-m);
       }
     }
 
-    if (nord != 0) {
-      double sinml[] = new double[C.length + 3];
-      double cosml[] = new double[C.length + 3];
-      sinml[1] = 0.0;
-      cosml[1] = 1.0;
-      for (int m = 1; m <= nord; m++) {
-        sinml[m + 1] = sinml[m] * cosphicosl + cosml[m] * cosphisinl;
-        cosml[m + 1] = cosml[m] * cosphicosl - sinml[m] * cosphisinl;
-        for (int n = m + 1; n <= (ndeg + 1); n++) {
-          V[n - m][n] = V[n][n - m];
-          V[n][n - m] = V[n][n - m] * sinml[m + 1];
-          V[n - m][n] = V[n - m][n] * cosml[m + 1];
-        }
+    
+    //-********************************************************************************************/
+    
+    double r3 = r2*r;
+    double r4 = r2*r2;
+    
+    double x2 = x*x;
+    double y2 = y*y;
+    double z2 = z*z;
+    
+    double rx = (y2+z2-x2)/r4;
+    double ix = -2*x*y/r4;
+    
+    double c1x = -2*x*z/r4;
+    double c2x = -2*x/r4;
+    
+    double ry = -(y*x)/r4;
+    double iy = (x2+z2-y2)/r4;
+    
+    double c1y = -2*y*z/r4;
+    double c2y = -2*y/r4;
+    
+    double rz = -2*z*x/r4;
+    double iz = -2*z*y/r4;
+    
+    double c1z = (x2+y2-z2)/r4;
+    double c2z = -2*z/r4;
+//    double calc =  3 * x * (4*z2 -  (x2+y2) )/(2*r3*r4) ;
+    double sigma = x2 +y2;
+    double calc20 = (2*z2 - sigma) / (2*r3*r2);
+    //    System.out.println("V21   :  " +  calc);
+    // diagonal  
+    
+    VrderX[0][0] = -x / r3;
+    ViderX[0][0] = 0;
+    VrderY[0][0] = -y / r3;
+    ViderY[0][0] = 0;
+    VrderZ[0][0] = -z / r3;
+    ViderZ[0][0] = 0;
+    
+    for (int n = 1; n<= mord; n++) {
+      VrderX[n][n] = (2*n-1)*(Vr[n-1][n-1]*rx-Vi[n-1][n-1]*ix +
+                              xonr2*VrderX[n-1][n-1]-yonr2*ViderX[n-1][n-1]);
+      
+      ViderX[n][n] = (2*n-1)*(Vr[n-1][n-1]*ix+Vi[n-1][n-1]*rx + 
+                              yonr2*VrderX[n-1][n-1]+xonr2*ViderX[n-1][n-1]);
+      
+      VrderY[n][n] = (2*n-1)*(Vr[n-1][n-1]*ry-Vi[n-1][n-1]*iy +
+                              xonr2*VrderY[n-1][n-1]-yonr2*ViderY[n-1][n-1]);                                                                      
+      
+      ViderY[n][n] = (2*n-1)*(Vr[n-1][n-1]*iy+Vi[n-1][n-1]*ry + 
+                              yonr2*VrderY[n-1][n-1]+xonr2*ViderY[n-1][n-1]);              
+      
+      VrderZ[n][n] = (2*n-1)*(Vr[n-1][n-1]*rz-Vi[n-1][n-1]*iz + 
+                              xonr2*VrderZ[n-1][n-1]-yonr2*ViderZ[n-1][n-1]);
+      
+      ViderZ[n][n] = (2*n-1)*(Vr[n-1][n-1]*iz+Vi[n-1][n-1]*rz + 
+                              yonr2*VrderZ[n-1][n-1]+xonr2*ViderZ[n-1][n-1]);
+      
+      VrderX[n][n-1] = ( 2 * n - 1 ) * ( c1x* Vr[n-1][n-1] + zonr2*VrderX[n-1][n-1]);
+      ViderX[n][n-1] = ( 2 * n - 1 ) * ( c1x* Vi[n-1][n-1] + zonr2*ViderX[n-1][n-1]);        
+      VrderY[n][n-1] = ( 2 * n - 1 ) * ( c1y* Vr[n-1][n-1] + zonr2*VrderY[n-1][n-1]);
+      ViderY[n][n-1] = ( 2 * n - 1 ) * ( c1y* Vi[n-1][n-1] + zonr2*ViderY[n-1][n-1]);        
+      VrderZ[n][n-1] = ( 2 * n - 1 ) * ( c1z* Vr[n-1][n-1] + zonr2*VrderZ[n-1][n-1]);
+      ViderZ[n][n-1] = ( 2 * n - 1 ) * ( c1z* Vi[n-1][n-1] + zonr2*ViderZ[n-1][n-1]);      
+    }
+    
+    // columns
+    
+    for (int m = 0; m<= mord; m++) {
+      for (int n = m+2; n<= ndeg; n++) {
+        VrderX[n][m] = (2.0*n - 1)/(n - m) * ( c1x* Vr[n-1][m] + zonr2*VrderX[n-1][m]) - (n+m-1.0)/(n-m)*(c2x*Vr[n-2][m] + zonr2*VrderX[n-2][m]);
+        ViderX[n][m] = (2.0*n - 1)/(n - m) * ( c1x* Vi[n-1][m] + zonr2*ViderX[n-1][m]) - (n+m-1.0)/(n-m)*(c2x*Vi[n-2][m] + zonr2*ViderX[n-2][m]);
+        VrderY[n][m] = (2.0*n - 1)/(n - m) * ( c1y* Vr[n-1][m] + zonr2*VrderY[n-1][m]) - (n+m-1.0)/(n-m)*(c2y*Vr[n-2][m] + zonr2*VrderY[n-2][m]);
+        ViderY[n][m] = (2.0*n - 1)/(n - m) * ( c1y* Vi[n-1][m] + zonr2*ViderY[n-1][m]) - (n+m-1.0)/(n-m)*(c2y*Vi[n-2][m] + zonr2*ViderY[n-2][m]);
+        VrderZ[n][m] = (2.0*n - 1)/(n - m) * ( c1z* Vr[n-1][m] + zonr2*VrderZ[n-1][m]) - (n+m-1.0)/(n-m)*(c2z*Vr[n-2][m] + zonr2*VrderZ[n-2][m]);
+        ViderZ[n][m] = (2.0*n - 1)/(n - m) * ( c1z* Vi[n-1][m] + zonr2*ViderZ[n-1][m]) - (n+m-1.0)/(n-m)*(c2z*Vi[n-2][m] + zonr2*ViderZ[n-2][m]);
       }
     }
-
-  }
-
-  /**
-   * compute the derivatives.
-   * 
-   * @param n degree of the concerened elementary potential
-   * @param m order of the concerened elementary potential
-   * @param V matrix containing all the elementary potentials
-   */
-  private void buildDerivatives(int n, int m, double[][] V) {
-    if (m == 0) {
-      // zonal terms
-
-      // real part
-      realDerX = -V[n + 1][n + 2];
-      realDerY = -V[n + 2][n + 1];
-      realDerZ = -V[n + 2][n + 2] * (double) (n + 1);
-
-      // no imaginary part for zonal terms
-      imaginaryDerX = 0.0;
-      imaginaryDerY = 0.0;
-      imaginaryDerZ = 0.0;
-
-    } else if (m == 1) {
-      // sectorial terms
-      double en = (double) n;
-      double fn = (double) (n * (n + 1));
-      double vn = fn * V[n + 2][n + 2];
-
-      // real part
-      realDerX = (-V[n][n + 2] + vn) / 2.0;
-      realDerY = -V[n + 2][n] / 2.0;
-      realDerZ = -en * V[n + 1][n + 2];
-
-      // imaginary part
-      imaginaryDerX = realDerY;
-      imaginaryDerY = (V[n][n + 2] + vn) / 2.0;
-      imaginaryDerZ = -en * V[n + 2][n + 1];
-
-    } else {
-      // tessereal terms
-      double enm = (double) (n - m + 1);
-      double fnm = (double) (n - m + 1) * (double) (n - m + 2);
-
-      // real part
-      realDerX = (-V[n - m + 1][n + 2] + fnm * V[n - m + 3][n + 2]) / 2.0;
-      realDerY = (-V[n + 2][n - m + 1] - fnm * V[n + 2][n - m + 3]) / 2.0;
-      realDerZ = -enm * V[n - m + 2][n + 2];
-
-      // imaginary part
-      imaginaryDerX = (-V[n + 2][n - m + 1] + fnm * V[n + 2][n - m + 3]) / 2.0;
-      imaginaryDerY = (V[n - m + 1][n + 2] + fnm * V[n - m + 3][n + 2]) / 2.0;
-      imaginaryDerZ = -enm * V[n + 2][n - m + 2];
-
+    
+    //******************-**********************************************************************
+    
+    double vdX = 0.0;
+    double vdY = 0.0;
+    double vdZ = 0.0;
+    double rn = 1.0;
+    for (int n=0 ; n<=ndeg ; n++) {
+      for (int m=0 ; m<=n ; m++) {
+        vdX += rn*(C[n][m]*VrderX[n][m]+S[n][m]*ViderX[n][m]);
+        vdY += rn*(C[n][m]*VrderY[n][m]+S[n][m]*ViderY[n][m]);
+        vdZ += rn*(C[n][m]*VrderZ[n][m]+S[n][m]*ViderZ[n][m]);
+      }
+      rn *= equatorialRadius;
     }
+    Vector3D accInBody = new Vector3D(mu*vdX,mu*vdY,mu*vdZ);
+    Vector3D accInInert = bodyFrame.getTransformTo(adder.getFrame(), date).transformVector(accInBody);
+    adder.addXYZAcceleration(accInInert.getX(), accInInert.getY(), accInInert.getZ());
+    
   }
-
+ 
+  
   public SWF[] getSwitchingFunctions() {
     return null;
   }
-
+  
   /** Initialisation of potential array. */
-  private double[][] V;
-
-  /** Real part of potential derivatives. */
-  private double realDerX;
-
-  private double realDerY;
-
-  private double realDerZ;
-
-  /** Imaginary part of potential derivatives. */
-  private double imaginaryDerX;
-
-  private double imaginaryDerY;
-
-  private double imaginaryDerZ;
-
-  /** Initialization of the acceleration. */
-  private int ndd;
-
-  private int nod;
-
+  
+  private double[][] Vi;
+  private double[][] Vr;
+  
+  private double[][] ViderX;
+  private double[][] VrderX;
+  private double[][] ViderY;
+  private double[][] VrderY;
+  private double[][] ViderZ;
+  private double[][] VrderZ;
+  
   /** Equatorial radius of the Central Body. */
   private double equatorialRadius;
-
+  
   /** Intermediate variables. */
-  private double muOnAe;
-
+  private double mu;
+  
   /** First normalized potential tesseral coefficients array. */
   private double[][] C;
-
+  
   /** Second normalized potential tesseral coefficients array. */
   private double[][] S;
-
+  
   /** Definition of degree, order and maximum potential size. */
   private int ndeg;
-
-  private int nord;
-
+  
+  private int mord;
+  
   /** Rotating body. */
-  private RotatingBody body;
-
+  private SynchronizedFrame bodyFrame;
+  
 }
