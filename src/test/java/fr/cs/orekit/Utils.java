2a1da33eb628bb8bf2251c92d2619946346d9d2f
Luc Maisonobe <luc@orekit.org>
2008-04-02 12:32:15 +0000

    removed unused method

--- src/test/java/fr/cs/orekit/Utils.java
+++ src/test/java/fr/cs/orekit/Utils.java
@@ -1,9 +1,5 @@
 package fr.cs.orekit;
 
-import java.util.Random;
-
-import org.apache.commons.math.geometry.Vector3D;
-
 public class Utils {
 
     // epsilon for tests
@@ -17,15 +13,6 @@ public class Utils {
 
     // epsilon for angles
     public static final double epsilonAngle = 1.e+5 * epsilonTest;
-
-
-    //trim an angle between ref - PI and ref + PI
-    public static Vector3D randomVector (int size) {
-        Random rand = new Random();
-        return new Vector3D(rand.nextDouble()*size,
-                            rand.nextDouble()*size,
-                            rand.nextDouble()*size);
-    }
 
     public static final double ae =  6378136.460;
     public static final double mu =  3.986004415e+14;
