aadc11d8d7f6daa257c210fcafbf789942d6d6d9
Luc Maisonobe <luc@orekit.org>
2008-04-02 15:49:11 +0000

    added correct handling of maneuvers depending on attitude (i.e. defined in spacecraft frame)

--- src/main/java/fr/cs/orekit/propagation/numerical/forces/maneuvers/ConstantThrustManeuver.java
+++ src/main/java/fr/cs/orekit/propagation/numerical/forces/maneuvers/ConstantThrustManeuver.java
@@ -24,6 +24,9 @@ public class ConstantThrustManeuver implements ForceModel {
     /** Identifier for inertial frame. */
     public static final int INERTIAL = 2;
 
+    /** Identifier for spacecraft frame. */
+    public static final int SPACECRAFT = 3;
+
     /** Reference gravity acceleration constant (m/s<sup>2</sup>) */
     private static final double g0 = 9.80665;
 
@@ -49,10 +52,7 @@ public class ConstantThrustManeuver implements ForceModel {
     private final double flowRate;
 
     /** Direction of the acceleration in selected frame. */
-    private Vector3D direction;
-
-    /** Thrust direction provider. */
-    private ThrustForceDirection variableDir;
+    private final Vector3D direction;
 
     /** Simple constructor for a constant direction and constant thrust.
      * @param date maneuver date
@@ -60,10 +60,10 @@ public class ConstantThrustManeuver implements ForceModel {
      * the date is considered to be the stop date)
      * @param thrust the thrust force (N)
      * @param isp the Isp (s)
-     * @param direction the acceleration direction in choosed frame.
-     * @param frameType the frame in which is defined the direction
+     * @param direction the acceleration direction in chosen frame.
+     * @param frameType the frame in which the direction is defined
      * @exception IllegalArgumentException if frame type is not one of
-     * {@link #TNW}, {@link #QSW} or  {@link #INERTIAL}
+     * {@link #TNW}, {@link #QSW}, {@link #INERTIAL} or {@link #SPACECRAFT}
      * @see #QSW
      * @see #TNW
      * @see #INERTIAL
@@ -73,11 +73,12 @@ public class ConstantThrustManeuver implements ForceModel {
                                   int frameType)
         throws IllegalArgumentException {
 
-        if ((frameType != QSW) && (frameType != TNW) && (frameType != INERTIAL)) {
+        if ((frameType != QSW) && (frameType != TNW) &&
+            (frameType != INERTIAL) && (frameType != SPACECRAFT)) {
             OrekitException.throwIllegalArgumentException("unsupported thrust direction frame, " +
-                                                          "supported types: {0}, {1} and {2}",
+                                                          "supported types: {0}, {1}, {2} and {3}",
                                                           new Object[] {
-                                                              "QSW", "TNW", "INERTIAL"
+                                                              "QSW", "TNW", "INERTIAL", "SPACECRAFT"
                                                           });
         }
 
@@ -99,21 +100,6 @@ public class ConstantThrustManeuver implements ForceModel {
 
     }
 
-    /** Constructor for a variable direction and constant thrust.
-     * @param date maneuver date
-     * @param duration the duration of the thrust (s) (if negative,
-     * the date is considered to be the stop date)
-     * @param thrust the thrust force (N)
-     * @param isp the specific impulse (s)
-     * @param direction the variable acceleration direction.
-     */
-    public ConstantThrustManeuver(AbsoluteDate startDate, double duration,
-                                  double thrust, double isp, ThrustForceDirection direction) {
-
-        this(startDate, duration, thrust, isp, null, direction.getType());
-        this.variableDir = direction;
-    }
-
     /** Compute the contribution of maneuver to the global acceleration.
      * @param s the current state information : date, cinematics, attitude
      * @param adder object where the contribution should be added
@@ -124,25 +110,36 @@ public class ConstantThrustManeuver implements ForceModel {
         throws OrekitException {
 
         if (firing) {
-            if (variableDir != null) {
-                direction = variableDir.getDirection(s).normalize();
+
+            // add thrust acceleration
+            final double acceleration = thrust / s.getMass();
+            switch (frameType) {
+            case QSW :
+                adder.addQSWAcceleration(acceleration * direction.getX(),
+                                         acceleration * direction.getY(),
+                                         acceleration * direction.getZ());
+                break;
+            case TNW :
+                adder.addTNWAcceleration(acceleration * direction.getX(),
+                                         acceleration * direction.getY(),
+                                         acceleration * direction.getZ());
+                break;
+            case INERTIAL :
+                adder.addXYZAcceleration(acceleration * direction.getX(),
+                                         acceleration * direction.getY(),
+                                         acceleration * direction.getZ());
+                break;
+            default :
+                // the thrust is in spacecraft frame, it depends on attitude
+                Vector3D inertialThrust = s.getAttitude().applyTo(direction);
+                adder.addXYZAcceleration(acceleration * inertialThrust.getX(),
+                                         acceleration * inertialThrust.getY(),
+                                         acceleration * inertialThrust.getZ());
             }
 
-            final double acc = thrust/s.getMass();
-            final Vector3D acceleration = new Vector3D(acc, direction);
-
-            if (frameType == QSW) {
-                adder.addQSWAcceleration(acceleration.getX(),
-                                         acceleration.getY(), acceleration.getZ());
-            } else if (frameType == TNW) {
-                adder.addTNWAcceleration(acceleration.getX(),
-                                         acceleration.getY(), acceleration.getZ());
-            } else {
-                adder.addXYZAcceleration(acceleration.getX(),
-                                         acceleration.getY(), acceleration.getZ());
-            }
-
+            // add flow rate
             adder.addMassDerivative(flowRate);
+
         }
 
     }
@@ -154,7 +151,7 @@ public class ConstantThrustManeuver implements ForceModel {
         return new OrekitSwitchingFunction[] { new StartSwitch(), new EndSwitch() };
     }
 
-    /** This class defines the begining of the acceleration switching function.
+    /** This class defines the beginning of the acceleration switching function.
      * It triggers at the ignition.
      */
     private class StartSwitch implements OrekitSwitchingFunction {
@@ -163,11 +160,12 @@ public class ConstantThrustManeuver implements ForceModel {
         private static final long serialVersionUID = -3763244241136150814L;
 
         public void eventOccurred(SpacecraftState s, double mu) {
+            // start the maneuver
             firing = true;
         }
 
         /** The G-function is the difference between the start date and the current date.
-         * @param s the current state information : date, cinematics, attitude
+         * @param s the current state information : date, kinematics, attitude
          * @param mu central gravitation coefficient
          */
         public double g(SpacecraftState s, double mu) throws OrekitException {
@@ -198,11 +196,12 @@ public class ConstantThrustManeuver implements ForceModel {
         private static final long serialVersionUID = -4081671157610680754L;
 
         public void eventOccurred(SpacecraftState s, double mu) {
+            // stop the maneuver
             firing = false;
         }
 
-        /** The G-function is the difference between the end date and the currentdate.
-         * @param s the current state information : date, cinematics, attitude
+        /** The G-function is the difference between the end date and the current date.
+         * @param s the current state information : date, kinematics, attitude
          * @param mu central gravitation coefficient
          */
         public double g(SpacecraftState s, double mu) throws OrekitException {
