17a9373a954427b6e551a659529f2ce8fd25a062
Luc Maisonobe <luc@orekit.org>
2008-06-19 13:42:45 +0000

    moved test packages to reflect main packages reorganization

--- src/test/java/org/orekit/forces/gravity/CunninghamAttractionModelTest.java
+++ src/test/java/org/orekit/forces/gravity/CunninghamAttractionModelTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.orekit.perturbations;
+package org.orekit.forces.gravity;
 
 import java.io.FileNotFoundException;
 import java.text.ParseException;
