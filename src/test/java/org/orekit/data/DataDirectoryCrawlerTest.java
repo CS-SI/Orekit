0c68f7469493208f034ce8bfe08ef7b5792acff1
Luc Maisonobe <luc@orekit.org>
2008-09-11 11:57:37 +0000

    improved test coverage

--- src/test/java/org/orekit/data/DataDirectoryCrawlerTest.java
+++ src/test/java/org/orekit/data/DataDirectoryCrawlerTest.java
@@ -17,35 +17,113 @@
 package org.orekit.data;
 
 import java.io.BufferedReader;
-
-import org.orekit.data.DataDirectoryCrawler;
-import org.orekit.data.DataFileCrawler;
-import org.orekit.errors.OrekitException;
+import java.io.IOException;
+import java.net.URL;
+import java.text.ParseException;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.orekit.errors.OrekitException;
+
 public class DataDirectoryCrawlerTest extends TestCase {
 
-    public void testNoDirectory() {
-        checkFailure("inexistant-directory");
+    public void testNoDirectoryFS() {
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, "inexistant-directory");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "");
+        checkFailure();
     }
 
-    public void testNotADirectory() {
-        checkFailure("regular-data/UTC-TAI.history");
+    public void testNoDirectoryCP() {
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, "");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "inexistant-directory");
+        checkFailure();
     }
 
-    public void testNominal() throws OrekitException {
+    public void testNotADirectoryFS() {
+        URL url =
+            DataDirectoryCrawlerTest.class.getClassLoader().getResource("regular-data/UTC-TAI.history");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, url.getPath());
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "");
+        checkFailure();
+    }
+
+    public void testNotADirectoryCP() {
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, "");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "regular-data/UTC-TAI.history");
+        checkFailure();
+    }
+
+    public void testNominalFS() throws OrekitException {
+        URL url =
+            DataDirectoryCrawlerTest.class.getClassLoader().getResource("regular-data");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, url.getPath());
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "");
+        CountingCrawler crawler = new CountingCrawler(".*");
+        new DataDirectoryCrawler().crawl(crawler);
+        assertTrue(crawler.getCount() > 0);
+    }
+
+    public void testNominalCP() throws OrekitException {
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, "");
         System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "regular-data");
         CountingCrawler crawler = new CountingCrawler(".*");
         new DataDirectoryCrawler().crawl(crawler);
         assertTrue(crawler.getCount() > 0);
     }
 
-    private void checkFailure(String directoryName) {
+    public void testIOException() throws OrekitException {
+        URL url =
+            DataDirectoryCrawlerTest.class.getClassLoader().getResource("regular-data");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, url.getPath());
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "");
         try {
-            System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, directoryName);
+            new DataDirectoryCrawler().crawl(new DataFileCrawler(".*") {
+                protected void visit(BufferedReader reader) throws IOException {
+                    if (getFile().getName().equals("UTC-TAI.history")) {
+                        throw new IOException("dummy error");
+                    }
+                }
+            });
+            fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            // expected behavior
+            assertNotNull(oe.getCause());
+            assertEquals(IOException.class, oe.getCause().getClass());
+            assertEquals("dummy error", oe.getMessage());
+        } catch (Exception e) {
+            fail("wrong exception caught");
+        }
+    }
+
+    public void testParseException() throws OrekitException {
+        URL url =
+            DataDirectoryCrawlerTest.class.getClassLoader().getResource("regular-data");
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_FS, url.getPath());
+        System.setProperty(DataDirectoryCrawler.DATA_ROOT_DIRECTORY_CP, "");
+        try {
+            new DataDirectoryCrawler().crawl(new DataFileCrawler(".*") {
+                protected void visit(BufferedReader reader) throws ParseException {
+                    if (getFile().getName().equals("UTC-TAI.history")) {
+                        throw new ParseException("dummy error", 0);
+                    }
+                }
+            });
+            fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            // expected behavior
+            assertNotNull(oe.getCause());
+            assertEquals(ParseException.class, oe.getCause().getClass());
+            assertEquals("dummy error", oe.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            fail("wrong exception caught");
+        }
+    }
+
+    private void checkFailure() {
+        try {
             new DataDirectoryCrawler().crawl(new CountingCrawler(".*"));
             fail("an exception should have been thrown");
         } catch (OrekitException e) {
