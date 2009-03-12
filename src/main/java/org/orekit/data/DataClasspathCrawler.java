cd08c9babc71cd2b1d00cab15489fb86fc24c4f2
Luc Maisonobe <luc@orekit.org>
2009-03-12 13:03:22 +0000

    Re-activated a way to put configuration data in the classpath.

---
+++ src/main/java/org/orekit/data/DataClasspathCrawler.java
@@ -0,0 +1,166 @@
+/* Copyright 2002-2009 CS Communication & Systèmes
+ * Licensed to CS Communication & Systèmes (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
+
+import org.orekit.errors.OrekitException;
+
+
+/** Provider for data files stored as resources in the classpath.
+
+ * <p>
+ * This class handles a list of data files or zip/jar archives located in the
+ * classpath. Since the classpath is not a tree structure the list elements
+ * cannot be whole directories recursively browsed as in {@link
+ * DataDirectoryCrawler}, they must be data files or zip/jar archives.
+ * </p>
+ * <p>
+ * A typical use case is to put all data files in a single zip or jar archive
+ * and to build an instance of this class with the single name of this zip/jar
+ * archive. Two different instances may be used one for user or project specific
+ * data and another one for system-wide or general data.
+ * </p>
+ * <p>
+ * Gzip-compressed files are supported.
+ * </p>
+ * <p>
+ * Zip archives entries are supported recursively.
+ * </p>
+ * <p>
+ * This is a simple application of the <code>visitor</code> design pattern for
+ * list browsing.
+ * </p>
+ * @see DataProvidersManager
+ * @author Luc Maisonobe
+ * @version $Revision$ $Date$
+ */
+public class DataClasspathCrawler implements DataProvider {
+
+    /** Serializable UID. */
+    private static final long serialVersionUID = -6827421110984075462L;
+
+    /** Pattern for gzip files. */
+    private static final Pattern GZIP_FILE_PATTERN = Pattern.compile("(.*)\\.gz$");
+
+    /** Pattern for zip archives. */
+    private static final Pattern ZIP_ARCHIVE_PATTERN = Pattern.compile("(.*)(?:(?:\\.zip)|(?:\\.jar))$");
+
+    /** List elements. */
+    private final List<String> listElements;
+
+    /** Build a data classpath crawler.
+     * @param list list of data file names within the classpath
+     * @exception OrekitException if a list elements is not an existing resource
+     */
+    public DataClasspathCrawler(String... list) throws OrekitException {
+
+        listElements = new ArrayList<String>();
+
+        // check the resources
+        for (final String name : list) {
+            if (!"".equals(name)) {
+
+                final String convertedName = name.replace('\\', '/');
+                final InputStream stream =
+                    DataClasspathCrawler.class.getClassLoader().getResourceAsStream(convertedName);
+                if (stream == null) {
+                    throw new OrekitException("{0} does not exist in classpath", new Object[] { name });
+                }
+
+                listElements.add(convertedName);
+                try {
+                    stream.close();
+                } catch (IOException exc) {
+                    // ignore this error
+                }
+            }
+        }
+
+    }
+
+    /** {@inheritDoc} */
+    public void feed(final DataFileLoader visitor) throws OrekitException {
+
+        try {
+            OrekitException delayedException = null;
+            boolean loaded = false;
+            for (String name : listElements) {
+                try {
+
+                    if (ZIP_ARCHIVE_PATTERN.matcher(name).matches()) {
+
+                        // browse inside the zip/jar file
+                        new DataZipCrawler(name, DataZipCrawler.Location.CLASSPATH).feed(visitor);
+                        loaded = true;
+
+                    } else {
+
+                        // remove suffix from gzip files
+                        final Matcher gzipMatcher = GZIP_FILE_PATTERN.matcher(name);
+                        final String baseName = gzipMatcher.matches() ? gzipMatcher.group(1) : name;
+
+                        if (visitor.fileIsSupported(baseName)) {
+
+                            final InputStream stream =
+                                DataClasspathCrawler.class.getClassLoader().getResourceAsStream(name);
+
+                            // visit the current file
+                            if (gzipMatcher.matches()) {
+                                visitor.loadData(new GZIPInputStream(stream), name);
+                            } else {
+                                visitor.loadData(stream, name);
+                            }
+
+                            stream.close();
+                            loaded = true;
+
+                        }
+
+                    }
+
+                } catch (OrekitException oe) {
+                    // maybe the next path component will be able to provide data
+                    // wait until all components have been tried
+                    delayedException = oe;
+                }
+            }
+
+            if (!loaded) {
+                if (delayedException == null) {
+                    throw new OrekitException("no data has been loaded", new Object[0]);
+                } else {
+                    throw delayedException;
+                }
+            }
+
+        } catch (IOException ioe) {
+            throw new OrekitException(ioe.getMessage(), ioe);
+        } catch (ParseException pe) {
+            throw new OrekitException(pe.getMessage(), pe);
+        }
+
+    }
+
+}
---
+++ src/test/java/org/orekit/data/DataClasspathCrawlerTest.java
@@ -0,0 +1,146 @@
+/* Copyright 2002-2009 CS Communication & Systèmes
+ * Licensed to CS Communication & Systèmes (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.regex.Pattern;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.orekit.errors.OrekitException;
+
+public class DataClasspathCrawlerTest extends TestCase {
+
+    public void testNoElement() {
+        checkFailure("inexistant-element");
+    }
+
+    public void testNominal() throws OrekitException {
+        CountingLoader crawler = new CountingLoader(".*");
+        new DataClasspathCrawler("regular-data/UTC-TAI.history",
+                                 "regular-data/de405-ephemerides/unxp0000.405",
+                                 "regular-data/de405-ephemerides/unxp0001.405",
+                                 "regular-data/de406-ephemerides/unxp0000.406",
+                                 "regular-data/Earth-orientation-parameters/monthly/bulletinb_IAU2000-216.txt",
+                                 "no-data").feed(crawler);
+        assertEquals(6, crawler.getCount());
+    }
+
+    public void testMultiZip() throws OrekitException {
+        CountingLoader crawler = new CountingLoader(".*\\.txt$");
+        new DataClasspathCrawler("multizip.zip").feed(crawler);
+        assertEquals(6, crawler.getCount());
+    }
+
+    public void testIOException() throws OrekitException {
+        try {
+            new DataClasspathCrawler("regular-data/UTC-TAI.history").feed(new IOExceptionLoader(".*"));
+            fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            // expected behavior
+            assertNotNull(oe.getCause());
+            assertEquals(IOException.class, oe.getCause().getClass());
+            assertEquals("dummy error", oe.getMessage());
+        } catch (Exception e) {
+            fail("wrong exception caught");
+        }
+    }
+
+    public void testParseException() throws OrekitException {
+        try {
+            new DataClasspathCrawler("regular-data/UTC-TAI.history").feed(new ParseExceptionLoader(".*"));
+            fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            // expected behavior
+            assertNotNull(oe.getCause());
+            assertEquals(ParseException.class, oe.getCause().getClass());
+            assertEquals("dummy error", oe.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            fail("wrong exception caught");
+        }
+    }
+
+    private void checkFailure(String ...list) {
+        try {
+            new DataClasspathCrawler(list).feed(new CountingLoader(".*"));
+            fail("an exception should have been thrown");
+        } catch (OrekitException e) {
+            // expected behavior
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("wrong exception caught");
+        }
+    }
+
+    private static class CountingLoader implements DataFileLoader {
+        private Pattern namePattern;
+        private int count;
+        public CountingLoader(String pattern) {
+            namePattern = Pattern.compile(pattern);
+            count = 0;
+        }
+        public void loadData(InputStream input, String name) {
+            ++count;
+        }
+        public int getCount() {
+            return count;
+        }
+        public boolean fileIsSupported(String fileName) {
+            return namePattern.matcher(fileName).matches();
+        }
+    }
+
+    private static class IOExceptionLoader implements DataFileLoader {
+        private Pattern namePattern;
+        public IOExceptionLoader(String pattern) {
+            namePattern = Pattern.compile(pattern);
+        }
+        public void loadData(InputStream input, String name) throws IOException {
+            if (name.endsWith("UTC-TAI.history")) {
+                throw new IOException("dummy error");
+            }
+        }
+        public boolean fileIsSupported(String fileName) {
+            return namePattern.matcher(fileName).matches();
+        }
+    }
+
+    private static class ParseExceptionLoader implements DataFileLoader {
+        private Pattern namePattern;
+        public ParseExceptionLoader(String pattern) {
+            namePattern = Pattern.compile(pattern);
+        }
+        public void loadData(InputStream input, String name) throws ParseException {
+            if (name.endsWith("UTC-TAI.history")) {
+                throw new ParseException("dummy error", 0);
+            }
+        }
+        public boolean fileIsSupported(String fileName) {
+            return namePattern.matcher(fileName).matches();
+        }
+    }
+
+    public static Test suite() {
+        return new TestSuite(DataClasspathCrawlerTest.class);
+    }
+
+}
