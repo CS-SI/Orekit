2deca1d31f5dcc1753d00f910422d00ecf296933
Luc Maisonobe <luc@orekit.org>
2009-08-05 10:14:19 +0000

    new tests

---
+++ src/test/java/org/orekit/frames/PEFFrameAlternateConfigurationTest.java
@@ -0,0 +1,116 @@
+/* Copyright 2002-2008 CS Communication & Systèmes
+ * Licensed to CS Communication & Systèmes (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.frames;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.math.geometry.Vector3D;
+import org.junit.Before;
+import org.junit.Test;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.errors.OrekitException;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.DateComponents;
+import org.orekit.time.TimeComponents;
+import org.orekit.time.UTCScale;
+import org.orekit.utils.PVCoordinates;
+
+
+public class PEFFrameAlternateConfigurationTest {
+
+    @Test
+    public void testAASReferenceLEO() throws OrekitException {
+
+        // this reference test has been extracted from the following paper:
+        // Implementation Issues Surrounding the New IAU Reference Systems for Astrodynamics
+        // David A. Vallado, John H. Seago, P. Kenneth Seidelmann
+        // http://www.centerforspace.com/downloads/files/pubs/AAS-06-134.pdf
+        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2004, 04, 06),
+                                           new TimeComponents(07, 51, 28.386009),
+                                           UTCScale.getInstance());
+
+        // PEF iau76
+        PVCoordinates pvPEF =
+           new PVCoordinates(new Vector3D(-1033475.0313, 7901305.5856, 6380344.5328),
+                             new Vector3D(-3225.632747, -2872.442511, 5531.931288));
+
+        // TOD iau76
+        PVCoordinates pvTEME =
+            new PVCoordinates(new Vector3D(5094514.7804, 6127366.4612, 6380344.5328),
+                              new Vector3D(-4746.088567, 786.077222, 5531.931288));
+        
+        Transform t = FrameFactory.getTEME(true).getTransformTo(FrameFactory.getPEF(true), t0);
+        checkPV(pvPEF, t.transformPVCoordinates(pvTEME), 0.0091, 4.7e-6);
+
+        // if dut1 and lod corrections are ignored, results must be really bad
+        t = FrameFactory.getTEME(false).getTransformTo(FrameFactory.getPEF(false), t0);
+        PVCoordinates delta = new PVCoordinates(1.0, pvPEF, -1.0, t.transformPVCoordinates(pvTEME));
+        assertEquals(255.644, delta.getPosition().getNorm(), 4.0e-6);
+        assertEquals(0.13856, delta.getVelocity().getNorm(), 9.0e-7);
+
+    }
+
+    @Test
+    public void testAASReferenceGEO() throws OrekitException {
+
+        // this reference test has been extracted from the following paper:
+        // Implementation Issues Surrounding the New IAU Reference Systems for Astrodynamics
+        // David A. Vallado, John H. Seago, P. Kenneth Seidelmann
+        // http://www.centerforspace.com/downloads/files/pubs/AAS-06-134.pdf
+        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2004, 06, 01),
+                                           TimeComponents.H00,
+                                           UTCScale.getInstance());
+
+        Transform t = FrameFactory.getTEME(true).getTransformTo(FrameFactory.getPEF(true), t0);
+
+        // TOD iau76
+        PVCoordinates pvTEME =
+            new PVCoordinates(new Vector3D(-40577427.7501, -11500096.1306, 10293.2583),
+                              new Vector3D(837.552338, -2957.524176, -0.928772));
+
+        //PEF iau76
+        PVCoordinates pvPEF =
+            new PVCoordinates(new Vector3D(24796919.2956, -34115870.9001, 10293.2583),
+                              new Vector3D(-0.979178, -1.476540, -0.928772));
+
+        checkPV(pvPEF, t.transformPVCoordinates(pvTEME), 0.049, 5.2e-7);
+
+        // if dut1 and lod corrections are ignored, results must be really bad
+        t = FrameFactory.getTEME(false).getTransformTo(FrameFactory.getPEF(false), t0);
+        PVCoordinates delta = new PVCoordinates(1.0, pvPEF, -1.0, t.transformPVCoordinates(pvTEME));
+        assertEquals(1448.217, delta.getPosition().getNorm(), 4.0e-4);
+        assertEquals(6.1e-5, delta.getVelocity().getNorm(), 2.0e-8);
+
+    }
+
+    @Before
+    public void setUp() {
+        String root = getClass().getClassLoader().getResource("testpef-data").getPath();
+        System.setProperty(DataProvidersManager.OREKIT_DATA_PATH, root);
+    }
+
+    private void checkPV(PVCoordinates reference,
+                         PVCoordinates result, double positionThreshold,
+                         double velocityThreshold) {
+
+        Vector3D dP = result.getPosition().subtract(reference.getPosition());
+        Vector3D dV = result.getVelocity().subtract(reference.getVelocity());
+        assertEquals(0, dP.getNorm(), positionThreshold);
+        assertEquals(0, dV.getNorm(), velocityThreshold);
+    }
+
+}
---
+++ src/test/java/org/orekit/frames/TEMEFrameAlternateConfigurationTest.java
@@ -0,0 +1,165 @@
+/* Copyright 2002-2008 CS Communication & Systèmes
+ * Licensed to CS Communication & Systèmes (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.frames;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.math.geometry.Vector3D;
+import org.orekit.data.DataProvidersManager;
+import org.orekit.errors.OrekitException;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.DateComponents;
+import org.orekit.time.TAIScale;
+import org.orekit.time.TimeComponents;
+import org.orekit.time.UTCScale;
+import org.orekit.utils.PVCoordinates;
+
+
+public class TEMEFrameAlternateConfigurationTest {
+
+    @Test
+    public void testAASReferenceLEO() throws OrekitException {
+
+        // this reference test has been extracted from the following paper:
+        // Implementation Issues Surrounding the New IAU Reference Systems for Astrodynamics
+        // David A. Vallado, John H. Seago, P. Kenneth Seidelmann
+        // http://www.centerforspace.com/downloads/files/pubs/AAS-06-134.pdf
+        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2004, 04, 06),
+                                           new TimeComponents(07, 51, 28.386009),
+                                           UTCScale.getInstance());
+
+        Transform tt = FrameFactory.getMEME(true).getTransformTo(FrameFactory.getTEME(true), t0);
+        Transform tf = FrameFactory.getMEME(false).getTransformTo(FrameFactory.getTEME(false), t0);
+
+        //TOD iau76
+        PVCoordinates pvTODiau76 =
+            new PVCoordinates(new Vector3D(5094514.7804, 6127366.4612, 6380344.5328),
+                              new Vector3D(-4746.088567, 786.077222, 5531.931288));
+        //MOD iau76
+        PVCoordinates pvMODiau76 =
+            new PVCoordinates(new Vector3D(5094029.0167, 6127870.9363, 6380247.8885),
+                              new Vector3D(-4746.262495, 786.014149, 5531.791025));
+        //MOD iau76 w corr
+        PVCoordinates pvMODiau76Wcorr =
+            new PVCoordinates(new Vector3D(5094028.3745, 6127870.8164, 6380248.5164),
+                              new Vector3D(-4746.263052, 786.014045, 5531.790562));
+
+        checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76Wcorr), 1.9, 1.2e-3);
+        checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76), 2.5, 1.5e-3);
+        checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76), 6.8e-5, 6.0e-7);
+        checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76Wcorr), 0.90615, 7.4e-4);
+
+    }
+
+    @Test
+    public void testAASReferenceGEO() throws OrekitException {
+
+        // this reference test has been extracted from the following paper:
+        // Implementation Issues Surrounding the New IAU Reference Systems for Astrodynamics
+        // David A. Vallado, John H. Seago, P. Kenneth Seidelmann
+        // http://www.centerforspace.com/downloads/files/pubs/AAS-06-134.pdf
+
+        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2004, 06, 01),
+                                           TimeComponents.H00,
+                                           UTCScale.getInstance());
+
+        Transform tt = FrameFactory.getMEME(true).getTransformTo(FrameFactory.getTEME(true), t0);
+        Transform tf = FrameFactory.getMEME(false).getTransformTo(FrameFactory.getTEME(false), t0);
+
+        //TOD iau76
+        PVCoordinates pvTODiau76 =
+            new PVCoordinates(new Vector3D(-40577427.7501, -11500096.1306, 10293.2583),
+                              new Vector3D(837.552338, -2957.524176, -0.928772));
+        //MOD iau76
+        PVCoordinates pvMODiau76 =
+            new PVCoordinates(new Vector3D(-40576822.6385, -11502231.5013, 9738.2304),
+                              new Vector3D(837.708020, -2957.480118, -0.814275));
+        //MOD iau76 w corr
+        PVCoordinates pvMODiau76Wcorr =
+            new PVCoordinates(new Vector3D(-40576822.6395, -11502231.5015, 9733.7842),
+                              new Vector3D(837.708020, -2957.480117, -0.814253));
+
+        checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76Wcorr), 10.1, 7.4e-4);
+        checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76Wcorr), 4.5, 2.3e-5);
+
+        checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76), 4.6e-5, 8.8e-7);
+        checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76), 11.1, 7.4e-4);
+
+    }
+
+    @Test
+    public void testInterpolationAccuracy() throws OrekitException, FileNotFoundException {
+
+        final boolean withNutationCorrection = true;
+        
+        TEMEFrame interpolatingFrame =
+            new TEMEFrame(withNutationCorrection, AbsoluteDate.J2000_EPOCH, "");
+        NonInterpolatingTEMEFrame nonInterpolatingFrame =
+            new NonInterpolatingTEMEFrame(withNutationCorrection, AbsoluteDate.J2000_EPOCH, "");
+
+        // the following time range is located around the maximal observed error
+        AbsoluteDate start = new AbsoluteDate(2002, 11, 11, 0, 0, 0.0, TAIScale.getInstance());
+        AbsoluteDate end   = new AbsoluteDate(2002, 11, 15, 6, 0, 0.0, TAIScale.getInstance());
+        double maxError = 0.0;
+        for (AbsoluteDate date = start;
+             date.compareTo(end) < 0;
+             date = new AbsoluteDate(date, 60)) {
+            final Transform transform =
+                interpolatingFrame.getTransformTo(nonInterpolatingFrame, date);
+            final double error = transform.getRotation().getAngle() * 648000 / Math.PI;
+            maxError = Math.max(maxError, error);
+        }
+
+        assertTrue(maxError < 7.2e-11);
+
+    }
+
+    @Before
+    public void setUp() {
+        String root = getClass().getClassLoader().getResource("testpef-data").getPath();
+        System.setProperty(DataProvidersManager.OREKIT_DATA_PATH, root);
+    }
+
+    private class NonInterpolatingTEMEFrame extends TEMEFrame {
+        private static final long serialVersionUID = -7116622345154042273L;
+        public NonInterpolatingTEMEFrame(final boolean ignoreNutationCorrection,
+                                         final AbsoluteDate date, final String name)
+            throws OrekitException {
+            super(ignoreNutationCorrection, date, name);
+        }
+        protected void setInterpolatedNutationElements(final double t) {
+            computeNutationElements(t);
+        }
+    }
+
+    private void checkPV(PVCoordinates reference,
+                         PVCoordinates result, double positionThreshold,
+                         double velocityThreshold) {
+
+        Vector3D dP = result.getPosition().subtract(reference.getPosition());
+        Vector3D dV = result.getVelocity().subtract(reference.getVelocity());
+        assertEquals(0, dP.getNorm(), positionThreshold);
+        assertEquals(0, dV.getNorm(), velocityThreshold);
+    }
+
+}
