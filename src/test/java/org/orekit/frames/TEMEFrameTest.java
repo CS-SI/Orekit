a2168472d1e5028c59505dde550906638973678f
Luc Maisonobe <luc@orekit.org>
2009-08-05 09:57:28 +0000

    test cleanup

--- src/test/java/org/orekit/frames/TEMEFrameTest.java
+++ src/test/java/org/orekit/frames/TEMEFrameTest.java
@@ -51,50 +51,28 @@ public class TEMEFrameTest {
         Transform tt = FrameFactory.getMEME(true).getTransformTo(FrameFactory.getTEME(true), t0);
         Transform tf = FrameFactory.getMEME(false).getTransformTo(FrameFactory.getTEME(false), t0);
 
-//TOD iau76
+        //TOD iau76
         PVCoordinates pvTODiau76 =
             new PVCoordinates(new Vector3D(5094514.7804, 6127366.4612, 6380344.5328),
                               new Vector3D(-4746.088567, 786.077222, 5531.931288));
-//MOD iau76
+        //MOD iau76
         PVCoordinates pvMODiau76 =
             new PVCoordinates(new Vector3D(5094029.0167, 6127870.9363, 6380247.8885),
                               new Vector3D(-4746.262495, 786.014149, 5531.791025));
-//MOD iau76 w corr
+        //MOD iau76 w corr
         PVCoordinates pvMODiau76Wcorr =
             new PVCoordinates(new Vector3D(5094028.3745, 6127870.8164, 6380248.5164),
                               new Vector3D(-4746.263052, 786.014045, 5531.790562));
- 
+
         checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76Wcorr), 1.9, 1.2e-3);
         checkPV(pvTODiau76, tt.transformPVCoordinates(pvMODiau76), 2.5, 1.5e-3);
-
         checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76), 6.8e-5, 6.0e-7);
         checkPV(pvTODiau76, tf.transformPVCoordinates(pvMODiau76Wcorr), 0.90615, 7.4e-4);
-
-//        final Transform transform = Frame.getMEME(true).getTransformTo(Frame.getMEME(false), t0);
-//        final double error = transform.getRotation().getAngle() * 648000 / Math.PI;
-//        System.out.println("\nError : " + error + "\"");
 
     }
 
     @Test
     public void testAASReferenceGEO() throws OrekitException {
-
-//        J2000 = 2451545.5
-//        eps0  = iauObl80(2454388.5);
-//        vvd(eps0, 0.4090751347643816218, 1e-14, "iauObl80", "", status);
-//            moe = 0.40907513475969354
-//        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2844),
-//                                           TimeComponents.H00,
-//                                           UTCScale.getInstance());
-        
-//        iauNut80(2453736.5, 0.0, &dpsi, &deps);
-//        vvd(dpsi, -0.9643658353226563966e-5, 1e-13, "iauNut80", "dpsi", status);
-//           dpsi = -0.964296426822596E-5
-//        vvd(deps,  0.4060051006879713322e-4, 1e-13, "iauNut80", "deps", status);
-//            deps = 0.4060070455616641E-4
-//        AbsoluteDate t0 = new AbsoluteDate(new DateComponents(2192),
-//                                           TimeComponents.H00,
-//                                           UTCScale.getInstance());
 
         // this reference test has been extracted from the following paper:
         // Implementation Issues Surrounding the New IAU Reference Systems for Astrodynamics
@@ -108,15 +86,15 @@ public class TEMEFrameTest {
         Transform tt = FrameFactory.getMEME(true).getTransformTo(FrameFactory.getTEME(true), t0);
         Transform tf = FrameFactory.getMEME(false).getTransformTo(FrameFactory.getTEME(false), t0);
 
-//TOD iau76
+        //TOD iau76
         PVCoordinates pvTODiau76 =
             new PVCoordinates(new Vector3D(-40577427.7501, -11500096.1306, 10293.2583),
                               new Vector3D(837.552338, -2957.524176, -0.928772));
-//MOD iau76
+        //MOD iau76
         PVCoordinates pvMODiau76 =
             new PVCoordinates(new Vector3D(-40576822.6385, -11502231.5013, 9738.2304),
                               new Vector3D(837.708020, -2957.480118, -0.814275));
-//MOD iau76 w corr
+        //MOD iau76 w corr
         PVCoordinates pvMODiau76Wcorr =
             new PVCoordinates(new Vector3D(-40576822.6395, -11502231.5015, 9733.7842),
                               new Vector3D(837.708020, -2957.480117, -0.814253));
@@ -143,7 +121,6 @@ public class TEMEFrameTest {
         AbsoluteDate start = new AbsoluteDate(2002, 11, 11, 0, 0, 0.0, TAIScale.getInstance());
         AbsoluteDate end   = new AbsoluteDate(2002, 11, 15, 6, 0, 0.0, TAIScale.getInstance());
         double maxError = 0.0;
-//        System.out.println("        TAI date          Error (arcseconds)");
         for (AbsoluteDate date = start;
              date.compareTo(end) < 0;
              date = new AbsoluteDate(date, 60)) {
@@ -151,9 +128,7 @@ public class TEMEFrameTest {
                 interpolatingFrame.getTransformTo(nonInterpolatingFrame, date);
             final double error = transform.getRotation().getAngle() * 648000 / Math.PI;
             maxError = Math.max(maxError, error);
-//            System.out.println(date.toString(TAIScale.getInstance()) + "   " + error);
         }
-//        System.out.println("\nError max : " + maxError);
 
         assertTrue(maxError < 7.2e-11);
 
