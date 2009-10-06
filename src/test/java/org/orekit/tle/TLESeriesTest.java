bee697940b5e0b37dd2ff53087c0af96b68f97de
Luc Maisonobe <luc@orekit.org>
2009-10-06 10:20:31 +0000

    improved test coverage

--- src/test/java/org/orekit/tle/TLESeriesTest.java
+++ src/test/java/org/orekit/tle/TLESeriesTest.java
@@ -18,6 +18,7 @@ package org.orekit.tle;
 
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -32,6 +33,21 @@ import org.orekit.time.TimeScalesFactory;
 
 public class TLESeriesTest {
 
+    @Deprecated
+    @Test(expected=OrekitException.class)
+    public void testDeprecated() throws IOException, OrekitException {
+        InputStream in =
+            TLETest.class.getResourceAsStream("/regular-data/tle/spot-5.tle");
+        TLESeries series = new TLESeries(in);
+        series.loadTLEData(22076);
+    }
+
+    @Test(expected=OrekitException.class)
+    public void testNoData() throws IOException, OrekitException {
+        TLESeries series = new TLESeries("^inexistant\\.tle$", false);
+        series.loadTLEData();
+    }
+
     @Test(expected=OrekitException.class)
     public void testNoTopexPoseidonNumber() throws IOException, OrekitException {
         TLESeries series = new TLESeries("^spot-5\\.tle$", false);
@@ -42,6 +58,24 @@ public class TLESeriesTest {
     public void testNoTopexPoseidonLaunchElements() throws IOException, OrekitException {
         TLESeries series = new TLESeries("^spot-5\\.tle$", false);
         series.loadTLEData(1992, 52, "A");
+    }
+
+    @Test
+    public void testSpot5WithExtraLines() throws IOException, OrekitException {
+        TLESeries series = new TLESeries("^spot-5-with-extra-lines\\.tle$", true);
+        series.loadTLEData(-1);
+        Assert.assertEquals(27421, series.getFirst().getSatelliteNumber());
+        Assert.assertEquals(0,
+                            series.getFirstDate().durationFrom(new AbsoluteDate(new DateComponents(2002, 5, 4),
+                                                                         new TimeComponents(11, 45, 15.695136),
+                                                                         TimeScalesFactory.getUTC())),
+                                                                         1e-13);
+        System.out.println(series.getLastDate());
+               Assert.assertEquals(0,
+                            series.getLastDate().durationFrom(new AbsoluteDate(new DateComponents(2002, 5, 4),
+                                                                        new TimeComponents(19, 10, 59.114784),
+                                                                        TimeScalesFactory.getUTC())),
+                                                                        1e-13);
     }
 
     @Test
@@ -58,6 +92,9 @@ public class TLESeriesTest {
         series.loadTLEData(-1, -1, null);
         Assert.assertEquals(27421, series.getFirst().getSatelliteNumber());
 
+        series.loadTLEData(2002, -1, null);
+        Assert.assertEquals(27421, series.getFirst().getSatelliteNumber());
+
         series.loadTLEData(2002, 21, "A");
         Assert.assertEquals(27421, series.getFirst().getSatelliteNumber());
         Assert.assertEquals(2002, series.getFirst().getLaunchYear());
@@ -70,14 +107,14 @@ public class TLESeriesTest {
 
         Assert.assertEquals(0,
                      series.getFirstDate().durationFrom(new AbsoluteDate(new DateComponents(2002, 05, 04),
-                                                                  new TimeComponents(11, 45, 15.695),
+                                                                  new TimeComponents(11, 45, 15.695136),
                                                                   TimeScalesFactory.getUTC())),
-                                                                  1e-3);
+                                                                  1e-13);
         Assert.assertEquals(0,
                      series.getLastDate().durationFrom(new AbsoluteDate(new DateComponents(2002, 06, 24),
-                                                                 new TimeComponents(18, 12, 44.592),
+                                                                 new TimeComponents(18, 12, 44.591616001),
                                                                  TimeScalesFactory.getUTC())),
-                                                                 1e-3);
+                                                                 1e-13);
 
         AbsoluteDate mid = new AbsoluteDate(new DateComponents(2002, 06, 02),
                                             new TimeComponents(11, 12, 15),
