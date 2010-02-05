b785069044a2001de7f3a1c6ff3604d25ad430cf
Luc Maisonobe <luc@orekit.org>
2010-02-05 15:30:40 +0000

    added a relative constructor for PVCoordinates javadocs improvements

--- src/main/java/org/orekit/attitudes/GroundPointing.java
+++ src/main/java/org/orekit/attitudes/GroundPointing.java
@@ -65,11 +65,11 @@ public abstract class GroundPointing implements AttitudeLaw {
 
     /** Get target point in body frame.
      * @param date date when the target point shall be computed
-     * @param pv position/velocity of the point
-     * @param frame frame in which the point shall be computed
+     * @param pv position/velocity of the satellite
+     * @param frame frame in which the point is provided
+     * @return target in body frame
      * @throws OrekitException if some specific error occurs,
      * such as no target reached
-     * @return target in body frame
      */
     protected abstract PVCoordinates getTargetInBodyFrame(AbsoluteDate date,
                                                           PVCoordinates pv,
@@ -77,14 +77,14 @@ public abstract class GroundPointing implements AttitudeLaw {
         throws OrekitException;
 
     /** Compute the target ground point at given date in given frame.
+     * <p>User should check that position/velocity and frame are consistent.</p>
      * @param date date when the point shall be computed
-     * @param pv position-velocity of the point
-     * @param frame frame in which the point shall be computed
-     * @throws OrekitException if some specific error occurs
+     * @param pv position-velocity of the satellite
+     * @param frame frame in which the satellite coordinates are provided
+     * and in which the target ground point should be computed
      * @return observed ground point position/velocity in given frame
-     *
-     * <p>User should check that position/velocity and frame is consistent with given frame.
-     * </p>
+     * @throws OrekitException if some specific error occurs,
+     * such as no target reached
      */
     public PVCoordinates getObservedGroundPoint(final AbsoluteDate date,
                                                 final PVCoordinates pv,
@@ -102,20 +102,18 @@ public abstract class GroundPointing implements AttitudeLaw {
     }
 
     /** Compute the system state at given date in given frame.
+     * <p>User should check that position/velocity and frame are consistent.</p>
      * @param date date when system state shall be computed
      * @param pv satellite position/velocity in given frame
      * @param frame the frame in which pv is defined
      * @return satellite attitude state at date
      * @throws OrekitException if some specific error occurs
-     *
-     * <p>User should check that position/velocity and frame is consistent with given frame.
-     * </p>
      */
     public Attitude getState(final AbsoluteDate date, final PVCoordinates pv, final Frame frame)
         throws OrekitException {
 
         // Construction of the satellite-target position/velocity vector
-        final PVCoordinates pointing =  new PVCoordinates(1, getObservedGroundPoint(date, pv, frame), -1, pv);
+        final PVCoordinates pointing = new PVCoordinates(pv, getObservedGroundPoint(date, pv, frame));
         final Vector3D pos = pointing.getPosition();
         final Vector3D vel = pointing.getVelocity();
 
@@ -130,8 +128,7 @@ public abstract class GroundPointing implements AttitudeLaw {
         final Rotation rot = new Rotation(pos, pv.getVelocity(), Vector3D.PLUS_K, Vector3D.PLUS_I);
 
         // Attitude spin
-        final Vector3D spin = new Vector3D(1 / Vector3D.dotProduct(pos, pos),
-                                           Vector3D.crossProduct(pos, vel));
+        final Vector3D spin = new Vector3D(1 / pos.getNormSq(), Vector3D.crossProduct(pos, vel));
 
         return new Attitude(frame, rot, rot.applyTo(spin));
     }
