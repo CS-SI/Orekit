8ca60a12b45d3799146939b74f02a4ac515518d4
Pascal Parraud <pascal@orekit.org>
2010-06-01 16:50:01 +0000

    added a new TimerDetector and fixed some documentation

---
+++ src/main/java/org/orekit/propagation/events/TimerDetector.java
@@ -0,0 +1,144 @@
+/* Copyright 2002-2010 CS Communication & Systèmes
+ * Licensed to CS Communication & Systèmes (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.propagation.events;
+
+import org.orekit.errors.OrekitException;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeStamped;
+
+/** Finder for date events triggered by a timer.
+ * <p>The trigger date for the timer can be reset.</p>
+ * <p>The default implementation behavior is to {@link EventDetector#STOP
+ * stop} propagation when the timer sets the event off. This can be changed by
+ * overriding the {@link #eventOccurred(SpacecraftState, boolean) eventOccurred}
+ * method in a derived class.</p>
+ * @see org.orekit.propagation.Propagator#addEventDetector(EventDetector)
+ * @author Pascal Parraud
+ * @version $Revision$ $Date$
+ */
+public class TimerDetector extends AbstractDetector implements TimeStamped {
+
+    /** Serializable UID. */
+    private static final long serialVersionUID = -334171965326514174L;
+
+    /** Flag the current way of g with respect to time. */
+    private boolean gIncrease = true;
+
+    /** Flag for date reset. */
+    private boolean reset = true;
+
+    /** Previous trigger date. */
+    private AbsoluteDate previous;
+
+    /** Current trigger date. */
+    private AbsoluteDate trigger;
+
+    /** Current event target date. */
+    private AbsoluteDate target;
+
+    /** Timer duration. */
+    private final double dt;
+
+    /** Build a new instance.
+     * @param triggerDate trigger date
+     * @param duration timer duration from trigger date
+     */
+    public TimerDetector(final AbsoluteDate triggerDate, final double duration) {
+        super(10.e9, 10.e-10);
+        previous = null;
+        trigger = null;
+        target = null;
+        dt = duration;
+        resetDate(triggerDate);
+    }
+
+    /** Build a new instance.
+     *  <p>The trigger date is set to infinity, so this is a pending timer.</p>
+     *  <p>It will be activated whith a call to {@link #resetDate(AbsoluteDate)}.</p>
+     * @param duration timer duration
+     */
+    public TimerDetector(final double duration) {
+        this(null, duration);
+    }
+
+    /** Handle a timed event and choose what to do next.
+     * <p>The default implementation behavior is to {@link
+     * EventDetector#STOP stop} propagation at target date occurrence.</p>
+     * @param s the current state information : date, kinematics, attitude
+     * @param increasing if true, the value of the switching function increases
+     * when times increases around event.
+     * @return {@link #STOP}
+     * @exception OrekitException if some specific error occurs
+     */
+    public int eventOccurred(final SpacecraftState s, final boolean increasing)
+        throws OrekitException {
+        return STOP;
+    }
+
+    /** Compute the value of the switching function.
+     * This function measures the difference between the current and the target date.
+     * @param s the current state information: date, kinematics, attitude
+     * @return value of the switching function
+     * @exception OrekitException if some specific error occurs
+     */
+    public double g(final SpacecraftState s) throws OrekitException {
+        final AbsoluteDate gDate = s.getDate();
+        if (reset) {
+            if (previous == null) {
+                gIncrease = true;
+            }
+            else if (gDate.durationFrom(previous) * gDate.durationFrom(target) > 0) {
+                gIncrease = !gIncrease;
+            }
+            reset = false;
+        }
+        return gIncrease ? gDate.durationFrom(target) : target.durationFrom(gDate);
+    }
+
+    /** Get the trigger date.
+     * @return trigger date
+     */
+    public AbsoluteDate getDate() {
+        return trigger;
+    }
+
+    /** Get the timer duration.
+     * @return timer duration
+     */
+    public double getTimerDuration() {
+        return dt;
+    }
+
+    /** Reset the trigger date.
+     * @param triggerDate trigger date
+     */
+    public void resetDate(final AbsoluteDate triggerDate) {
+        if (target != null) {
+            previous = target;
+        }
+        if (triggerDate != null) {
+            trigger = triggerDate;
+            target = new AbsoluteDate(triggerDate, dt);
+        } else {
+            trigger = AbsoluteDate.FUTURE_INFINITY;
+            target = AbsoluteDate.FUTURE_INFINITY;
+        }
+        reset = true;
+    }
+
+}
---
+++ src/test/java/org/orekit/propagation/events/TimerDetectorTest.java
@@ -0,0 +1,123 @@
+package org.orekit.propagation.events;
+
+import org.apache.commons.math.geometry.Vector3D;
+import org.apache.commons.math.ode.nonstiff.AdaptiveStepsizeIntegrator;
+import org.apache.commons.math.ode.nonstiff.DormandPrince853Integrator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.orekit.Utils;
+import org.orekit.errors.OrekitException;
+import org.orekit.frames.FramesFactory;
+import org.orekit.orbits.EquinoctialOrbit;
+import org.orekit.orbits.Orbit;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.numerical.NumericalPropagator;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.PVCoordinates;
+
+public class TimerDetectorTest {
+
+    private int                  evtno;
+    private double               mu;
+    private double               dt;
+    private AbsoluteDate         iniDate;
+    private AbsoluteDate         triggerDate;
+    private SpacecraftState      initialState;
+    private TimerDetector        timerDetector;
+    private DateDetector         dateDetector;
+    private NumericalPropagator  propagator;
+
+    @Test
+    public void testSimpleTimer() throws OrekitException {
+    	triggerDate = iniDate.shiftedBy(dt);
+    	timerDetector = new TimerDetector(triggerDate, dt) {
+            private static final long serialVersionUID = 1L;
+			public int eventOccurred(SpacecraftState s, boolean increasing) throws OrekitException {
+		        return STOP;
+            }
+        };
+
+        propagator.addEventDetector(timerDetector);
+        final SpacecraftState finalState = propagator.propagate(iniDate.shiftedBy(100.*dt));
+
+        Assert.assertEquals(2.0*dt, finalState.getDate().durationFrom(iniDate), 1.0e-10);
+    }
+
+    @Test
+    public void testEmbeddedTimer() throws OrekitException {
+        timerDetector = new TimerDetector(dt) {
+            private static final long serialVersionUID = 1L;
+			public int eventOccurred(SpacecraftState s, boolean increasing) throws OrekitException {
+		        return STOP;
+            }
+        };
+    	dateDetector = new DateDetector(iniDate.shiftedBy(2.*dt)) {
+            private static final long serialVersionUID = 1L;
+			public int eventOccurred(SpacecraftState s, boolean increasing) throws OrekitException {
+  		        timerDetector.resetDate(s.getDate());
+		        return CONTINUE;
+            }
+        };
+        propagator.addEventDetector(dateDetector);
+        propagator.addEventDetector(timerDetector);
+        final SpacecraftState finalState = propagator.propagate(iniDate.shiftedBy(100.*dt));
+
+        Assert.assertEquals(3.*dt, finalState.getDate().durationFrom(iniDate), 1.0e-10);
+    }
+
+    @Test
+    public void testAutoEmbeddedTimer() throws OrekitException {
+        evtno = 0;
+    	triggerDate = iniDate;
+    	timerDetector = new TimerDetector(triggerDate, dt) {
+            private static final long serialVersionUID = 1L;
+			public int eventOccurred(SpacecraftState s, boolean increasing) throws OrekitException {
+  		        this.resetDate(s.getDate());
+  		        ++evtno;
+		        return CONTINUE;
+            }
+        };
+        propagator.addEventDetector(timerDetector);
+        propagator.propagate(iniDate.shiftedBy(100.*dt));
+
+        Assert.assertEquals(100, evtno);
+    }
+
+    @Before
+    public void setUp() {
+        Utils.setDataRoot("regular-data");
+        mu = 3.9860047e14;
+        dt = 60.;
+        final Vector3D position  = new Vector3D(-6142438.668, 3492467.560, -25767.25680);
+        final Vector3D velocity  = new Vector3D(505.8479685, 942.7809215, 7435.922231);
+        iniDate = new AbsoluteDate(1969, 7, 28, 4, 0, 0.0, TimeScalesFactory.getTT());
+        final Orbit orbit = new EquinoctialOrbit(new PVCoordinates(position,  velocity),
+                                                 FramesFactory.getEME2000(), iniDate, mu);
+        initialState = new SpacecraftState(orbit);
+        double[] absTolerance = {
+            0.001, 1.0e-9, 1.0e-9, 1.0e-6, 1.0e-6, 1.0e-6, 0.001
+        };
+        double[] relTolerance = {
+            1.0e-7, 1.0e-4, 1.0e-4, 1.0e-7, 1.0e-7, 1.0e-7, 1.0e-7
+        };
+        AdaptiveStepsizeIntegrator integrator =
+            new DormandPrince853Integrator(0.001, 1000, absTolerance, relTolerance);
+        integrator.setInitialStepSize(60);
+        propagator = new NumericalPropagator(integrator);
+        propagator.setInitialState(initialState);
+    }
+
+    @After
+    public void tearDown() {
+        iniDate = null;
+        initialState = null;
+        propagator = null;
+        triggerDate = null;
+        timerDetector = null;
+        dateDetector = null;
+    }
+
+}
