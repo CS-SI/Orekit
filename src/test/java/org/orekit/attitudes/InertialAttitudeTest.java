74d5fa30c9e6a3927c9a65d4fa525902daba81ea
Luc Maisonobe <luc@orekit.org>
2010-06-08 16:08:48 +0000

    improved test

--- src/test/java/org/orekit/attitudes/InertialAttitudeTest.java
+++ src/test/java/org/orekit/attitudes/InertialAttitudeTest.java
@@ -90,6 +90,18 @@ public class InertialAttitudeTest {
         SpacecraftState s0     = propagator.propagate(date);
         SpacecraftState sPlus  = propagator.propagate(date.shiftedBy(h));
 
+        // check spin is consistent with attitude evolution
+        double errorAngleMinus     = Rotation.distance(sMinus.shiftedBy(h).getAttitude().getRotation(),
+                                                       s0.getAttitude().getRotation());
+        double evolutionAngleMinus = Rotation.distance(sMinus.getAttitude().getRotation(),
+                                                       s0.getAttitude().getRotation());
+        Assert.assertTrue(errorAngleMinus <= 1.0e-6 * evolutionAngleMinus);
+        double errorAnglePlus      = Rotation.distance(s0.getAttitude().getRotation(),
+                                                       sPlus.shiftedBy(-h).getAttitude().getRotation());
+        double evolutionAnglePlus  = Rotation.distance(s0.getAttitude().getRotation(),
+                                                       sPlus.getAttitude().getRotation());
+        Assert.assertTrue(errorAnglePlus <= 1.0e-6 * evolutionAnglePlus);
+
         // compute spin axis using finite differences
         Rotation rMinus = sMinus.getAttitude().getRotation();
         Rotation rPlus  = sPlus.getAttitude().getRotation();
