fbc9401ea9da07ba356593d01e8f9903d6a84248
Luc Maisonobe <luc@orekit.org>
2010-11-09 14:42:38 +0000

    removed test offset betwen TAI and TCB, they are NOT related at reference time added a symmetry test

--- src/test/java/org/orekit/time/TCBScaleTest.java
+++ src/test/java/org/orekit/time/TCBScaleTest.java
@@ -2,28 +2,43 @@ package org.orekit.time;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.orekit.utils.Constants;
 
 
 public class TCBScaleTest {
 
     @Test
     public void testReference() {
-        TimeScale scale = TimeScalesFactory.getTCB();
-        Assert.assertEquals("TCB", scale.toString());
-        AbsoluteDate refTCB = new AbsoluteDate("1977-01-01T00:00:32.184", scale);
-        AbsoluteDate refTAI = new AbsoluteDate("1977-01-01T00:00:00.000", TimeScalesFactory.getTAI());
-        Assert.assertEquals(0.0, refTCB.durationFrom(refTAI), 6.0e-5);
+        TimeScale tcb = TimeScalesFactory.getTCB();
+        TimeScale tdb = TimeScalesFactory.getTDB();
+        Assert.assertEquals("TCB", tcb.toString());
+        AbsoluteDate refTCB = new AbsoluteDate("1977-01-01T00:00:32.184", tcb);
+        AbsoluteDate refTDB = new AbsoluteDate("1977-01-01T00:00:32.184", tdb);
+        Assert.assertEquals(0.0, refTCB.durationFrom(refTDB), 1.0e-12);
     }
 
     @Test
     public void testRate() {
-        TimeScale scale = TimeScalesFactory.getTCB();
+        TimeScale tcb = TimeScalesFactory.getTCB();
+        TimeScale tdb = TimeScalesFactory.getTDB();
         AbsoluteDate t0 = AbsoluteDate.J2000_EPOCH;
         for (double deltaT = 1.0; deltaT < 10.0; deltaT += 0.3) {
             AbsoluteDate t1 = t0.shiftedBy(deltaT);
-            double tdbRate = t1.offsetFrom(t0, TimeScalesFactory.getTDB()) / deltaT;
-            double tcbRate = t1.offsetFrom(t0, scale) / deltaT;
+            double tdbRate = t1.offsetFrom(t0, tdb) / deltaT;
+            double tcbRate = t1.offsetFrom(t0, tcb) / deltaT;
             Assert.assertEquals(tdbRate + 1.550505e-8, tcbRate, 1.0e-14);
+        }
+    }
+
+    @Test
+    public void testSymmetry() {
+        TimeScale scale = TimeScalesFactory.getTCB();
+        for (double dt = -10000; dt < 10000; dt += 123.456789) {
+            AbsoluteDate date = AbsoluteDate.J2000_EPOCH.shiftedBy(dt * Constants.JULIAN_DAY);
+            double dt1 = scale.offsetFromTAI(date);
+            DateTimeComponents components = date.getComponents(scale);
+            double dt2 = scale.offsetToTAI(components.getDate(), components.getTime());
+            Assert.assertEquals( 0.0, dt1 + dt2, 1.0e-10);
         }
     }
 
