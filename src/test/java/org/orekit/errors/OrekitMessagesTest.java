b9178bd6ed473a3c56fa6f3eb6ab1758a7e41e45
Luc Maisonobe <luc@orekit.org>
2012-11-16 13:49:05 +0100

    Fixed a test after number of error messages has changed.

--- src/test/java/org/orekit/errors/OrekitMessagesTest.java
+++ src/test/java/org/orekit/errors/OrekitMessagesTest.java
@@ -30,7 +30,7 @@ public class OrekitMessagesTest {
 
     @Test
     public void testMessageNumber() {
-        Assert.assertEquals(119, OrekitMessages.values().length);
+        Assert.assertEquals(120, OrekitMessages.values().length);
     }
 
     @Test
