9b32ba4ce5b74bac921600a8fafbef9f706c8eb8
Luc Maisonobe <luc@orekit.org>
2013-02-22 09:29:32 +0100

    Use new partial derivatives framework in Newtonian model.

--- src/main/java/org/orekit/forces/gravity/NewtonianAttraction.java
+++ src/main/java/org/orekit/forces/gravity/NewtonianAttraction.java
@@ -16,21 +16,24 @@
  */
 package org.orekit.forces.gravity;
 
+import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.apache.commons.math3.ode.AbstractParameterizable;
 import org.apache.commons.math3.util.FastMath;
 import org.orekit.errors.OrekitException;
 import org.orekit.forces.ForceModel;
+import org.orekit.frames.Frame;
 import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.events.EventDetector;
-import org.orekit.propagation.numerical.AccelerationJacobiansProvider;
 import org.orekit.propagation.numerical.TimeDerivativesEquations;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.utils.RotationDS;
+import org.orekit.utils.Vector3DDS;
 
 /** Force model for Newtonian central body attraction.
  * @author Luc Maisonobe
  */
-public class NewtonianAttraction extends AbstractParameterizable
-                                 implements AccelerationJacobiansProvider, ForceModel {
+public class NewtonianAttraction extends AbstractParameterizable implements ForceModel {
 
     /** Name of the single parameter of this model: the central attraction coefficient. */
     public static final String CENTRAL_ATTRACTION_COEFFICIENT = "central attraction coefficient";
@@ -47,45 +50,27 @@ public class NewtonianAttraction extends AbstractParameterizable
     }
 
     /** {@inheritDoc} */
-    public void addDAccDState(final SpacecraftState s,
-                              final double[][] dAccdPos, final double[][] dAccdVel, final double[] dAccdM)
+    public Vector3DDS accelerationDerivatives(final AbsoluteDate date, final Frame frame,
+                                              final Vector3DDS position, final Vector3DDS velocity,
+                                              final RotationDS rotation, final DerivativeStructure mass)
         throws OrekitException {
 
-        final Vector3D position     = s.getPVCoordinates().getPosition();
-        final double r2             = position.getNormSq();
-        final Vector3D acceleration = new Vector3D(-mu / (r2 * FastMath.sqrt(r2)), position);
-
-        final double x2 = position.getX() * position.getX();
-        final double y2 = position.getY() * position.getY();
-        final double z2 = position.getZ() * position.getZ();
-        final double xy = position.getX() * position.getY();
-        final double yz = position.getY() * position.getZ();
-        final double zx = position.getZ() * position.getX();
-        final double prefix = -Vector3D.dotProduct(acceleration, position) / (r2 * r2);
-
-        // the only non-null contribution for this force is on dAcc/dPos
-        dAccdPos[0][0] += prefix * (2 * x2 - y2 - z2);
-        dAccdPos[0][1] += prefix * 3 * xy;
-        dAccdPos[0][2] += prefix * 3 * zx;
-        dAccdPos[1][0] += prefix * 3 * xy;
-        dAccdPos[1][1] += prefix * (2 * y2 - z2 - x2);
-        dAccdPos[1][2] += prefix * 3 * yz;
-        dAccdPos[2][0] += prefix * 3 * zx;
-        dAccdPos[2][1] += prefix * 3 * yz;
-        dAccdPos[2][2] += prefix * (2 * z2 - x2 - y2);
+        final DerivativeStructure r2 = position.getNormSq();
+        return new Vector3DDS(r2.sqrt().multiply(r2).reciprocal().multiply(-mu), position);
 
     }
 
     /** {@inheritDoc} */
-    public void addDAccDParam(final SpacecraftState s, final String paramName, final double[] dAccdParam)
+    public Vector3DDS accelerationDerivatives(final SpacecraftState s, final String paramName)
         throws OrekitException {
+
         complainIfNotSupported(paramName);
-        final Vector3D position = s.getPVCoordinates().getPosition();
-        final double r2         = position.getNormSq();
-        final double factor     = -1.0 / (r2 * FastMath.sqrt(r2));
-        dAccdParam[0] += factor * position.getX();
-        dAccdParam[1] += factor * position.getY();
-        dAccdParam[2] += factor * position.getZ();
+
+        final Vector3D            position = s.getPVCoordinates().getPosition();
+        final double              r2       = position.getNormSq();
+        final DerivativeStructure muds     = new DerivativeStructure(1, 1, 0, mu);
+        return new Vector3DDS(muds.divide(-r2 * FastMath.sqrt(r2)), position);
+
     }
 
     /** Get the central attraction coefficient &mu;.
