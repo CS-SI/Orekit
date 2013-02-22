86768b7ec94aeaea212381f150d405736ba8e26f
Luc Maisonobe <luc@orekit.org>
2013-02-22 20:58:06 +0100

    Added test for drag with box and solar array spacecraft shape.

--- src/test/java/org/orekit/forces/drag/DragForceTest.java
+++ src/test/java/org/orekit/forces/drag/DragForceTest.java
@@ -27,6 +27,7 @@ import org.orekit.bodies.CelestialBodyFactory;
 import org.orekit.bodies.OneAxisEllipsoid;
 import org.orekit.errors.OrekitException;
 import org.orekit.forces.AbstractForceModelTest;
+import org.orekit.forces.BoxAndSolarArraySpacecraft;
 import org.orekit.forces.SphericalSpacecraft;
 import org.orekit.frames.FramesFactory;
 import org.orekit.orbits.CartesianOrbit;
@@ -47,7 +48,7 @@ import org.orekit.utils.PVCoordinates;
 public class DragForceTest extends AbstractForceModelTest {
 
     @Test
-    public void testParameterDerivative() throws OrekitException {
+    public void testParameterDerivativeSphere() throws OrekitException {
 
         final Vector3D pos = new Vector3D(6.46885878304673824e+06, -1.88050918456274318e+06, -1.32931592294715829e+04);
         final Vector3D vel = new Vector3D(2.14718074509906819e+03, 7.38239351251748485e+03, -1.14097953925384523e+01);
@@ -69,7 +70,7 @@ public class DragForceTest extends AbstractForceModelTest {
     }
 
     @Test
-    public void testStateJacobian()
+    public void testStateJacobianSphere()
         throws OrekitException {
 
         // initialization
@@ -100,6 +101,65 @@ public class DragForceTest extends AbstractForceModelTest {
 
         checkStateJacobian(propagator, state0, date.shiftedBy(3.5 * 3600.0),
                            4, 1e3, tolerances[0], 3.0e-4);
+
+    }
+
+    @Test
+    public void testParameterDerivativeBox() throws OrekitException {
+
+        final Vector3D pos = new Vector3D(6.46885878304673824e+06, -1.88050918456274318e+06, -1.32931592294715829e+04);
+        final Vector3D vel = new Vector3D(2.14718074509906819e+03, 7.38239351251748485e+03, -1.14097953925384523e+01);
+        final SpacecraftState state =
+                new SpacecraftState(new CartesianOrbit(new PVCoordinates(pos, vel),
+                                                       FramesFactory.getGCRF(),
+                                                       new AbsoluteDate(2003, 3, 5, 0, 24, 0.0, TimeScalesFactory.getTAI()),
+                                                       Constants.EIGEN5C_EARTH_MU));
+
+        final DragForce forceModel =
+                new DragForce(new HarrisPriester(CelestialBodyFactory.getSun(),
+                                                 new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                                                                      Constants.WGS84_EARTH_FLATTENING,
+                                                                      FramesFactory.getITRF2008())),
+                              new BoxAndSolarArraySpacecraft(1.5, 2.0, 1.8, CelestialBodyFactory.getSun(), 20.0,
+                                                             Vector3D.PLUS_J, 1.2, 0.7, 0.2));
+
+        checkParameterDerivative(state, forceModel, DragSensitive.DRAG_COEFFICIENT, 1.0e-4, 5.0e-13);
+
+    }
+
+    @Test
+    public void testStateJacobianBox()
+        throws OrekitException {
+
+        // initialization
+        AbsoluteDate date = new AbsoluteDate(new DateComponents(2003, 03, 01),
+                                             new TimeComponents(13, 59, 27.816),
+                                             TimeScalesFactory.getUTC());
+        double i     = FastMath.toRadians(98.7);
+        double omega = FastMath.toRadians(93.0);
+        double OMEGA = FastMath.toRadians(15.0 * 22.5);
+        Orbit orbit = new KeplerianOrbit(7201009.7124401, 1e-3, i , omega, OMEGA,
+                                         0, PositionAngle.MEAN, FramesFactory.getEME2000(), date,
+                                         Constants.EIGEN5C_EARTH_MU);
+        OrbitType integrationType = OrbitType.CARTESIAN;
+        double[][] tolerances = NumericalPropagator.tolerances(0.01, orbit, integrationType);
+
+        NumericalPropagator propagator =
+                new NumericalPropagator(new DormandPrince853Integrator(1.0e-3, 120,
+                                                                       tolerances[0], tolerances[1]));
+        propagator.setOrbitType(integrationType);
+        final DragForce forceModel =
+                new DragForce(new HarrisPriester(CelestialBodyFactory.getSun(),
+                                                 new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
+                                                                      Constants.WGS84_EARTH_FLATTENING,
+                                                                      FramesFactory.getITRF2008())),
+                              new BoxAndSolarArraySpacecraft(1.5, 2.0, 1.8, CelestialBodyFactory.getSun(), 20.0,
+                                                             Vector3D.PLUS_J, 1.2, 0.7, 0.2));
+        propagator.addForceModel(forceModel);
+        SpacecraftState state0 = new SpacecraftState(orbit);
+
+        checkStateJacobian(propagator, state0, date.shiftedBy(3.5 * 3600.0),
+                           4, 1e3, tolerances[0], 2.0e-3);
 
     }
 
