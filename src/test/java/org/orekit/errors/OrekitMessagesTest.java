c93fb01de1002418674dc51da1301aa849663daa
Luc Maisonobe <luc@orekit.org>
2013-02-28 09:42:52 +0100

    Removed unused error messages.
    These messages will be added back when the support for CCSDS Orbit Datra
    Messages will be finalized.

--- src/test/java/org/orekit/errors/OrekitMessagesTest.java
+++ src/test/java/org/orekit/errors/OrekitMessagesTest.java
@@ -29,7 +29,7 @@ public class OrekitMessagesTest {
 
     @Test
     public void testMessageNumber() {
-        Assert.assertEquals(123, OrekitMessages.values().length);
+        Assert.assertEquals(111, OrekitMessages.values().length);
     }
 
     @Test
