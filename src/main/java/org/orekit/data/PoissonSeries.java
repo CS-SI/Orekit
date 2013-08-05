70747b1b67e1bb54e18c3e3a43bf389527f1e5e4
Luc Maisonobe <luc@orekit.org>
2013-08-05 20:11:04 +0200

    Improved parsing of Poisson series.
    We now check the index of each series term, and allow series without
    polynomial part (such as some series from IERS 2010 conventions).

--- src/main/java/org/orekit/data/PoissonSeries.java
+++ src/main/java/org/orekit/data/PoissonSeries.java
@@ -87,6 +87,16 @@ public class PoissonSeries implements NutationFunction, Serializable {
             //  2     -523908.04        -544.76    0    0    2   -2    2    0    0    0    0    0    0    0    0    0
             //  3      -90552.22         111.23    0    0    2    0    2    0    0    0    0    0    0    0    0    0
             //  4       82168.76         -27.64    0    0    0    0    2    0    0    0    0    0    0    0    0    0
+            //
+            // or something like:
+            //
+            // ----------------------------------------------------------------------------------------------------------
+            // j = 0  Number  of terms = 1037
+            // ----------------------------------------------------------------------------------------------------------
+            //     i         B"_i            B_i      l    l'   F    D   Om  L_Me L_Ve  L_E L_Ma  L_J L_Sa  L_U L_Ne  p_A
+            // ----------------------------------------------------------------------------------------------------------
+            //     1        1537.70     9205233.10    0    0    0    0    1    0    0    0    0    0    0    0    0    0
+            //     2        -458.70      573033.60    0    0    2   -2    2    0    0    0    0    0    0    0    0    0
             final Pattern seriesHeaderPattern =
                 Pattern.compile("^\\p{Space}*j\\p{Space}*=\\p{Space}*(\\p{Digit}+)" +
                                 ".*=\\p{Space}*(\\p{Digit}+)\\p{Space}*$");
@@ -95,61 +105,66 @@ public class PoissonSeries implements NutationFunction, Serializable {
             // setup the reader
             final BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
             String line = reader.readLine();
-            int lineNumber = 1;
-
-            // look for the polynomial part
-            while (line != null) {
-                if (parsePolynomial(termPattern.matcher(line), polyFactor)) {
-                    // we have parsed the polynomial part
-                    line = null;
-                } else {
-                    // we are still in the header
-                    line = reader.readLine();
-                    ++lineNumber;
-                }
-            }
-            if (coefficients == null) {
-                throw new OrekitException(OrekitMessages.NOT_A_SUPPORTED_IERS_DATA_FILE, name);
-            }
-
-            line = reader.readLine();
-            ++lineNumber;
-
-            // look for the non-polynomial part
+            int lineNumber    = 1;
+            int expectedIndex = 1;
             final List<SeriesTerm[]> array = new ArrayList<SeriesTerm[]>();
+
             while (line != null) {
+
                 final int nTerms = parseSeriesHeader(seriesHeaderPattern.matcher(line),
                                                      array.size(), name, lineNumber);
                 if (nTerms >= 0) {
+
                     // we have found a non-polynomial series
 
-                    // skip blank lines
+                    if (coefficients == null) {
+                        // since non-polynomial part starts after polynomial part,
+                        // getting here means there are no coefficients at all
+                        coefficients = new double[0];
+                    }
+
+                    // skip sub-headers lines
                     line = reader.readLine();
                     ++lineNumber;
-                    while ((line != null) && (line.trim().length() == 0)) {
+                    while (line != null) {
+                        final String fields[] = line.trim().split(" +");
+                        if (fields.length > 0 && Integer.toString(expectedIndex).equals(fields[0])) {
+                            // we have found the first line we are interested in
+                            break;
+                        }
                         line = reader.readLine();
                         ++lineNumber;
                     }
 
-                    // read the terms of the current serie
+                    // read the terms of the current series
                     final SeriesTerm[] serie = new SeriesTerm[nTerms];
                     for (int i = 0; i < nTerms; ++i) {
-                        serie[i] = parseSeriesTerm(line, nonPolyFactor, name, lineNumber);
+                        serie[i] =
+                                parseSeriesTerm(line, nonPolyFactor, expectedIndex, name, lineNumber);
                         line = reader.readLine();
                         ++lineNumber;
+                        ++expectedIndex;
                     }
 
-                    // the serie has been completed, store it
+                    // the series has been completed, store it
                     array.add(serie);
 
                 } else {
-                    // we are still in the intermediate lines
+
+                    if (coefficients == null) {
+                        // look for the polynomial part
+                        coefficients = parsePolynomial(termPattern.matcher(line), polyFactor);
+                    }
+
+                    // we are still in the header
                     line = reader.readLine();
                     ++lineNumber;
+
                 }
+
             }
 
-            if (array.isEmpty()) {
+            if (coefficients == null || array.isEmpty()) {
                 throw new OrekitException(OrekitMessages.NOT_A_SUPPORTED_IERS_DATA_FILE, name);
             }
 
@@ -165,13 +180,13 @@ public class PoissonSeries implements NutationFunction, Serializable {
     /** Parse a polynomial description line.
      * @param termMatcher matcher for the polynomial terms
      * @param polyFactor multiplicative factor to use for polynomial coefficients
-     * @return true if the line was parsed successfully
+     * @return parsed coefficients, or null if no coefficients found
      */
-    private boolean parsePolynomial(final Matcher termMatcher, final double polyFactor) {
+    private double[] parsePolynomial(final Matcher termMatcher, final double polyFactor) {
 
         // parse the polynomial one polynomial term after the other
         if (!termMatcher.lookingAt()) {
-            return false;
+            return null;
         }
 
         // store the concatenated sign, integer and fractional parts of the monomial coefficient
@@ -181,12 +196,12 @@ public class PoissonSeries implements NutationFunction, Serializable {
         } while (termMatcher.find());
 
         // parse the coefficients
-        coefficients = new double[coeffs.size()];
-        for (int i = 0; i < coefficients.length; ++i) {
-            coefficients[i] = polyFactor * Double.parseDouble(coeffs.get(i));
+        final double[] c = new double[coeffs.size()];
+        for (int i = 0; i < c.length; ++i) {
+            c[i] = polyFactor * Double.parseDouble(coeffs.get(i));
         }
 
-        return true;
+        return c;
 
     }
 
@@ -221,13 +236,15 @@ public class PoissonSeries implements NutationFunction, Serializable {
 
     /** Parse a series term line.
      * @param line data line to parse
-     * @param nonPolyFactor multiplicative factor to use for non-ploynomial coefficients
+     * @param nonPolyFactor multiplicative factor to use for non-polynomial coefficients
+     * @param expectedIndex expected index of the series term
      * @param name name of the resource file (for error messages only)
      * @param lineNumber line number (for error messages only)
      * @return a series term
      * @exception OrekitException if the line is null or cannot be parsed
      */
     private SeriesTerm parseSeriesTerm (final String line, final double nonPolyFactor,
+                                        final int expectedIndex,
                                         final String name, final int lineNumber)
         throws OrekitException {
 
@@ -238,9 +255,9 @@ public class PoissonSeries implements NutationFunction, Serializable {
         }
 
         // parse the Poisson series term
-        final String[] fields = line.split("\\p{Space}+");
+        final String[] fields = line.trim().split("\\p{Space}+");
         final int l = fields.length;
-        if ((l == 17) || ((l == 18) && (fields[0].length() == 0))) {
+        if (l == 17 && Integer.parseInt(fields[0]) == expectedIndex) {
             return SeriesTerm.buildTerm(Double.parseDouble(fields[l - 16]) * nonPolyFactor,
                                         Double.parseDouble(fields[l - 15]) * nonPolyFactor,
                                         Integer.parseInt(fields[l - 14]), Integer.parseInt(fields[l - 13]),
--- src/test/java/org/orekit/data/PoissonSeriesTest.java
+++ src/test/java/org/orekit/data/PoissonSeriesTest.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import org.junit.Assert;
 import org.junit.Test;
 import org.orekit.errors.OrekitException;
+import org.orekit.errors.OrekitMessages;
 
 
 public class PoissonSeriesTest {
@@ -122,28 +123,152 @@ public class PoissonSeriesTest {
             + "j = 0  Nb of terms = 2\n"
             + "\n"
             + "   1    -6844318.44        1328.67    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
-            + "1306           0.11           0.00    0    0    4   -4    4    0    0    0    0    0    0    0    0    0\n"
+            + "   2           0.11           0.00    0    0    4   -4    4    0    0    0    0    0    0    0    0    0\n"
             + "\n"
             + "j = 1  Nb of terms = 2\n"
             + "\n"
-            + " 1307       -3328.48      205833.15    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
-            + " 1559           0.00          -0.10    1   -1   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
+            + "   3       -3328.48      205833.15    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   4           0.00          -0.10    1   -1   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
             + "\n"
             + " j = 2  Nb of terms = 2\n"
             + "\n"
-            + "  1560        2038.00          82.26    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
-            + "  1595          -0.12           0.00    1    0   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
+            + "   5        2038.00          82.26    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   6          -0.12           0.00    1    0   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
             + "  \n"
             + " j = 3  Nb of terms = 2\n"
             + "\n"
-            + "  1596           1.76         -20.39    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
-            + "  1599           0.00           0.20    0    0    0    0    2    0    0    0    0    0    0    0    0    0\n"
+            + "   7           1.76         -20.39    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   8           0.00           0.20    0    0    0    0    2    0    0    0    0    0    0    0    0    0\n"
             + "\n"
             + " j = 4  Nb of terms = 1\n"
             + "       \n"
-            + "  1600          -0.10          -0.02    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n";
+            + "   9          -0.10          -0.02    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n";
         Assert.assertNotNull(new PoissonSeries(new ByteArrayInputStream(data.getBytes()),
                                                "dummy", 1.0, 1.0));
+    }
+
+    @Test
+    public void testWrongIndex() throws OrekitException {
+        String data =
+            "Expression for the X coordinate of the CIP in the GCRS based on the IAU2000A\n"
+            + "precession-nutation model\n"
+            + "\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "X = polynomial part + non-polynomial part\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "Polynomial part (unit microarcsecond)\n"
+            + "\n"
+            + "  -16616.99 + 2004191742.88 t - 427219.05 t^2 - 198620.54 t^3 - 46.05 t^4 + 5.98 t^5\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "Non-polynomial part (unit microarcsecond)\n"
+            + "(ARG being for various combination of the fundamental arguments of the nutation theory)\n"
+            + "\n"
+            + "  Sum_i[a_{s,0})_i * sin(ARG) + a_{c,0})_i * cos(ARG)] \n"
+            + "\n"
+            + "+ Sum_i)j=1,4 [a_{s,j})_i * t^j * sin(ARG) + a_{c,j})_i * cos(ARG)] * t^j]\n"
+            + "\n"
+            + "The Table below provides the values for a_{s,j})_i and a_{c,j})_i\n"
+            + "\n"
+            + "The expressions for the fundamental arguments appearing in columns 4 to 8 (luni-solar part) \n"
+            + "and in columns 6 to 17 (planetary part) are those of the IERS Conventions 2000\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "    i    a_{s,j})_i      a_{c,j})_i    l    l'   F    D   Om L_Me L_Ve  L_E L_Ma  L_J L_Sa  L_U L_Ne  p_A\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "-16616.99 + 2004191742.88 t - 427219.05 t^2 - 198620.54 t^3 - 46.05 t^4 + 5.98 t^5\n"
+            + "j = 0  Nb of terms = 2\n"
+            + "\n"
+            + "   1    -6844318.44        1328.67    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   2           0.11           0.00    0    0    4   -4    4    0    0    0    0    0    0    0    0    0\n"
+            + "\n"
+            + "j = 1  Nb of terms = 2\n"
+            + "\n"
+            + "   3       -3328.48      205833.15    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   4           0.00          -0.10    1   -1   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
+            + "\n"
+            + " j = 2  Nb of terms = 2\n"
+            + "\n"
+            + "   5        2038.00          82.26    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   6          -0.12           0.00    1    0   -2   -2   -1    0    0    0    0    0    0    0    0    0\n"
+            + "  \n"
+            + " j = 3  Nb of terms = 2\n"
+            + "\n"
+            + "   7           1.76         -20.39    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + " 999           0.00           0.20    0    0    0    0    2    0    0    0    0    0    0    0    0    0\n"
+            + "\n"
+            + " j = 4  Nb of terms = 1\n"
+            + "       \n"
+            + "   9          -0.10          -0.02    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n";
+        try {
+            new PoissonSeries(new ByteArrayInputStream(data.getBytes()), "dummy", 1.0, 1.0);
+            Assert.fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            Assert.assertEquals(OrekitMessages.UNABLE_TO_PARSE_LINE_IN_FILE, oe.getSpecifier());
+            Assert.assertEquals(53, oe.getParts()[0]);
+            Assert.assertTrue(((String) oe.getParts()[2]).startsWith(" 999           0.00"));
+        }
+    }
+
+    @Test
+    public void testTruncated() throws OrekitException {
+        String data =
+            "Expression for the X coordinate of the CIP in the GCRS based on the IAU2000A\n"
+            + "precession-nutation model\n"
+            + "\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "X = polynomial part + non-polynomial part\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "Polynomial part (unit microarcsecond)\n"
+            + "\n"
+            + "  -16616.99 + 2004191742.88 t - 427219.05 t^2 - 198620.54 t^3 - 46.05 t^4 + 5.98 t^5\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "Non-polynomial part (unit microarcsecond)\n"
+            + "(ARG being for various combination of the fundamental arguments of the nutation theory)\n"
+            + "\n"
+            + "  Sum_i[a_{s,0})_i * sin(ARG) + a_{c,0})_i * cos(ARG)] \n"
+            + "\n"
+            + "+ Sum_i)j=1,4 [a_{s,j})_i * t^j * sin(ARG) + a_{c,j})_i * cos(ARG)] * t^j]\n"
+            + "\n"
+            + "The Table below provides the values for a_{s,j})_i and a_{c,j})_i\n"
+            + "\n"
+            + "The expressions for the fundamental arguments appearing in columns 4 to 8 (luni-solar part) \n"
+            + "and in columns 6 to 17 (planetary part) are those of the IERS Conventions 2000\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "\n"
+            + "    i    a_{s,j})_i      a_{c,j})_i    l    l'   F    D   Om L_Me L_Ve  L_E L_Ma  L_J L_Sa  L_U L_Ne  p_A\n"
+            + "\n"
+            + "----------------------------------------------------------------------\n"
+            + "-16616.99 + 2004191742.88 t - 427219.05 t^2 - 198620.54 t^3 - 46.05 t^4 + 5.98 t^5\n"
+            + "j = 0  Nb of terms = 2\n"
+            + "\n"
+            + "   1    -6844318.44        1328.67    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n"
+            + "   2           0.11           0.00    0    0    4   -4    4    0    0    0    0    0    0    0    0    0\n"
+            + "\n"
+            + "j = 1  Nb of terms = 2\n"
+            + "\n"
+            + "   3       -3328.48      205833.15    0    0    0    0    1    0    0    0    0    0    0    0    0    0\n";
+        try {
+            new PoissonSeries(new ByteArrayInputStream(data.getBytes()), "dummy", 1.0, 1.0);
+            Assert.fail("an exception should have been thrown");
+        } catch (OrekitException oe) {
+            Assert.assertEquals(OrekitMessages.UNEXPECTED_END_OF_FILE_AFTER_LINE, oe.getSpecifier());
+            Assert.assertEquals(42, oe.getParts()[1]);
+        }
     }
 
     @Test
