e45f069033b4b35cb6cdee52da7c1c9c6be5b146
Luc Maisonobe <luc@orekit.org>
2014-03-01 18:06:43 +0100

    Unit test TLESeriesTest modded for TLESeries total coverage.

--- src/test/java/org/orekit/files/general/SatelliteInformationTest.java
+++ src/test/java/org/orekit/files/general/SatelliteInformationTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2014  Applied Defense Solutions, Inc.
+/* Copyright 2014 Applied Defense Solutions, Inc.
  * Licensed to CS Systèmes d'Information (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
