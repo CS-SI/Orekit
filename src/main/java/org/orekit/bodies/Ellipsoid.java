468131056ba75a35e048bcd64f6f081572e493d9
Luc Maisonobe <luc@orekit.org>
2014-09-24 20:19:14 +0200

    Added a general 3 axes ellipsoid class.
    This class includes a feature to compute any plane section (which result
    in a 2D ellipse).

---
+++ src/main/java/org/orekit/bodies/Ellipsoid.java
@@ -0,0 +1,319 @@
+/* Copyright 2002-2014 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.bodies;
+
+import java.io.Serializable;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.util.MathArrays;
+import org.apache.commons.math3.util.Precision;
+import org.orekit.frames.Frame;
+
+/**
+ * Modeling of a general three-axes ellipsoid. <p>
+ * @since 7.0
+ * @author Luc Maisonobe
+ */
+public class Ellipsoid implements Serializable {
+
+    /** Serializable UID. */
+    private static final long serialVersionUID = 20140924L;
+
+    /** Frame at the ellipsoid center, aligned with principal axes. */
+    private final Frame frame;
+
+    /** First semi-axis length. */
+    private final double a;
+
+    /** Second semi-axis length. */
+    private final double b;
+
+    /** Third semi-axis length. */
+    private final double c;
+
+    /** Simple constructor.
+     * @param frame at the ellipsoid center, aligned with principal axes
+     * @param a first semi-axis length
+     * @param b second semi-axis length
+     * @param c third semi-axis length
+     */
+    public Ellipsoid(final Frame frame, final double a, final double b, final double c) {
+        this.frame = frame;
+        this.a     = a;
+        this.b     = b;
+        this.c     = c;
+    }
+
+    /** Get the length of the first semi-axis.
+     * @return length of the first semi-axis (m)
+     */
+    public double getA() {
+        return a;
+    }
+
+    /** Get the length of the second semi-axis.
+     * @return length of the second semi-axis (m)
+     */
+    public double getB() {
+        return b;
+    }
+
+    /** Get the length of the third semi-axis.
+     * @return length of the third semi-axis (m)
+     */
+    public double getC() {
+        return c;
+    }
+
+    /** Get the ellipsoid central frame.
+     * @return ellipsoid central frame
+     */
+    public Frame getFrame() {
+        return frame;
+    }
+
+    /** Compute the the 2D ellipse at the intersection of the 3D ellipsoid and a plane.
+     * @param planePoint point belonging to the plane
+     * @param planeNormal normal of the plane
+     * @return plane section or null if there are no intersections
+     */
+    public PlaneSection getPlaneSection(final Vector3D planePoint, final Vector3D planeNormal) {
+
+        // we define the points Q in the plane using two free variables τ and υ
+        // as:
+        // Q = P + τ u + υ v
+        // Q belongs to the ellipse so:
+        // (xQ / a)² + (yQ / b)² + (zQ / c)² = 1
+        // combining both equations, we get:
+        //   (xP² + 2 xP (τ xU + υ xV) + (τ xU + υ xV)²) / a²
+        // + (yP² + 2 yP (τ yU + υ yV) + (τ yU + υ yV)²) / b²
+        // + (zP² + 2 zP (τ zU + υ zV) + (τ zU + υ zV)²) / c²
+        // = 1
+        // which can be rewritten:
+        // α τ² + β υ² + 2 γ τυ + 2 δ τ + 2 ε υ + ζ = 0
+        // with
+        // α =  xU²  / a² +  yU²  / b² +  zU²  / c² > 0
+        // β =  xV²  / a² +  yV²  / b² +  zV²  / c² > 0
+        // γ = xU xV / a² + yU yV / b² + zU zV / c²
+        // δ = xP xU / a² + yP yU / b² + zP zU / c²
+        // ε = xP xV / a² + yP yV / b² + zP zV / c²
+        // ζ =  xP²  / a² +  yP²  / b² +  zP²  / c² - 1
+        // this is the equation of a conic (here an ellipse)
+        // Of course, we note that if the point P belongs to the ellipse
+        // then ζ = 0 and the equation holds at point P since τ = 0 and υ = 0
+        final Vector3D u     = planeNormal.orthogonal();
+        final Vector3D v     = Vector3D.crossProduct(planeNormal, u).normalize();
+        final double xUOa    = u.getX() / a;
+        final double yUOb    = u.getY() / b;
+        final double zUOc    = u.getZ() / c;
+        final double xVOa    = v.getX() / a;
+        final double yVOb    = v.getY() / b;
+        final double zVOc    = v.getZ() / c;
+        final double xPOa    = planePoint.getX() / a;
+        final double yPOb    = planePoint.getY() / b;
+        final double zPOc    = planePoint.getZ() / c;
+        final double alpha   = xUOa * xUOa + yUOb * yUOb + zUOc * zUOc;
+        final double beta    = xVOa * xVOa + yVOb * yVOb + zVOc * zVOc;
+        final double gamma   = MathArrays.linearCombination(xUOa, xVOa, yUOb, yVOb, zUOc, zVOc);
+        final double delta   = MathArrays.linearCombination(xPOa, xUOa, yPOb, yUOb, zPOc, zUOc);
+        final double epsilon = MathArrays.linearCombination(xPOa, xVOa, yPOb, yVOb, zPOc, zVOc);
+        final double zeta    = MathArrays.linearCombination(xPOa, xPOa, yPOb, yPOb, zPOc, zPOc, 1, -1);
+
+        // reduce the general equation α τ² + β υ² + 2 γ τυ + 2 δ τ + 2 ε υ + ζ = 0
+        // to canonical form (λ/l)² + (μ/m)² = 1
+        // using a coordinates change
+        //       τ = τC + λ cosθ - μ sinθ
+        //       υ = υC + λ sinθ + μ cosθ
+        // or equivalently
+        //       λ =   (τ - τC) cosθ + (υ - υC) sinθ
+        //       μ = - (τ - τC) sinθ + (υ - υC) cosθ
+        // τC and υC are the coordinates of the 2D ellipse center with respect to P
+        // 2l and 2m and are the axes lengths (major or minor depending on which one is greatest)
+        // θ is the angle of the 2D ellipse axis corresponding to axis with length 2l
+
+        // choose θ in order to cancel the coupling term in λμ
+        // expanding the general equation, we get:
+        // A λ² + B μ² + C λμ + D λ + E μ + F = 0
+        // with C = 2[(β - α) cosθ sinθ + γ (cos²θ - sin²θ)]
+        // hence the term is cancelled when θ = arctan(t), with γ t² + (α - β) t - γ = 0
+        // As the solutions of the quadratic equation obey t₁t₂ = -1, they correspond to
+        // angles θ in quadrature to each other. Selecting one solution or the other simply
+        // exchanges the principal axes. As we don't care about which axis we want as the
+        // first one, we select the solution with |tanθ] <= 1, in order to increase accuracy
+        // when extracting θ
+        final double theta;
+        if (FastMath.abs(gamma) < Precision.SAFE_MIN) {
+            theta = 0.0;
+        } else {
+            final double bMA = beta - alpha;
+            final double tanTheta =
+                    (bMA >= 0) ?
+                    ((bMA + FastMath.sqrt(bMA * bMA + 4 * gamma * gamma)) / (2 * gamma)) :
+                    ((bMA - FastMath.sqrt(bMA * bMA + 4 * gamma * gamma)) / (2 * gamma));
+            theta = FastMath.abs(tanTheta) > 1 ? FastMath.atan(-1.0 / tanTheta) : FastMath.atan(tanTheta);
+        }
+
+        // choose τC and υC in order to cancel the linear terms in λ and μ
+        // expanding the general equation, we get:
+        // A λ² + B μ² + C λμ + D λ + E μ + F = 0
+        // with D = 2[(α τC + γ υC + δ) cosθ + (γ τC + β υC + ε) sinθ]
+        //      E = 2[(γ τC + β υC + ε) cosθ - (α τC + γ υC + δ) sinθ]
+        // θ can be eliminated by combining the equations
+        // D cosθ - E sinθ = 2[α τC + γ υC + δ]
+        // E cosθ + D sinθ = 2[γ τC + β υC + ε]
+        // hence the terms D and E are both cancelled (regardless of θ) when
+        //     τC = (β δ - γ ε) / (γ² - α β)
+        //     υC = (α ε - γ δ) / (γ² - α β)
+        final double denom = gamma * gamma - alpha * beta;
+        final double tauC  = (beta  * delta   - gamma * epsilon) / denom;
+        final double nuC   = (alpha * epsilon - gamma * delta)   / denom;
+
+        // compute l and m
+        // expanding the general equation, we get:
+        // A λ² + B μ² + C λμ + D λ + E μ + F = 0
+        // with A = α cos²θ + β sin²θ + 2 γ cosθ sinθ
+        //      B = α sin²θ + β cos²θ - 2 γ cosθ sinθ
+        //      F = α τC² + β υC² + 2 γ τC υC + 2 δ τC + 2 ε υC + ζ
+        // hence we compute directly l = √(-F/A) and m = √(-F/B)
+        final double cos    = FastMath.cos(theta);
+        final double sin    = FastMath.sin(theta);
+        final double cos2   = cos * cos;
+        final double sin2   = sin * sin;
+        final double twogcs = 2 * gamma * cos * sin;
+        final double bigA   = alpha * cos2 + beta * sin2 + twogcs;
+        final double bigB   = alpha * sin2 + beta * cos2 - twogcs;
+        final double bigF   = (alpha * tauC + 2 * (gamma * nuC + delta)) * tauC +
+                              (beta * nuC + 2 * epsilon) * nuC + zeta;
+        final double l      = FastMath.sqrt(-bigF / bigA);
+        final double m      = FastMath.sqrt(-bigF / bigB);
+        if (Double.isNaN(l + m)) {
+            // the plane does not intersect the ellipsoid
+            return null;
+        }
+
+        if (l > m) {
+            return new PlaneSection(new Vector3D(1, planePoint, tauC, u, nuC, v),
+                                    new Vector3D( cos, u, sin, v),
+                                    new Vector3D(-sin, u, cos, v),
+                                    l, m);
+        } else {
+            return new PlaneSection(new Vector3D(1, planePoint, tauC, u, nuC, v),
+                                    new Vector3D(sin, u, -cos, v),
+                                    new Vector3D(cos, u,  sin, v),
+                                    m, l);
+        }
+
+    }
+
+    /**
+     * Class holding the 2D ellipse at the intersection of the 3D ellipsoid and
+     * a plane.
+     * <p>
+     * Instances of this class are guaranteed to be immutable (but the underlying
+     * ellipsoid may not be).
+     * </p>
+     */
+    public class PlaneSection implements Serializable {
+
+        /** Serializable UID. */
+        private static final long serialVersionUID = 20140923L;
+
+        /** Center of the 2D ellipse. */
+        private final Vector3D center;
+
+        /** Unit vector along the major axis. */
+        private final Vector3D u;
+
+        /** Unit vector along the minor axis. */
+        private final Vector3D v;
+
+        /** Semi major axis. */
+        private final double a;
+
+        /** Semi minor axis. */
+        private final double b;
+
+        /** Simple constructor.
+         * @param center center of the 2D ellipse
+         * @param u unit vector along the major axis
+         * @param v unit vector along the minor axis
+         * @param a semi major axis
+         * @param b semi minor axis
+         */
+        private PlaneSection(final Vector3D center, final Vector3D u,
+                             final Vector3D v, final double a, final double b) {
+            this.center = center;
+            this.u = u;
+            this.v = v;
+            this.a = a;
+            this.b = b;
+        }
+
+        /** Get the center of the 2D ellipse.
+         * @return center of the 2D ellipse
+         */
+        public Vector3D getCenter() {
+            return center;
+        }
+
+        /** Get the unit vector along the major axis.
+         * @return unit vector along the major axis
+         */
+        public Vector3D getU() {
+            return u;
+        }
+
+        /** Get the unit vector along the minor axis.
+         * @return unit vector along the minor axis
+         */
+        public Vector3D getV() {
+            return v;
+        }
+
+        /** Get the semi major axis.
+         * @return semi major axis
+         */
+        public double getA() {
+            return a;
+        }
+
+        /** Get the semi minor axis.
+         * @return semi minor axis
+         */
+        public double getB() {
+            return b;
+        }
+
+        /** Get the base ellipsoid.
+         * @return base ellipsoid
+         */
+        public Ellipsoid getEllipsoid() {
+            return Ellipsoid.this;
+        }
+
+        /** Get a point of the 2D ellipse.
+         * @param theta angular parameter on the ellipse (really the eccentric anomaly)
+         * @return ellipse point at theta, in underlying ellipsoid frame
+         */
+        public Vector3D pointAt(final double theta) {
+            return new Vector3D(1, center, a * FastMath.cos(theta), u, b * FastMath.sin(theta), v);
+        }
+
+    }
+
+}
---
+++ src/test/java/org/orekit/bodies/EllipsoidTest.java
@@ -0,0 +1,176 @@
+/* Copyright 2002-2014 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.bodies;
+
+
+import java.io.IOException;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.Well19937a;
+import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.util.MathArrays;
+import org.junit.Assert;
+import org.junit.Test;
+import org.orekit.frames.FactoryManagedFrame;
+import org.orekit.frames.FramesFactory;
+import org.orekit.frames.Predefined;
+
+
+public class EllipsoidTest {
+
+    @Test
+    public void testGetters() {
+
+        final Ellipsoid ellipsoid =
+                new Ellipsoid(FramesFactory.getEME2000(), 1, 2, 3);
+
+        Assert.assertEquals(Predefined.EME2000,
+                            ((FactoryManagedFrame) ellipsoid.getFrame()).getFactoryKey());
+        Assert.assertEquals(1.0, ellipsoid.getA(), 1.0e-15);
+        Assert.assertEquals(2.0, ellipsoid.getB(), 1.0e-15);
+        Assert.assertEquals(3.0, ellipsoid.getC(), 1.0e-15);
+
+    }
+
+    @Test
+    public void testPrincipalPlanesIntersections() {
+
+        final Ellipsoid ellipsoid =
+                new Ellipsoid(FramesFactory.getEME2000(), 1, 2, 3);
+
+        final Ellipsoid.PlaneSection xy = ellipsoid.getPlaneSection(Vector3D.ZERO, Vector3D.PLUS_K);
+        Assert.assertEquals(0, Vector3D.distance(Vector3D.ZERO, xy.getCenter()), 1.0e-15);
+        checkPrincipalAxes(xy, Vector3D.PLUS_J, Vector3D.MINUS_I);
+        Assert.assertEquals(2.0, xy.getA(), 1.0e-15);
+        Assert.assertEquals(1.0, xy.getB(), 1.0e-15);
+        Assert.assertTrue(xy.getEllipsoid() == ellipsoid);
+        Assert.assertEquals(0.0, errorOnEllipsoid(xy, ellipsoid), 1.0e-15);
+        Assert.assertEquals(0.0, errorOnPlane(xy, Vector3D.ZERO, Vector3D.PLUS_K), 1.0e-15);
+
+        final Ellipsoid.PlaneSection yz = ellipsoid.getPlaneSection(Vector3D.ZERO, Vector3D.PLUS_I);
+        Assert.assertEquals(0, Vector3D.distance(Vector3D.ZERO, yz.getCenter()), 1.0e-15);
+        checkPrincipalAxes(yz, Vector3D.PLUS_K, Vector3D.MINUS_J);
+        Assert.assertEquals(3.0, yz.getA(), 1.0e-15);
+        Assert.assertEquals(2.0, yz.getB(), 1.0e-15);
+        Assert.assertTrue(yz.getEllipsoid() == ellipsoid);
+        Assert.assertEquals(0.0, errorOnEllipsoid(yz, ellipsoid), 1.0e-15);
+        Assert.assertEquals(0.0, errorOnPlane(yz, Vector3D.ZERO, Vector3D.PLUS_I), 1.0e-15);
+
+        final Ellipsoid.PlaneSection zx = ellipsoid.getPlaneSection(Vector3D.ZERO, Vector3D.PLUS_J);
+        Assert.assertEquals(0, Vector3D.distance(Vector3D.ZERO, zx.getCenter()), 1.0e-15);
+        checkPrincipalAxes(zx, Vector3D.PLUS_K, Vector3D.PLUS_I);
+        Assert.assertEquals(3.0, zx.getA(), 1.0e-15);
+        Assert.assertEquals(1.0, zx.getB(), 1.0e-15);
+        Assert.assertTrue(zx.getEllipsoid() == ellipsoid);
+        Assert.assertEquals(0.0, errorOnEllipsoid(zx, ellipsoid), 1.0e-15);
+        Assert.assertEquals(0.0, errorOnPlane(zx, Vector3D.ZERO, Vector3D.PLUS_J), 1.0e-15);
+
+    }
+
+    @Test
+    public void testNoIntersections() {
+        final Ellipsoid ellipsoid =
+                new Ellipsoid(FramesFactory.getEME2000(), 1, 2, 3);
+        final Ellipsoid.PlaneSection ps = ellipsoid.getPlaneSection(new Vector3D(0, 0, 4), Vector3D.PLUS_K);
+        Assert.assertNull(ps);
+    }
+
+    @Test
+    public void testSinglePoint() throws IOException {
+        final Ellipsoid ellipsoid =
+                new Ellipsoid(FramesFactory.getEME2000(), 1, 2, 3);
+        final Ellipsoid.PlaneSection ps = ellipsoid.getPlaneSection(new Vector3D(0, 0, 3), Vector3D.PLUS_K);
+        Assert.assertEquals(0, Vector3D.distance(new Vector3D(0, 0, 3), ps.getCenter()), 1.0e-15);
+        Assert.assertEquals(0.0, ps.getA(), 1.0e-15);
+        Assert.assertEquals(0.0, ps.getB(), 1.0e-15);
+    }
+
+    @Test
+    public void testRandomNormalSections() throws IOException {
+        RandomGenerator random = new Well19937a(0x573c54d152aeafe4l);
+        for (int i = 0; i < 100; ++i) {
+            double a = 10 * random.nextDouble();
+            double b = 10 * random.nextDouble();
+            double c = 10 * random.nextDouble();
+            double size = FastMath.max(FastMath.max(a, b), c);
+            final Ellipsoid ellipsoid = new Ellipsoid(FramesFactory.getEME2000(), a, b, c);
+            for (int j = 0; j < 50; ++j) {
+                double phi     = FastMath.PI * (random.nextDouble() - 0.5);
+                double lambda  = 2 * FastMath.PI * random.nextDouble();
+                double cPhi    = FastMath.cos(phi);
+                double sPhi    = FastMath.sin(phi);
+                double cLambda = FastMath.cos(lambda);
+                double sLambda = FastMath.sin(lambda);
+                Vector3D surfacePoint = new Vector3D(ellipsoid.getA() * cPhi * cLambda,
+                                                     ellipsoid.getB() * cPhi * sLambda,
+                                                     ellipsoid.getC() * sPhi);
+                Vector3D t1 = new Vector3D(-ellipsoid.getA() * cPhi * sLambda,
+                                            ellipsoid.getB() * cPhi * cLambda,
+                                            0).normalize();
+                Vector3D t2 = new Vector3D(-ellipsoid.getA() * sPhi * cLambda,
+                                           -ellipsoid.getB() * sPhi * sLambda,
+                                            ellipsoid.getC() * cPhi).normalize();
+                final double azimuth = 2 * FastMath.PI * random.nextDouble();
+                double cAzimuth = FastMath.cos(azimuth);
+                double sAzimuth = FastMath.sin(azimuth);
+                Vector3D tAz = new Vector3D(cAzimuth, t1, sAzimuth, t2);
+                
+                final Ellipsoid.PlaneSection ps = ellipsoid.getPlaneSection(surfacePoint, tAz);
+                Assert.assertEquals(0.0, errorOnEllipsoid(ps, ellipsoid), 1.0e-12 * size);
+                Assert.assertEquals(0.0, errorOnPlane(ps, surfacePoint, tAz), 1.0e-10 * size);
+                double cos = Vector3D.dotProduct(surfacePoint.subtract(ps.getCenter()), ps.getU()) / ps.getA();
+                double sin = Vector3D.dotProduct(surfacePoint.subtract(ps.getCenter()), ps.getV()) / ps.getB();
+                final Vector3D rebuilt = ps.pointAt(FastMath.atan2(sin, cos));
+                Assert.assertEquals(0, Vector3D.distance(surfacePoint, rebuilt), 1.0e-11 * size);
+            }
+        }
+    }
+
+    private void checkPrincipalAxes(Ellipsoid.PlaneSection ps, Vector3D expectedU, Vector3D expectedV) {
+        if (Vector3D.dotProduct(expectedU, ps.getU()) >= 0) {
+            Assert.assertEquals(0, Vector3D.distance(expectedU,  ps.getU()), 1.0e-15);
+            Assert.assertEquals(0, Vector3D.distance(expectedV,  ps.getV()), 1.0e-15);
+        } else {
+            Assert.assertEquals(0, Vector3D.distance(expectedU.negate(), ps.getU()), 1.0e-15);
+            Assert.assertEquals(0, Vector3D.distance(expectedV.negate(), ps.getV()), 1.0e-15);
+        }
+    }
+
+    private double errorOnEllipsoid(Ellipsoid.PlaneSection ps, Ellipsoid ellipsoid) {
+        double max = 0;
+        for (double theta = 0; theta < 2 * FastMath.PI; theta += 0.1) {
+            Vector3D p = ps.pointAt(theta);
+            double xOa = p.getX() / ellipsoid.getA();
+            double yOb = p.getY() / ellipsoid.getB();
+            double zOc = p.getZ() / ellipsoid.getC();
+            max = FastMath.max(max, FastMath.abs(MathArrays.linearCombination(xOa, xOa, yOb, yOb, zOc, zOc, 1, -1)));
+        }
+        return max;
+    }
+
+    private double errorOnPlane(Ellipsoid.PlaneSection ps, Vector3D planePoint, Vector3D planeNormal) {
+        double max = 0;
+        for (double theta = 0; theta < 2 * FastMath.PI; theta += 0.1) {
+            Vector3D p = ps.pointAt(theta);
+            max = FastMath.max(max, FastMath.abs(Vector3D.dotProduct(p.subtract(planePoint).normalize(), planeNormal)));
+        }
+        return max;
+    }
+
+}
+
