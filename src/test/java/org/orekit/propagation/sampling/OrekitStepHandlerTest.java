1876268a85264a5b29cb1ec80f55694a9cedece0
Luc Maisonobe <luc@orekit.org>
2015-04-29 09:38:20 +0200

    Unused import.

--- src/test/java/org/orekit/propagation/sampling/OrekitStepHandlerTest.java
+++ src/test/java/org/orekit/propagation/sampling/OrekitStepHandlerTest.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.math3.util.FastMath;
 import org.junit.Before;
