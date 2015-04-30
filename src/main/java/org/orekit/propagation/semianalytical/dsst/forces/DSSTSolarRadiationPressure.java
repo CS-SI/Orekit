0bf533f758829ef0bf27485918d4501ca1587bd9
Luc Maisonobe <luc@orekit.org>
2015-04-30 19:00:39 +0200

    Removed TODO and replaced it by issue #199.

--- src/main/java/org/orekit/propagation/semianalytical/dsst/forces/DSSTSolarRadiationPressure.java
+++ src/main/java/org/orekit/propagation/semianalytical/dsst/forces/DSSTSolarRadiationPressure.java
@@ -172,7 +172,6 @@ public class DSSTSolarRadiationPressure extends AbstractGaussianContribution {
 
     /** {@inheritDoc} */
     public EventDetector[] getEventsDetectors() {
-        // TODO: add eclipses handling
         return null;
     }
 
