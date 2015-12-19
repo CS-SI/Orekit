d86fd4c7ce00385e0f2b65f8a9d24bccb04a0cd2
Luc Maisonobe <luc@orekit.org>
2015-12-19 15:59:06 +0100

    Cleanup.

--- src/main/java/org/orekit/estimation/measurements/Angular.java
+++ src/main/java/org/orekit/estimation/measurements/Angular.java
@@ -44,7 +44,7 @@ public class Angular extends AbstractMeasurement<Angular> {
      * name conflict occurs
      */
     public Angular(final GroundStation station, final AbsoluteDate date,
-                final double[] angular, final double[] sigma, final double[] baseWeight)
+                   final double[] angular, final double[] sigma, final double[] baseWeight)
         throws OrekitException {
         super(date, angular, sigma, baseWeight);
         this.station = station;
@@ -72,47 +72,45 @@ public class Angular extends AbstractMeasurement<Angular> {
         final double          dt           = delta - tauD;
         final SpacecraftState transitState = state.shiftedBy(dt);
 
-        // Transformation from inertial frame to station parent frame
-        final Transform iner2planeto = state.getFrame().getTransformTo(station.getOffsetFrame().getParent(), getDate());
+        // transformation from inertial frame to station parent frame
+        final Transform iner2Body = state.getFrame().getTransformTo(station.getOffsetFrame().getParent(), getDate());
 
-        // Station topocentric frame (east-north-zenith) in station parent frame expressed as DerivativeStructures
+        // station topocentric frame (east-north-zenith) in station parent frame expressed as DerivativeStructures
         final FieldVector3D<DerivativeStructure> east   = station.getOffsetDerivatives(6, 3, 4, 5).getEast();
         final FieldVector3D<DerivativeStructure> north  = station.getOffsetDerivatives(6, 3, 4, 5).getNorth();
         final FieldVector3D<DerivativeStructure> zenith = station.getOffsetDerivatives(6, 3, 4, 5).getZenith();
 
-        // Station origin in station parent frame
-        final FieldVector3D<DerivativeStructure> Qp = station.getOffsetDerivatives(6, 3, 4, 5).getOrigin();
+        // station origin in station parent frame
+        final FieldVector3D<DerivativeStructure> qP = station.getOffsetDerivatives(6, 3, 4, 5).getOrigin();
 
         // satellite vector expressed in station parent frame
-        final Vector3D transitp = iner2planeto.transformPosition(transitState.getPVCoordinates().getPosition());
+        final Vector3D transitp = iner2Body.transformPosition(transitState.getPVCoordinates().getPosition());
 
         // satellite vector expressed in station parent frame expressed as DerivativeStructures
-        final FieldVector3D<DerivativeStructure> Pp = new FieldVector3D<DerivativeStructure> (new DerivativeStructure(6, 1, 0, transitp.getX()),
-                                                                                              new DerivativeStructure(6, 1, 1, transitp.getY()),
-                                                                                              new DerivativeStructure(6, 1, 2, transitp.getZ()));
+        final FieldVector3D<DerivativeStructure> pP = new FieldVector3D<DerivativeStructure>(new DerivativeStructure(6, 1, 0, transitp.getX()),
+                                                                                             new DerivativeStructure(6, 1, 1, transitp.getY()),
+                                                                                             new DerivativeStructure(6, 1, 2, transitp.getZ()));
         // station-satellite vector expressed in station parent frame
-        final FieldVector3D<DerivativeStructure> StaSat = Pp.subtract(Qp);
+        final FieldVector3D<DerivativeStructure> staSat = pP.subtract(qP);
 
-        final DerivativeStructure azimuth   = DerivativeStructure.atan2(StaSat.dotProduct(east), StaSat.dotProduct(north));
-        final DerivativeStructure elevation = (StaSat.dotProduct(zenith).divide(StaSat.getNorm())).asin();
-        //System.out.println("Reference azimuth : " + azimuth.getValue() + "\n");
-        //System.out.println("Reference elevation : " + elevation.getValue() + "\n");
+        final DerivativeStructure azimuth   = DerivativeStructure.atan2(staSat.dotProduct(east), staSat.dotProduct(north));
+        final DerivativeStructure elevation = staSat.dotProduct(zenith).divide(staSat.getNorm()).asin();
 
         // prepare the evaluation
         final Evaluation<Angular> evaluation = new Evaluation<Angular>(this, iteration, transitState);
 
-        // Azimuth - Elevation values
+        // azimuth - elevation values
         evaluation.setValue(azimuth.getValue(), elevation.getValue());
 
-        // partial derivatives of Azimuth with respect to state
-        final AngularCoordinates ac = iner2planeto.getInverse().getAngular();
+        // partial derivatives of azimuth with respect to state
+        final AngularCoordinates ac = iner2Body.getInverse().getAngular();
 
-        // partial derivatives of Azimuth with respect to state expressed in station parent frame
-        final Vector3D tto  = new Vector3D (azimuth.getPartialDerivative(1, 0, 0, 0, 0, 0),
-                                            azimuth.getPartialDerivative(0, 1, 0, 0, 0, 0),
-                                            azimuth.getPartialDerivative(0, 0, 1, 0, 0, 0));
+        // partial derivatives of azimuth with respect to state expressed in station parent frame
+        final Vector3D tto  = new Vector3D(azimuth.getPartialDerivative(1, 0, 0, 0, 0, 0),
+                                           azimuth.getPartialDerivative(0, 1, 0, 0, 0, 0),
+                                           azimuth.getPartialDerivative(0, 0, 1, 0, 0, 0));
 
-        // partial derivatives of Azimuth with respect to state expressed in Satellite inertial frame
+        // partial derivatives of azimuth with respect to state expressed in satellite inertial frame
         final Vector3D dAzOndPtmp = ac.getRotation().applyTo(tto);
         final double[] dAzOndP = new double[] {
                                                dAzOndPtmp.getX(),
@@ -124,11 +122,11 @@ public class Angular extends AbstractMeasurement<Angular> {
         };
 
         // partial derivatives of Elevation with respect to state expressed in station parent frame
-        final Vector3D ttu  = new Vector3D (elevation.getPartialDerivative(1, 0, 0, 0, 0, 0),
-                                            elevation.getPartialDerivative(0, 1, 0, 0, 0, 0),
-                                            elevation.getPartialDerivative(0, 0, 1, 0, 0, 0));
+        final Vector3D ttu  = new Vector3D(elevation.getPartialDerivative(1, 0, 0, 0, 0, 0),
+                                           elevation.getPartialDerivative(0, 1, 0, 0, 0, 0),
+                                           elevation.getPartialDerivative(0, 0, 1, 0, 0, 0));
 
-        // partial derivatives of Elevation with respect to state expressed in Satellite inertial frame
+        // partial derivatives of elevation with respect to state expressed in satellite inertial frame
         final Vector3D dElOndPtmp = ac.getRotation().applyTo(ttu);
         final double[] dElOndP = new double[] {
                                                 dElOndPtmp.getX(),
@@ -145,15 +143,17 @@ public class Angular extends AbstractMeasurement<Angular> {
         if (station.isEstimated()) {
 
             // partial derivatives with respect to parameters
-            // Be aware: east; north and zenith are expressed in Station Parent frame but the Derivative are expressed with respect to Reference Station Topocentric frame
-            // partial derivativtestParameterDerivativestestParameterDerivativeses of Azimuth with respect to parameters in planetocentric frame
+            // Be aware: east; north and zenith are expressed in station parent frame but the derivatives are expressed
+            // with respect to reference station topocentric frame
+
+            // partial derivatives of azimuth with respect to parameters in body frame
             final double[] dAzOndQ = new double[] {
                                                    azimuth.getPartialDerivative(0, 0, 0, 1, 0, 0),
                                                    azimuth.getPartialDerivative(0, 0, 0, 0, 1, 0),
                                                    azimuth.getPartialDerivative(0, 0, 0, 0, 0, 1)
             };
 
-            // partial derivatives of Elevation with respect to parameters in planetocentric frame
+            // partial derivatives of elevation with respect to parameters in body frame
             final double[] dElOndQ = new double[] {
                                                    elevation.getPartialDerivative(0, 0, 0, 1, 0, 0),
                                                    elevation.getPartialDerivative(0, 0, 0, 0, 1, 0),
