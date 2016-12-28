1a7b405221632ab09413d0b7c7e5e7c54a9dac82
Hank Grabowski <hank@applieddefense.com>
2016-12-28 16:02:55 -0500

    Code cleanup for warnings

--- src/test/java/org/orekit/files/ccsds/OEMWriterTest.java
+++ src/test/java/org/orekit/files/ccsds/OEMWriterTest.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.junit.Before;
