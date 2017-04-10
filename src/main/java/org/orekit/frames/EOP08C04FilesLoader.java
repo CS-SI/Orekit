bf555d2e93d037f9e3fac8cd03b45af7f7469f34
Luc Maisonobe <luc@orekit.org>
2017-04-10 12:11:01 +0200

    Typo.

--- src/main/java/org/orekit/frames/EOP08C04FilesLoader.java
+++ src/main/java/org/orekit/frames/EOP08C04FilesLoader.java
@@ -40,7 +40,7 @@ import org.orekit.utils.IERSConventions;
  * <p>EOP 08 C04 files contain {@link EOPEntry
  * Earth Orientation Parameters} consistent with ITRF2008 for one year periods.</p>
  * <p>The EOP 08 C04 files are recognized thanks to their base names, which
- * must match one of the the patterns <code>eopc04_08_IAU2000.##</code> or
+ * must match one of the patterns <code>eopc04_08_IAU2000.##</code> or
  * <code>eopc04_08.##</code> (or the same ending with <code>.gz</code> for
  * gzip-compressed files) where # stands for a digit character.</p>
  * <p>Between 2002 and 2007, another series of Earth Orientation Parameters was
