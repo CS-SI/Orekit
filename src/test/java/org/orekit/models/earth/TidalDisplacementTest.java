45fa36f6874d792f7ad316e616f4f91817940c76
Luc Maisonobe <luc@orekit.org>
2017-09-13 19:47:28 +0200

    Work In Progress on frequency domain correction testing.

--- src/test/java/org/orekit/models/earth/TidalDisplacementTest.java
+++ src/test/java/org/orekit/models/earth/TidalDisplacementTest.java
@@ -17,17 +17,23 @@
 package org.orekit.models.earth;
 
 
+import java.lang.reflect.Field;
+
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
+import org.hipparchus.util.FastMath;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.orekit.Utils;
 import org.orekit.bodies.CelestialBodyFactory;
+import org.orekit.data.BodiesElements;
+import org.orekit.data.FundamentalNutationArguments;
 import org.orekit.errors.OrekitException;
 import org.orekit.frames.EOPHistory;
 import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
 import org.orekit.utils.Constants;
 import org.orekit.utils.IERSConventions;
@@ -74,11 +80,29 @@ public class TidalDisplacementTest {
     }
 
     @Test
-    public void testDehant() throws OrekitException {
+    public void testDehantOriginalArguments() throws OrekitException {
+        // this test intends to reproduce as much as possible the DEHANTTIDEINEL.F test case
+        // it does so by replacing the fundamental nutation arguments used by Orekit
+        // (which come from IERS conventions) by the Doodson arguments in the aforementioned program
+        // The results should be really close to the reference values from the original test case
+        doTestDehant(true, 1.0e-10);
+    }
+
+    @Test
+    public void testDehantIERSArguments() throws OrekitException {
+        // this test intends to replay the DEHANTTIDEINEL.F test case but using the
+        // fundamental nutation arguments from IERS conventions.
+        // The results should be very slightly different from the reference values
+        doTestDehant(false, 1.0e-10);
+    }
+
+    private void doTestDehant(final boolean replaceFundamentalArguments, final double tolerance)
+        throws OrekitException {
 
         IERSConventions conventions = IERSConventions.IERS_2010;
         Frame           itrf        = FramesFactory.getITRF(conventions, false);
         EOPHistory      eopHistory  = FramesFactory.getEOPHistory(conventions, false);
+        TimeScale       ut1         = TimeScalesFactory.getUT1(conventions, false);
 
         // constants consistent with DEHANTTIDEINEL.F reference program
         // available at <ftp://tai.bipm.org/iers/conv2010/chapter7/dehanttideinel/>
@@ -100,6 +124,7 @@ public class TidalDisplacementTest {
                                                                                                     -169288918.592160),
                                                                                        Vector3D.ZERO,
                                                                                        Vector3D.ZERO);
+
         TidalDisplacement td = new TidalDisplacement(itrf, re,
                                                      massRatioSun * (1.0 / (1.0 + massRatioMoon)),
                                                      1.0 / massRatioMoon,
@@ -107,13 +132,61 @@ public class TidalDisplacementTest {
                                                      fakeMoon,
                                                      eopHistory);
 
+        if (replaceFundamentalArguments) {
+            try {
+                // we override the official IERS conventions 2010 arguments with fake arguments matching DEHANTTIDEINEL.F code
+                String regularArguments = "/assets/org/orekit/IERS-conventions/2010/nutation-arguments.txt";
+                FundamentalNutationArguments fakeArguments =
+                                new FundamentalNutationArguments(conventions, ut1,
+                                                                 IERSConventions.class.getResourceAsStream(regularArguments),
+                                                                 regularArguments) {
+
+                    private static final long serialVersionUID = 20170913L;
+
+                    @Override
+                    public BodiesElements evaluateAll(final AbsoluteDate date) {
+                        BodiesElements base = super.evaluateAll(date);
+                        double fhr = date.getComponents(ut1).getTime().getSecondsInUTCDay() / 3600.0;
+                        double t = base.getTC();
+
+                        // Doodson fundamental arguments as per DEHANTTIDEINEL.F code
+                        double s   = 218.31664563 + (481267.88194 + (-0.0014663889 + (0.00000185139) * t) * t) * t; 
+                        double tau = fhr * 15 + 280.4606184 + (36000.7700536 + (0.00038793 + (-0.0000000258) * t) * t) * t - s;
+                        double pr  = (1.396971278 + (0.000308889 + (0.000000021 + (0.000000007) * t) * t) * t) * t ;
+                        double h   = 280.46645 + (36000.7697489 + (0.00030322222  + (0.000000020 + (-0.00000000654) * t) * t) * t) * t; 
+                        double p   = 83.35324312 + (4069.01363525 + (-0.01032172222 + (-0.0000124991 + (0.00000005263) * t) * t) * t) * t; 
+                        double zns = 234.95544499 + (1934.13626197 + (-0.00207561111 + (-0.00000213944 + (0.00000001650) * t) * t) * t) * t;
+                        double ps  = 282.93734098 + (1.71945766667 + (0.00045688889 + (-0.00000001778 + (-0.00000000334) * t) * t) * t) * t;
+                        s += pr;
+
+                        // rebuild Delaunay arguments from Doodson arguments, ignoring derivatives
+                        return new BodiesElements(date, base.getTC(),
+                                                  FastMath.toRadians(s + tau), 0.0, FastMath.toRadians(s - p), 0.0, FastMath.toRadians(h - ps), 0.0,
+                                                  FastMath.toRadians(s + zns), 0.0, FastMath.toRadians(s - h), 0.0, FastMath.toRadians(-zns),   0.0,
+                                                  base.getLMe(),               0.0, base.getLVe(),             0.0, base.getLE(),               0.0,
+                                                  base.getLMa(),               0.0, base.getLJu(),             0.0, base.getLSa(),              0.0,
+                                                  base.getLUr(),               0.0, base.getLNe(),             0.0, base.getPa(),               0.0);
+
+                    }
+                };
+                Field correctionFunctionField = td.getClass().getDeclaredField("frequencyCorrection");
+                correctionFunctionField.setAccessible(true);
+                Object correctionFunction = correctionFunctionField.get(td);
+                Field argumentsField = correctionFunction.getClass().getDeclaredField("arguments");
+                argumentsField.setAccessible(true);
+                argumentsField.set(correctionFunction, fakeArguments);
+            } catch (SecurityException | NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
+                Assert.fail(e.getLocalizedMessage());
+            }
+
+        }
+
         Vector3D fundamentalStationWettzell = new Vector3D(4075578.385, 931852.890, 4801570.154);
-        AbsoluteDate date = new AbsoluteDate(2009, 4, 13, 0, 0, 0.0,
-                                             TimeScalesFactory.getUT1(conventions, false));
+        AbsoluteDate date = new AbsoluteDate(2009, 4, 13, 0, 0, 0.0, ut1);
         Vector3D displacement = td.displacement(date, fundamentalStationWettzell);
-        Assert.assertEquals(0.07700420357108125891, displacement.getX(), 1.0e-10);
-        Assert.assertEquals(0.06304056321824967613, displacement.getY(), 1.0e-10);
-        Assert.assertEquals(0.05516568152597246810, displacement.getZ(), 1.0e-10);
+        Assert.assertEquals(0.07700420357108125891, displacement.getX(), tolerance);
+        Assert.assertEquals(0.06304056321824967613, displacement.getY(), tolerance);
+        Assert.assertEquals(0.05516568152597246810, displacement.getZ(), tolerance);
 
     }
 
