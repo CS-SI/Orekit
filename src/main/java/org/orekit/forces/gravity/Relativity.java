c0d1b2de1c3c511d2998345c86f4dfd5bef4e872
Maxime Journot <maxime.journot@c-s.fr>
2018-02-01 12:51:16 +0100

    Updated files lacking a copyright header

--- src/main/java/org/orekit/forces/gravity/Relativity.java
+++ src/main/java/org/orekit/forces/gravity/Relativity.java
@@ -1,4 +1,4 @@
-/*
+/* Contributed to the public domain
  * Licensed to CS Systèmes d'Information (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
