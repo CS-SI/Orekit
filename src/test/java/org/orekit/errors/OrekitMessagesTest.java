9b2936aa8db352c4444db5a425eb2581247bcdfc
Luc Maisonobe <luc@orekit.org>
2018-05-21 22:11:51 +0200

    Fixed the number of messages.

--- src/test/java/org/orekit/errors/OrekitMessagesTest.java
+++ src/test/java/org/orekit/errors/OrekitMessagesTest.java
@@ -31,7 +31,7 @@ public class OrekitMessagesTest {
 
     @Test
     public void testMessageNumber() {
-        Assert.assertEquals(167, OrekitMessages.values().length);
+        Assert.assertEquals(168, OrekitMessages.values().length);
     }
 
     @Test
