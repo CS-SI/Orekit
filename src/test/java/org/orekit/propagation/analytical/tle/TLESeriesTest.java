8c3c2d8318b46918ef44b825e91d151e6985de76
Luc Maisonobe <luc@orekit.org>
2018-05-23 13:28:19 +0200

    Allow several TLE with same date in TLESeries.
    Fixes issue #411.

--- src/test/java/org/orekit/propagation/analytical/tle/TLESeriesTest.java
+++ src/test/java/org/orekit/propagation/analytical/tle/TLESeriesTest.java
@@ -247,6 +247,20 @@ public class TLESeriesTest {
         Assert.assertEquals(tleFrame.getName(), FramesFactory.getFrame(Predefined.TEME).getName());
     }
 
+    @Test
+    public void testIssue411() throws IOException, OrekitException {
+
+        TLESeries series = new TLESeries("^same-date-tle.txt$", false);
+
+        series.loadTLEData(-1);
+        TLE first = series.getFirst();
+        TLE last  = series.getLast();
+        Assert.assertEquals(16908, first.getSatelliteNumber());
+        Assert.assertEquals(16908, last.getSatelliteNumber());
+        Assert.assertNotSame(first, last);
+
+    }
+
     @Before
     public void setUp() {
         Utils.setDataRoot("regular-data:tle/tle-series");
