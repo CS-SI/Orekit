07a7a5e0b5bf05682372f8bf06bb3fc9f0483fa5
Luc Maisonobe <luc@orekit.org>
2018-07-27 11:44:19 +0200

    Allow non-equinoxial initial orbit.

--- src/main/java/org/orekit/propagation/conversion/DSSTPropagatorBuilder.java
+++ src/main/java/org/orekit/propagation/conversion/DSSTPropagatorBuilder.java
@@ -27,6 +27,7 @@ import org.orekit.estimation.leastsquares.DSSTModel;
 import org.orekit.estimation.leastsquares.ModelObserver;
 import org.orekit.estimation.measurements.ObservedMeasurement;
 import org.orekit.orbits.EquinoctialOrbit;
+import org.orekit.orbits.Orbit;
 import org.orekit.orbits.OrbitType;
 import org.orekit.orbits.PositionAngle;
 import org.orekit.propagation.Propagator;
@@ -66,7 +67,7 @@ public class DSSTPropagatorBuilder extends AbstractPropagatorBuilder implements 
      * (typically set to the expected standard deviation of the position)
      * @exception OrekitException if parameters drivers cannot be scaled
      */
-    public DSSTPropagatorBuilder(final EquinoctialOrbit referenceOrbit,
+    public DSSTPropagatorBuilder(final Orbit referenceOrbit,
                                  final ODEIntegratorBuilder builder,
                                  final double positionScale)
         throws OrekitException {
