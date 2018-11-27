f14296bab16c9010ab7469e808dc8845c8f256ba
Luc Maisonobe <luc@orekit.org>
2018-11-27 17:17:43 +0100

    Added builder for AngularAzEl measurements.

---
+++ src/main/java/org/orekit/estimation/measurements/generation/AngularAzElBuilder.java
@@ -0,0 +1,79 @@
+/* Copyright 2002-2018 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.estimation.measurements.generation;
+
+import org.hipparchus.random.CorrelatedRandomVectorGenerator;
+import org.orekit.estimation.measurements.AngularAzEl;
+import org.orekit.estimation.measurements.GroundStation;
+import org.orekit.propagation.SpacecraftState;
+
+
+/** Builder for {@link AngularAzEl} measurements.
+ * @author Luc Maisonobe
+ * @since 9.3
+ */
+public class AngularAzElBuilder extends AbstractMeasurementBuilder<AngularAzEl> {
+
+    /** Ground station from which measurement is performed. */
+    private final GroundStation station;
+
+    /** Simmple constructor.
+     * @param noiseSource noise source, may be null for generating perfect measurements
+     * @param station ground station from which measurement is performed
+     * @param sigma theoretical standard deviation
+     * @param baseWeight base weight
+     * @param propagatorIndex index of the propagator related to this measurement
+     */
+    public AngularAzElBuilder(final CorrelatedRandomVectorGenerator noiseSource,
+                        final GroundStation station,
+                        final double[] sigma, final double[] baseWeight,
+                        final int propagatorIndex) {
+        super(noiseSource, sigma, baseWeight, propagatorIndex);
+        this.station = station;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public AngularAzEl build(final SpacecraftState... states) {
+
+        final int propagatorIndex   = getPropagatorsIndices()[0];
+        final double[] sigma        = getTheoreticalStandardDeviation();
+        final double[] baseWeight   = getBaseWeight();
+        final SpacecraftState state = states[propagatorIndex];
+
+        // create a dummy measurement
+        final AngularAzEl dummy = new AngularAzEl(station, state.getDate(),
+                                                  new double[] {
+                                                      Double.NaN, Double.NaN
+                                                  }, sigma, baseWeight, propagatorIndex);
+
+        // estimate the perfect value of the measurement
+        final double[] angular = dummy.estimate(0, 0, states).getEstimatedValue();
+
+        // add the noise
+        final double[] noise = getNoise();
+        if (noise != null) {
+            angular[0] += noise[0];
+            angular[1] += noise[1];
+        }
+
+        // generate measurement
+        return new AngularAzEl(station, state.getDate(), angular, sigma, baseWeight, propagatorIndex);
+
+    }
+
+}
