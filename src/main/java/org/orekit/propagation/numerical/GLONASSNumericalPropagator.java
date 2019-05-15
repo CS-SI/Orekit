dec9379c87314db6d9b1fed54e2473ba40c72641
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2019-05-15 18:19:36 +0200

    Fixed a forgot issue.

--- src/main/java/org/orekit/propagation/numerical/GLONASSNumericalPropagator.java
+++ src/main/java/org/orekit/propagation/numerical/GLONASSNumericalPropagator.java
@@ -506,7 +506,7 @@ public class GLONASSNumericalPropagator extends AbstractIntegratedPropagator {
         double term = E;
         double d    = 0;
         // the inequality test below IS intentional and should NOT be replaced by a check with a small tolerance
-        for (double x0 = Double.NaN; x != x0;) {
+        for (double x0 = Double.NaN; !Double.valueOf(x).equals(Double.valueOf(x0));) {
             d += 2;
             term *= mE2 / (d * (d + 1));
             x0 = x;
