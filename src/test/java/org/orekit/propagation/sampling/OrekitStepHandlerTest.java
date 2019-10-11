e61b933856393bbc18d44a98ef42852252df28b9
Luc Maisonobe <luc@orekit.org>
2019-10-11 14:28:55 +0200

    Removed spurious import.

--- src/test/java/org/orekit/propagation/sampling/OrekitStepHandlerTest.java
+++ src/test/java/org/orekit/propagation/sampling/OrekitStepHandlerTest.java
@@ -18,7 +18,6 @@ package org.orekit.propagation.sampling;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
