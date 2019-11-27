1a9ce5c6048f3b0e20d901aba9e17804e919b700
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2019-11-27 10:23:07 +0100

    Added default constructors for DSSTZonal and DSSTTesseral.
    Fixes issue #625

--- src/main/java/org/orekit/propagation/semianalytical/dsst/forces/DSSTZonal.java
+++ src/main/java/org/orekit/propagation/semianalytical/dsst/forces/DSSTZonal.java
@@ -147,6 +147,24 @@ public class DSSTZonal implements DSSTForceModel {
     /** Flag for force model initialization with field elements. */
     private boolean pendingInitialization;
 
+    /** Constructor with default reference values.
+     * <p>
+     * When this constructor is used, maximum allowed values are used
+     * for the short periodic coefficients:
+     * </p>
+     * <ul>
+     *    <li> {@link #maxDegreeShortPeriodics} is set to {@code provider.getMaxDegree()} </li>
+     *    <li> {@link #maxEccPowShortPeriodics} is set to {@code min(provider.getMaxDegree() - 1, 4)}.
+     *         This parameter should not exceed 4 as higher values will exceed computer capacity </li>
+     *    <li> {@link #maxFrequencyShortPeriodics} is set to {@code 2 * provider.getMaxDegree() + 1} </li>
+     * </ul>
+     * @param provider provider for spherical harmonics
+     * @since 10.1
+     */
+    public DSSTZonal(final UnnormalizedSphericalHarmonicsProvider provider) {
+        this(provider, provider.getMaxDegree(), FastMath.min(4, provider.getMaxDegree() - 1), 2 * provider.getMaxDegree() + 1);
+    }
+
     /** Simple constructor.
      * @param provider provider for spherical harmonics
      * @param maxDegreeShortPeriodics maximum degree to consider for short periodics zonal harmonics potential
