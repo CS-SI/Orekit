d920db1ed5afcb945e53e40521ceb382d799952e
Luc Maisonobe <luc@orekit.org>
2020-01-17 15:45:18 +0100

    Updated CS identity from CS Systèmes d'Information to CS Group.

--- README.md
+++ README.md
@@ -83,6 +83,6 @@ More detailed information is available in the
 
 ## License
 
-Orekit is licensed by [CS Systèmes d'Information](https://www.c-s.fr/) under
+Orekit is licensed by [CS Group](https://www.c-s.fr/) under
 the [Apache License, version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
 A copy of this license is provided in the [LICENSE.txt](LICENSE.txt) file.
--- src/test/java/org/orekit/models/earth/ionosphere/GlobalIonosphereMapModelTest.java
+++ src/test/java/org/orekit/models/earth/ionosphere/GlobalIonosphereMapModelTest.java
@@ -1,5 +1,5 @@
-/* Copyright 2002-2020 CS Systèmes d'Information
- * Licensed to CS Systèmes d'Information (CS) under one or more
+/* Copyright 2002-2020 CS Group
+ * Licensed to CS Group (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * CS licenses this file to You under the Apache License, Version 2.0
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 /*
- * Copyright 2018-2019 CS Systèmes d'Information.
+ * Copyright 2018-2019 CS Group.
  * All rights reserved.
  */
 package org.orekit.models.earth.ionosphere;
