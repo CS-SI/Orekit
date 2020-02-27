d1772fbc6cb949b0af2d9fbf4d9a709b8d8bd1e8
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2020-02-27 13:42:20 +0100

    Removed duplication in ADM and ODM files.

--- src/main/java/org/orekit/files/ccsds/ADMFile.java
+++ src/main/java/org/orekit/files/ccsds/ADMFile.java
@@ -16,186 +16,25 @@
  */
 package org.orekit.files.ccsds;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.orekit.data.DataContext;
-import org.orekit.errors.OrekitException;
-import org.orekit.errors.OrekitMessages;
-import org.orekit.time.AbsoluteDate;
-import org.orekit.utils.IERSConventions;
-
 /**
  * The ADMFile (Attitude Data Message) class represents any of the two attitude
  * messages used by the CCSDS, (i.e. the Attitude Parameter Message (APM),
  * and the Attitude Ephemeris Message (AEM). It contains the information of the message's
  * header and configuration data (set in the parser).
+ * <p>
+ * This class has no specific methods. It is used, in particular, to distinguish
+ * between the Orbit Data Message and the Attitude Data Message.
+ * </p>
  * @author Bryan Cazabonne
  * @since 10.2
  */
-public abstract class ADMFile {
-
-    /** CCSDS Format version. */
-    private double formatVersion;
-
-    /** Header comments. The list contains a string for each line of comment. */
-    private List<String> headerComment;
-
-    /** File creation date and time in UTC. */
-    private AbsoluteDate creationDate;
-
-    /** Creating agency or operator. */
-    private String originator;
-
-    /** Data context. */
-    private DataContext dataContext;
-
-    /** IERS conventions used. */
-    private IERSConventions conventions;
-
-    /** Gravitational coefficient. */
-    private double mu;
-
-    /** Initial Date for MET or MRT time systems. */
-    private AbsoluteDate missionReferenceDate;
+public abstract class ADMFile extends NDMFile {
 
     /**
      * Constructor.
      */
     public ADMFile() {
-        mu = Double.NaN;
-    }
-
-    /**
-     * Get the used gravitational coefficient.
-     * @return the coefficient
-     */
-    public double getMu() {
-        return mu;
-    }
-
-    /**
-     * Set the used gravitational coefficient.
-     * @param mu the coefficient to set
-     */
-    void setMu(final double mu) {
-        this.mu = mu;
-    }
-
-    /**
-     * Get the CCSDS ADM (APM or AEM) format version.
-     * @return format version
-     */
-    public double getFormatVersion() {
-        return formatVersion;
-    }
-
-    /**
-     * Set the CCSDS ADM (APM or AEM) format version.
-     * @param formatVersion the format version to be set
-     */
-    void setFormatVersion(final double formatVersion) {
-        this.formatVersion = formatVersion;
-    }
-
-    /**
-     * Get the header comment.
-     * @return header comment
-     */
-    public List<String> getHeaderComment() {
-        return headerComment;
-    }
-
-    /**
-     * Set the header comment.
-     * @param headerComment header comment
-     */
-    void setHeaderComment(final List<String> headerComment) {
-        this.headerComment = new ArrayList<String>(headerComment);
-    }
-
-    /**
-     * Get the file creation date and time in UTC.
-     * @return the file creation date and time in UTC.
-     */
-    public AbsoluteDate getCreationDate() {
-        return creationDate;
-    }
-
-    /**
-     * Set the file creation date and time in UTC.
-     * @param creationDate the creation date to be set
-     */
-    void setCreationDate(final AbsoluteDate creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    /**
-     * Get the file originator.
-     * @return originator the file originator.
-     */
-    public String getOriginator() {
-        return originator;
-    }
-
-    /**
-     * Set the file originator.
-     * @param originator the originator to be set
-     */
-    void setOriginator(final String originator) {
-        this.originator = originator;
-    }
-
-    /**
-     * Get IERS conventions.
-     * @return conventions IERS conventions
-     */
-    public IERSConventions getConventions() {
-        if (conventions != null) {
-            return conventions;
-        } else {
-            throw new OrekitException(OrekitMessages.CCSDS_UNKNOWN_CONVENTIONS);
-        }
-    }
-
-    /**
-     * Set IERS conventions.
-     * @param conventions IERS conventions to be set
-     */
-    void setConventions(final IERSConventions conventions) {
-        this.conventions = conventions;
-    }
-
-    /**
-     * Get reference date for Mission Elapsed Time and Mission Relative Time time systems.
-     * @return the reference date
-     */
-    public AbsoluteDate getMissionReferenceDate() {
-        return missionReferenceDate;
-    }
-
-    /**
-     * Set reference date for Mission Elapsed Time and Mission Relative Time time systems.
-     * @param missionReferenceDate reference date for Mission Elapsed Time and Mission Relative Time time systems.
-     */
-    void setMissionReferenceDate(final AbsoluteDate missionReferenceDate) {
-        this.missionReferenceDate = missionReferenceDate;
-    }
-
-    /**
-     * Get the data context.
-     * @return the data context used for creating frames, time scales, etc.
-     */
-    public DataContext getDataContext() {
-        return dataContext;
-    }
-
-    /**
-     * Set the data context.
-     * @param dataContext used for creating frames, time scales, etc.
-     */
-    void setDataContext(final DataContext dataContext) {
-        this.dataContext = dataContext;
+        // Do nothing
     }
 
 }
