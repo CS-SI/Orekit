c294cf836acff8bf33726c9b0aa6f949b2aad20c
David Soulard <david.soulard@c-s.fr>
2020-03-24 10:50:13 +0100

    Added measurement pre-processing filters.

---
+++ src/main/java/org/orekit/estimation/measurements/pretreatment/ElevationFilter.java
@@ -0,0 +1,49 @@
+/* Copyright 2002-2019 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.estimation.measurements.pretreatment;
+
+import org.orekit.estimation.measurements.GroundStation;
+import org.orekit.estimation.measurements.ObservedMeasurement;
+import org.orekit.propagation.SpacecraftState;
+
+public class ElevationFilter<T extends ObservedMeasurement<T>> implements PreProcessingFilter {
+
+    /** Elevation threshold under which the measurement will be rejected (angle in rad). */
+    private final double threshold;
+
+    /** Ground station considered. */
+    private final GroundStation station;
+
+
+    /**Contructor.
+     * @param station considered by the filter
+     * @param threshold minimum elevation for an measurements to be accepted (rad)
+     */
+    public ElevationFilter(final GroundStation station, final double threshold) {
+        this.station        = station;
+        this.threshold      = threshold;
+    }
+
+    @Override
+    public void filter(final ObservedMeasurement<?> measurement, final SpacecraftState sc) {
+        final double trueElevation      = station.getBaseFrame().getElevation(sc.getPVCoordinates().getPosition(),
+                                                                              sc.getFrame(), sc.getDate());
+        if (trueElevation < threshold) {
+            measurement.setEnabled(false);
+        }
+    }
+}
---
+++ src/main/java/org/orekit/estimation/measurements/pretreatment/ResidualsFilter.java
@@ -0,0 +1,50 @@
+/* Copyright 2002-2019 CS Systèmes d'Information
+ * Licensed to CS Systèmes d'Information (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.estimation.measurements.pretreatment;
+
+import org.hipparchus.util.FastMath;
+import org.orekit.estimation.measurements.EstimatedMeasurement;
+import org.orekit.estimation.measurements.ObservedMeasurement;
+import org.orekit.propagation.SpacecraftState;
+
+public class ResidualsFilter<T extends ObservedMeasurement<?>> implements PreProcessingFilter {
+
+    /** Elevation threshold under which the measurement will be rejected. */
+    private final double threshold;
+
+    /**
+     * Contructor.
+     * @param threshold maximum value between estimated and obrserved value in order to the measurement to be accepted
+     */
+    public ResidualsFilter(final double threshold) {
+        this.threshold  = threshold;
+    }
+
+    @Override
+    public void filter(final ObservedMeasurement<?> measurement, final SpacecraftState currentSC) {
+        final SpacecraftState[] sc              = new SpacecraftState[] {currentSC};
+        final EstimatedMeasurement<?> estimated = measurement.estimate(0, 0, sc);
+        final double[] observedValue            = measurement.getObservedValue();
+        final double[] estimatedValue           = estimated.getEstimatedValue();
+        final double[] sigma                    = measurement.getTheoreticalStandardDeviation();
+        for (int i = 0; i < observedValue.length; i++) {
+            if (FastMath.abs(observedValue[i] - estimatedValue[i]) > threshold * sigma[i]) {
+                measurement.setEnabled(false);
+            }
+        }
+    }
+}
