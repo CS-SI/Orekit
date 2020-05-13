3ef603bf842c11fb7f9034b120b368f88e5e91e6
Evan Ward <evan.ward@nrl.navy.mil>
2020-05-13 12:25:29 -0400

    Fix invalid times in AbsoluteDate.getComponents
    Also fix for FieldAbsoluteDate.
    
    Fix #681, Fix #676

--- src/main/java/org/orekit/time/AbsoluteDate.java
+++ src/main/java/org/orekit/time/AbsoluteDate.java
@@ -1088,13 +1088,12 @@ public class AbsoluteDate
 
         // extract calendar elements
         final DateComponents dateComponents = new DateComponents(DateComponents.J2000_EPOCH, date);
-        TimeComponents timeComponents = new TimeComponents((int) time, offset2000B);
 
-        if (timeScale.insideLeap(this)) {
-            // fix the seconds number to take the leap into account
-            timeComponents = new TimeComponents(timeComponents.getHour(), timeComponents.getMinute(),
-                                                timeComponents.getSecond() + timeScale.getLeap(this));
-        }
+        // extract time element, accounting for leap seconds
+        final double leap = timeScale.insideLeap(this) ? timeScale.getLeap(this) : 0;
+        final int minuteDuration = timeScale.minuteDuration(this);
+        final TimeComponents timeComponents =
+                TimeComponents.fromSeconds((int) time, offset2000B, leap, minuteDuration);
 
         // build the components
         return new DateTimeComponents(dateComponents, timeComponents);
