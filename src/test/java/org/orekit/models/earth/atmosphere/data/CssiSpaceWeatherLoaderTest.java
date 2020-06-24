0c483f1b84cc657feb6c39143c469dd0f7b22fa7
Clément Jonglez <clement@jonglez.space>
2020-06-24 16:00:37 +0200

    Update copyright header

--- src/test/java/org/orekit/models/earth/atmosphere/data/CssiSpaceWeatherLoaderTest.java
+++ src/test/java/org/orekit/models/earth/atmosphere/data/CssiSpaceWeatherLoaderTest.java
@@ -1,11 +1,13 @@
-/*
- * Copyright 2020 CS Group.
+/* 
+ * Copyright 2020 Clément Jonglez
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Clément Jonglez licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
