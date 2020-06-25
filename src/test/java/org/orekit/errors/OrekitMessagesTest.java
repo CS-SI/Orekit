960e1a084dfdfa3990456d2adda405d7cb915d95
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2020-06-25 09:31:14 +0200

    Fixed failure in Orekit errors tests.

--- src/test/java/org/orekit/errors/OrekitMessagesTest.java
+++ src/test/java/org/orekit/errors/OrekitMessagesTest.java
@@ -30,7 +30,7 @@ public class OrekitMessagesTest {
 
     @Test
     public void testMessageNumber() {
-        Assert.assertEquals(208, OrekitMessages.values().length);
+        Assert.assertEquals(209, OrekitMessages.values().length);
     }
 
     @Test
