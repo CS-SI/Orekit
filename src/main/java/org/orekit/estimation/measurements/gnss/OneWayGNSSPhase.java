ea608355fddca487a22f7bc628a507ae1c8e77b7
Bryan Cazabonne <bryan.cazabonne@c-s.fr>
2020-10-08 17:21:00 +0200

    Added support for one-way GNSS phase measurement.

--- src/main/java/org/orekit/estimation/measurements/gnss/OneWayGNSSPhase.java
+++ src/main/java/org/orekit/estimation/measurements/gnss/OneWayGNSSPhase.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import org.hipparchus.analysis.differentiation.Gradient;
 import org.orekit.estimation.measurements.AbstractMeasurement;
 import org.orekit.estimation.measurements.EstimatedMeasurement;
-import org.orekit.estimation.measurements.InterSatellitesRange;
 import org.orekit.estimation.measurements.ObservableSatellite;
 import org.orekit.propagation.SpacecraftState;
 import org.orekit.time.AbsoluteDate;
@@ -34,26 +33,32 @@ import org.orekit.utils.ParameterDriver;
 import org.orekit.utils.TimeStampedFieldPVCoordinates;
 import org.orekit.utils.TimeStampedPVCoordinates;
 
-/** One-way GNSS range measurement.
+/** One-way GNSS phase measurement.
  * <p>
  * This class can be used in precise orbit determination applications
- * for modeling a range measurement between a GNSS satellite (emitter)
+ * for modeling a phase measurement between a GNSS satellite (emitter)
  * and a LEO satellite (receiver).
  * <p>
- * The one-way GNSS range measurement assumes knowledge of the orbit and
+ * The one-way GNSS phase measurement assumes knowledge of the orbit and
  * the clock offset of the emitting GNSS satellite. For instance, it is
  * possible to use a SP3 file or a GNSS navigation message to recover
  * the satellite's orbit and clock.
  * <p>
- * This class is very similar to {@link InterSatellitesRange} measurement
- * class. However, using the one-way GNSS range measurement, the orbit and clock
+ * This class is very similar to {@link InterSatellitesPhase} measurement
+ * class. However, using the one-way GNSS phase measurement, the orbit and clock
  * of the emitting GNSS satellite are <b>NOT</b> estimated simultaneously with
  * LEO satellite coordinates.
  *
  * @author Bryan Cazabonne
  * @since 10.3
  */
-public class OneWayGNSSRange extends AbstractMeasurement<OneWayGNSSRange> {
+public class OneWayGNSSPhase extends AbstractMeasurement<OneWayGNSSPhase> {
+
+    /** Name for ambiguity driver. */
+    public static final String AMBIGUITY_NAME = "ambiguity";
+
+    /** Driver for ambiguity. */
+    private final ParameterDriver ambiguityDriver;
 
     /** Emitting satellite. */
     private final PVCoordinatesProvider remote;
@@ -61,58 +66,78 @@ public class OneWayGNSSRange extends AbstractMeasurement<OneWayGNSSRange> {
     /** Clock offset of the emitting satellite. */
     private final double dtRemote;
 
+    /** Wavelength of the phase observed value [m]. */
+    private final double wavelength;
+
     /** Simple constructor.
      * @param remote provider for GNSS satellite which simply emits the signal
      * @param dtRemote clock offset of the GNSS satellite, in seconds
      * @param date date of the measurement
-     * @param range observed value
+     * @param phase observed value, in cycles
+     * @param wavelength phase observed value wavelength, in meters
      * @param sigma theoretical standard deviation
      * @param baseWeight base weight
      * @param local satellite which receives the signal and perform the measurement
      */
-    public OneWayGNSSRange(final PVCoordinatesProvider remote,
+    public OneWayGNSSPhase(final PVCoordinatesProvider remote,
                            final double dtRemote,
                            final AbsoluteDate date,
-                           final double range, final double sigma,
+                           final double phase, final double wavelength, final double sigma,
                            final double baseWeight, final ObservableSatellite local) {
         // Call super constructor
-        super(date, range, sigma, baseWeight, Arrays.asList(local));
+        super(date, phase, sigma, baseWeight, Arrays.asList(local));
+
+        // Initialize phase ambiguity driver
+        ambiguityDriver = new ParameterDriver(AMBIGUITY_NAME, 0.0, 1.0,
+                                              Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
         // The local satellite clock offset affects the measurement
+        addParameterDriver(ambiguityDriver);
         addParameterDriver(local.getClockOffsetDriver());
+
         // Initialise fields
-        this.dtRemote = dtRemote;
-        this.remote   = remote;
+        this.dtRemote   = dtRemote;
+        this.remote     = remote;
+        this.wavelength = wavelength;
+    }
+
+    /** Get the wavelength.
+     * @return wavelength (m)
+     */
+    public double getWavelength() {
+        return wavelength;
     }
 
     /** {@inheritDoc} */
     @Override
-    protected EstimatedMeasurement<OneWayGNSSRange> theoreticalEvaluation(final int iteration,
+    protected EstimatedMeasurement<OneWayGNSSPhase> theoreticalEvaluation(final int iteration,
                                                                           final int evaluation,
                                                                           final SpacecraftState[] states) {
 
-        // Range derivatives are computed with respect to spacecraft state in inertial frame
+        // Phase derivatives are computed with respect to spacecrafts states in inertial frame
         // Parameters:
-        //  - 0..2 - Position of the spacecraft in inertial frame
-        //  - 3..5 - Velocity of the spacecraft in inertial frame
-        //  - 6..n - measurements parameters (clock offset, etc)
-        int nbEstimatedParams = 6;
-        final Map<String, Integer> parameterIndices = new HashMap<>();
-        for (ParameterDriver measurementDriver : getParametersDrivers()) {
-            if (measurementDriver.isSelected()) {
-                parameterIndices.put(measurementDriver.getName(), nbEstimatedParams++);
+        //  - 0..2  - Position of the receiver satellite in inertial frame
+        //  - 3..5  - Velocity of the receiver satellite in inertial frame
+        //  - 6..n  - Measurement parameters: ambiguity and clock offset
+        int nbEstimatedParamsPhase = 6;
+        final Map<String, Integer> parameterIndicesPhase = new HashMap<>();
+        for (ParameterDriver phaseMeasurementDriver : getParametersDrivers()) {
+            if (phaseMeasurementDriver.isSelected()) {
+                parameterIndicesPhase.put(phaseMeasurementDriver.getName(), nbEstimatedParamsPhase++);
             }
         }
 
-        // Coordinates of both satellites in local satellite frame
+        // Coordinates of both satellites
         final SpacecraftState localState  = states[0];
-        final TimeStampedFieldPVCoordinates<Gradient> pvaLocal  = getCoordinates(localState, 0, nbEstimatedParams);
+        final TimeStampedFieldPVCoordinates<Gradient> pvaLocal  = getCoordinates(localState, 0, nbEstimatedParamsPhase);
         final TimeStampedPVCoordinates                pvaRemote = remote.getPVCoordinates(getDate(), localState.getFrame());
 
         // Downlink delay
-        final Gradient dtLocal = getSatellites().get(0).getClockOffsetDriver().getValue(nbEstimatedParams, parameterIndices);
+        final Gradient dtLocal = getSatellites().get(0).getClockOffsetDriver().getValue(nbEstimatedParamsPhase, parameterIndicesPhase);
         final FieldAbsoluteDate<Gradient> arrivalDate = new FieldAbsoluteDate<>(getDate(), dtLocal.negate());
 
-        final TimeStampedFieldPVCoordinates<Gradient> s1Downlink = pvaLocal.shiftedBy(arrivalDate.durationFrom(pvaLocal.getDate()));
+        final TimeStampedFieldPVCoordinates<Gradient> s1Downlink =
+                        pvaLocal.shiftedBy(arrivalDate.durationFrom(pvaLocal.getDate()));
         final Gradient tauD = signalTimeOfFlight(new TimeStampedFieldPVCoordinates<>(pvaRemote.getDate(), dtLocal.getField().getOne(), pvaRemote),
                                                  s1Downlink.getPosition(), arrivalDate);
 
@@ -120,8 +145,8 @@ public class OneWayGNSSRange extends AbstractMeasurement<OneWayGNSSRange> {
         final double   delta      = getDate().durationFrom(pvaRemote.getDate());
         final Gradient deltaMTauD = tauD.negate().add(delta);
 
-        // Estimated measurement
-        final EstimatedMeasurement<OneWayGNSSRange> estimatedRange =
+        // prepare the evaluation
+        final EstimatedMeasurement<OneWayGNSSPhase> estimatedPhase =
                         new EstimatedMeasurement<>(this, iteration, evaluation,
                                                    new SpacecraftState[] {
                                                        localState.shiftedBy(deltaMTauD.getValue())
@@ -130,24 +155,26 @@ public class OneWayGNSSRange extends AbstractMeasurement<OneWayGNSSRange> {
                                                        localState.shiftedBy(delta).getPVCoordinates()
                                                    });
 
-        // Range value
-        final Gradient range            = tauD.add(dtLocal).subtract(dtRemote).multiply(Constants.SPEED_OF_LIGHT);
-        final double[] rangeDerivatives = range.getGradient();
+        // Phase value
+        final double   cOverLambda      = Constants.SPEED_OF_LIGHT / wavelength;
+        final Gradient ambiguity        = ambiguityDriver.getValue(nbEstimatedParamsPhase, parameterIndicesPhase);
+        final Gradient phase            = tauD.add(dtLocal).subtract(dtRemote).multiply(cOverLambda).add(ambiguity);
+        final double[] phaseDerivatives = phase.getGradient();
 
         // Set value and state derivatives of the estimated measurement
-        estimatedRange.setEstimatedValue(range.getValue());
-        estimatedRange.setStateDerivatives(0, Arrays.copyOfRange(rangeDerivatives, 0,  6));
+        estimatedPhase.setEstimatedValue(phase.getValue());
+        estimatedPhase.setStateDerivatives(0, Arrays.copyOfRange(phaseDerivatives, 0,  6));
 
         // Set partial derivatives with respect to parameters
-        for (final ParameterDriver measurementDriver : getParametersDrivers()) {
-            final Integer index = parameterIndices.get(measurementDriver.getName());
+        for (final ParameterDriver phaseMeasurementDriver : getParametersDrivers()) {
+            final Integer index = parameterIndicesPhase.get(phaseMeasurementDriver.getName());
             if (index != null) {
-                estimatedRange.setParameterDerivatives(measurementDriver, rangeDerivatives[index]);
+                estimatedPhase.setParameterDerivatives(phaseMeasurementDriver, phaseDerivatives[index]);
             }
         }
 
         // Return the estimated measurement
-        return estimatedRange;
+        return estimatedPhase;
 
     }
 
