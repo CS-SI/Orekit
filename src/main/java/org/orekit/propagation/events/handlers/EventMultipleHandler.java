3423718c700a30e0c39948a19cc54d5072121be7
Lara HUE <lara.hue@airbus.com>
2020-11-05 16:20:09 +0100

    EventMultipleHandler minor modification post merge request

--- src/main/java/org/orekit/propagation/events/handlers/EventMultipleHandler.java
+++ src/main/java/org/orekit/propagation/events/handlers/EventMultipleHandler.java
@@ -1,4 +1,4 @@
-/* Copyright 2013 Applied Defense Solutions, Inc.
+/* Copyright 2020 Airbus Defence and Space
  * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -41,14 +41,20 @@ import org.orekit.time.AbsoluteDate;
  * @param <D> object type of the detector which is linked to the handler
  * @since 10.3
  */
-public class MultipleEventHandler<D extends EventDetector> implements EventHandler<D> {
+public class EventMultipleHandler<D extends EventDetector> implements EventHandler<D> {
 
     /** Default list of handlers for event overrides. */
-    private List<EventHandler<D>> handlers = new ArrayList<>();
+    private List<EventHandler<D>> handlers;
 
     /** List of handlers whose Action returned is RESET_STATE. */
-    private List<EventHandler<D>> resetStateHandlers = new ArrayList<>();
+    private List<EventHandler<D>> resetStateHandlers;
 
+    /** Constructor with list initialisation */
+    public EventMultipleHandler() {
+        handlers = new ArrayList<>();
+        resetStateHandlers = new ArrayList<>();
+    }
+    
     /** Initialize event handler at the start of a propagation.
      * <p>
      * This method is called once at the start of the propagation. It
@@ -81,8 +87,9 @@ public class MultipleEventHandler<D extends EventDetector> implements EventHandl
      * The MultipleEventHandler class implies a different behaviour on event detections 
      * than with other handlers :
      * Without the MultipleEventHandler, there is a total order on event occurrences. 
-     * Handlers H1, H2, ... that are associated with different instances of {@link AbstractDetector} 
-     * are successively called and Action from H1 can prevent H2 from happening if H1 returned {@link Action#RESET_STATE resetState}. 
+     * Handlers H1, H2, ... that are associated with different instances of 
+     * {@link AbstractDetector} are successively called and Action from H1 can prevent 
+     * H2 from happening if H1 returned {@link Action#RESET_STATE resetState}. 
      * With the MultipleEventHandler class, when event E occurs, all methods eventOccurred
      * of Handlers H1, H2... from MultiEventHandler attributes are called, then Action is decided.
      *
@@ -94,14 +101,21 @@ public class MultipleEventHandler<D extends EventDetector> implements EventHandl
      */
     @Override
     public Action eventOccurred(SpacecraftState s, D detector, boolean increasing) {
-        Map<EventHandler<D>, Action> actions = handlers.stream().collect(Collectors.toMap(Function.identity(), handler -> handler.eventOccurred(s, detector, increasing)));
+        Map<EventHandler<D>, Action> actions = 
+                handlers.stream()
+                        .collect(Collectors.toMap(Function.identity(), 
+                                                  handler -> handler.eventOccurred(s, detector, increasing)));
 
         if (actions.containsValue(Action.STOP)) {
             return Action.STOP;
         }
 
         if (actions.containsValue(Action.RESET_STATE)) {
-            resetStateHandlers = actions.entrySet().stream().filter(entry -> Action.RESET_STATE.equals(entry.getValue())).map(Map.Entry::getKey).collect(Collectors.toList());
+            resetStateHandlers = actions.entrySet()
+                                        .stream()
+                                        .filter(entry -> Action.RESET_STATE.equals(entry.getValue()))
+                                        .map(Map.Entry::getKey)
+                                        .collect(Collectors.toList());
             return Action.RESET_STATE;
         }
 
@@ -117,21 +131,9 @@ public class MultipleEventHandler<D extends EventDetector> implements EventHandl
     }
 
     /** Reset the state prior to continue propagation.
-     * <p>This method is called after the step handler has returned and
-     * before the next step is started, but only when {@link
-     * #eventOccurred} has itself returned the {@link Action#RESET_STATE resetState} 
-     * for at least one handler of the list, except if one handler has return {@link Action#STOP stop}, 
-     * see priorities in {@link #eventOccurred}
-     * It allows the user to reset the state for the next step,
-     * without perturbing the step handler of the finishing step. If the
-     * {@link #eventOccurred} never returns the {@link Action#RESET_STATE resetState}
-     * indicator, this function will never be called, and it is safe to simply return null.</p>
-     * <p>
-     * The default implementation simply return its argument.
-     * </p>
      * <p> 
-     * All handlers that return {@link Action#RESET_STATE resetState} when calling {@link #eventOccurred} are
-     * saved in resetStateHandlers. Their methods resetState are successively called.
+     * All handlers that return {@link Action#RESET_STATE resetState} when calling {@link #eventOccurred}
+     * are saved in resetStateHandlers. Their methods resetState are successively called.
      * The order for calling resetState methods is the order in which handlers are added.
      * </p>
      * @param detector object with appropriate type that can be used in determining correct return state
@@ -151,7 +153,7 @@ public class MultipleEventHandler<D extends EventDetector> implements EventHandl
      * @param handler handler associated with D detector
      *
      */
-    public MultipleEventHandler<D> addHandler(EventHandler<D> handler) {
+    public EventMultipleHandler<D> addHandler(EventHandler<D> handler) {
         handlers.add(handler);
         return this;
     }
@@ -161,7 +163,7 @@ public class MultipleEventHandler<D extends EventDetector> implements EventHandl
      *
      */
     @SafeVarargs // this method is safe
-    public final MultipleEventHandler<D> addHandlers(EventHandler<D>... newHandlers) {
+    public final EventMultipleHandler<D> addHandlers(EventHandler<D>... newHandlers) {
     Arrays.stream(newHandlers).forEach(this::addHandler);
         return this;
     }
