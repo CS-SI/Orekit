a7e4576bdec3d252be117eb0b173e94b7c04cb35
Luc Maisonobe <luc@orekit.org>
2021-03-26 11:24:54 +0100

    Use an enumerate for OCM MAN_BASIS.

--- src/main/java/org/orekit/files/ccsds/ndm/odm/ocm/ManeuverHistoryMetadata.java
+++ src/main/java/org/orekit/files/ccsds/ndm/odm/ocm/ManeuverHistoryMetadata.java
@@ -48,7 +48,7 @@ public class ManeuverHistoryMetadata extends CommentsContainer {
     private String manNextID;
 
     /** Basis of this maneuver history data. */
-    private String manBasis;
+    private ManBasis manBasis;
 
     /** Identification number of the orbit determination or simulation upon which this maneuver is based. */
     private String manBasisID;
@@ -134,7 +134,7 @@ public class ManeuverHistoryMetadata extends CommentsContainer {
     ManeuverHistoryMetadata(final AbsoluteDate epochT0) {
         // we don't call the setXxx() methods in order to avoid
         // calling refuseFurtherComments as a side effect
-        manBasis            = "PLANNED";
+        manBasis            = ManBasis.PLANNED;
         manReferenceFrame   = new FrameFacade(null, null,
                                               OrbitRelativeFrame.TNW_INERTIAL, null,
                                               OrbitRelativeFrame.TNW_INERTIAL.name());
@@ -239,14 +239,14 @@ public class ManeuverHistoryMetadata extends CommentsContainer {
     /** Get basis of this maneuver history data.
      * @return basis of this maneuver history data
      */
-    public String getManBasis() {
+    public ManBasis getManBasis() {
         return manBasis;
     }
 
     /** Set basis of this maneuver history data.
      * @param manBasis basis of this maneuver history data
      */
-    public void setManBasis(final String manBasis) {
+    public void setManBasis(final ManBasis manBasis) {
         refuseFurtherComments();
         this.manBasis = manBasis;
     }
