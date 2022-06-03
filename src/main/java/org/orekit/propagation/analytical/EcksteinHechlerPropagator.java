06024fd1479e5e4e4ea113601745736c3fd28ff8
Luc Maisonobe <luc@orekit.org>
2022-06-03 18:24:11 +0200

    Raise too stringent convergence threshold in Eckstein-Hechler model.
    Fixes #936

--- src/main/java/org/orekit/propagation/analytical/EcksteinHechlerPropagator.java
+++ src/main/java/org/orekit/propagation/analytical/EcksteinHechlerPropagator.java
@@ -565,7 +565,7 @@ public class EcksteinHechlerPropagator extends AbstractAnalyticalPropagator {
         final double epsilon         = 1.0e-13;
         final double thresholdA      = epsilon * (1 + FastMath.abs(current.mean.getA()));
         final double thresholdE      = epsilon * (1 + current.mean.getE());
-        final double thresholdAngles = epsilon * FastMath.PI;
+        final double thresholdAngles = epsilon * MathUtils.TWO_PI;
 
         int i = 0;
         while (i++ < 100) {
