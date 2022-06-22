3c7a7785addc63fd63633be78064fcb5dc8ad3a1
gpierre <gpierre@PO18517LX>
2022-06-22 13:05:41 +0200

    Added tests for unscented Kalman estimation.

--- src/test/java/org/orekit/estimation/sequential/UnscentedKalmanEstimatorTest.java
+++ src/test/java/org/orekit/estimation/sequential/UnscentedKalmanEstimatorTest.java
@@ -89,13 +89,13 @@ public class UnscentedKalmanEstimatorTest {
         
         // Filter the measurements and check the results
         final double   expectedDeltaPos  = 0.;
-        final double   posEps            = 5.80e-4;
+        final double   posEps            = 4e-6;
         final double   expectedDeltaVel  = 0.;
-        final double   velEps            = 2.28e-4;
+        final double   velEps            = 2.28e-9;
         final double[] expectedsigmasPos = {0., 0., 0.};
-        final double   sigmaPosEps       = 1e-4;
+        final double   sigmaPosEps       = 1.1e-6;
         final double[] expectedSigmasVel = {0., 0., 0.};
-        final double   sigmaVelEps       = 1e-4;
+        final double   sigmaVelEps       = 1e-8;
         UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
                                            refOrbit, positionAngle,
                                            expectedDeltaPos, posEps,
@@ -184,9 +184,9 @@ public class UnscentedKalmanEstimatorTest {
         final double   expectedDeltaVel  = 0.;
         final double   velEps            = 8e-6;
         final double[] expectedsigmasPos = {0., 0., 0.};
-        final double   sigmaPosEps       = 0.5;
+        final double   sigmaPosEps       = 0.32;
         final double[] expectedSigmasVel = {0., 0., 0.};
-        final double   sigmaVelEps       = 0.5;
+        final double   sigmaVelEps       = 2e-4;
         UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
                                            refOrbit, positionAngle,
                                            expectedDeltaPos, posEps,
@@ -244,13 +244,13 @@ public class UnscentedKalmanEstimatorTest {
         
         // Filter the measurements and check the results
         final double   expectedDeltaPos  = 0.;
-        final double   posEps            = 5.80e-5;
+        final double   posEps            = 1.7e-7;
         final double   expectedDeltaVel  = 0.;
-        final double   velEps            = 2.28e-5;
+        final double   velEps            = 7e-11;
         final double[] expectedsigmasPos = {0., 0., 0.};
-        final double   sigmaPosEps       = 1e-6;
+        final double   sigmaPosEps       = 7.3e-7;
         final double[] expectedSigmasVel = {0., 0., 0.};
-        final double   sigmaVelEps       = 1e-6;
+        final double   sigmaVelEps       = 3.1e-11;
         UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
                                            refOrbit, positionAngle,
                                            expectedDeltaPos, posEps,
@@ -308,13 +308,13 @@ public class UnscentedKalmanEstimatorTest {
         
         // Filter the measurements and check the results
         final double   expectedDeltaPos  = 0.;
-        final double   posEps            = 5.80e-2;
+        final double   posEps            = 4.7e-7;
         final double   expectedDeltaVel  = 0.;
-        final double   velEps            = 2.28e-6;
+        final double   velEps            = 1.7e-10;
         final double[] expectedsigmasPos = {0., 0., 0.};
-        final double   sigmaPosEps       = 1e-6;
+        final double   sigmaPosEps       = 1.3e-7;
         final double[] expectedSigmasVel = {0., 0., 0.};
-        final double   sigmaVelEps       = 1e-6;
+        final double   sigmaVelEps       = 4.3e-11;
         UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
                                            refOrbit, positionAngle,
                                            expectedDeltaPos, posEps,
@@ -390,9 +390,9 @@ public class UnscentedKalmanEstimatorTest {
         
         // Filter the measurements and check the results
         final double   expectedDeltaPos  = 0.;
-        final double   posEps            = 1.5e-4;
+        final double   posEps            = 7.5e-6;
         final double   expectedDeltaVel  = 0.;
-        final double   velEps            = 5.1e-8;
+        final double   velEps            = 2.8e-9;
         final double[] expectedSigmasPos = {0.324407, 1.347014, 1.743326};
         final double   sigmaPosEps       = 1e-6;
         final double[] expectedSigmasVel = {2.85688e-4,  5.765933e-4, 5.056124e-4};
@@ -470,13 +470,13 @@ public class UnscentedKalmanEstimatorTest {
         
         // Filter the measurements and check the results
         final double   expectedDeltaPos  = 0.;
-        final double   posEps            = 9.1e-5;
+        final double   posEps            = 2.7e-5;
         final double   expectedDeltaVel  = 0.;
-        final double   velEps            = 1.2e-8;
+        final double   velEps            = 8.8e-9;
         final double[] expectedSigmasPos = {0.356902, 1.297507, 1.798551};
-        final double   sigmaPosEps       = 1.5e-2;
+        final double   sigmaPosEps       = 1.4e-2;
         final double[] expectedSigmasVel = {2.468745e-4, 5.810027e-4, 3.887394e-4};
-        final double   sigmaVelEps       = 1e-5;
+        final double   sigmaVelEps       = 9e-6;
         UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
                                            refOrbit, positionAngle,
                                            expectedDeltaPos, posEps,
@@ -485,5 +485,84 @@ public class UnscentedKalmanEstimatorTest {
                                            expectedSigmasVel, sigmaVelEps);
     }
     
+    /**
+     * Perfect azimuth/elevation measurements with a perfect start
+     */
+    @Test
+    public void testCircularAzimuthElevation() {
 
+        // Create context
+        Context context = UnscentedEstimationTestUtils.eccentricContext("regular-data:potential:tides");
+
+        // Create initial orbit and propagator builder
+        final OrbitType     orbitType     = OrbitType.CIRCULAR;
+        final PositionAngle positionAngle = PositionAngle.TRUE;
+        final boolean       perfectStart  = true;
+        final double        minStep       = 1.e-6;
+        final double        maxStep       = 60.;
+        final double        dP            = 1.;
+        final NumericalPropagatorBuilder propagatorBuilder =
+                        context.createBuilder(orbitType, positionAngle, perfectStart,
+                                              minStep, maxStep, dP);
+
+        // Create perfect range measurements
+        final Propagator propagator = UnscentedEstimationTestUtils.createPropagator(context.initialOrbit,
+                                                                           propagatorBuilder);
+        final List<ObservedMeasurement<?>> measurements =
+                UnscentedEstimationTestUtils.createMeasurements(propagator,
+                                                               new AngularAzElMeasurementCreator(context),
+                                                               1.0, 4.0, 60.0);
+
+        // Reference propagator for estimation performances
+        final NumericalPropagator referencePropagator = propagatorBuilder.
+                        buildPropagator(propagatorBuilder.getSelectedNormalizedParameters());
+        
+        // Reference position/velocity at last measurement date
+        final Orbit refOrbit = referencePropagator.
+                        propagate(measurements.get(measurements.size()-1).getDate()).getOrbit();
+
+        // Cartesian covariance matrix initialization
+        final RealMatrix cartesianP = MatrixUtils.createRealDiagonalMatrix(new double [] {
+            1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-10
+        });
+        
+        // Jacobian of the orbital parameters w/r to Cartesian
+        final Orbit initialOrbit = orbitType.convertType(context.initialOrbit);
+        final double[][] dYdC = new double[6][6];
+        initialOrbit.getJacobianWrtCartesian(PositionAngle.TRUE, dYdC);
+        final RealMatrix Jac = MatrixUtils.createRealMatrix(dYdC);
+        
+        // Initial covariance matrix
+        final RealMatrix initialP = Jac.multiply(cartesianP.multiply(Jac.transpose()));
+        
+        
+        // Process noise matrix
+        final RealMatrix cartesianQ = MatrixUtils.createRealDiagonalMatrix(new double [] {
+            1.e-6, 1.e-6, 1.e-6, 1.e-12, 1.e-12, 1.e-12
+        });
+        final RealMatrix Q = Jac.multiply(cartesianQ.multiply(Jac.transpose()));
+
+        
+        // Build the Kalman filter
+        final UnscentedKalmanEstimator kalman = new UnscentedKalmanEstimatorBuilder().
+                        addPropagationConfiguration(propagatorBuilder, new ConstantProcessNoise(initialP, Q)).
+                        build();
+        
+        // Filter the measurements and check the results
+        final double   expectedDeltaPos  = 0.;
+        final double   posEps            = 2.7e-5;
+        final double   expectedDeltaVel  = 0.;
+        final double   velEps            = 8.8e-9;
+        final double[] expectedSigmasPos = {0.356902, 1.297507, 1.798551};
+        final double   sigmaPosEps       = 3.5e-1;
+        final double[] expectedSigmasVel = {2.468745e-4, 5.810027e-4, 3.887394e-4};
+        final double   sigmaVelEps       = 1.2e-4;
+        UnscentedEstimationTestUtils.checkKalmanFit(context, kalman, measurements,
+                                           refOrbit, positionAngle,
+                                           expectedDeltaPos, posEps,
+                                           expectedDeltaVel, velEps,
+                                           expectedSigmasPos, sigmaPosEps,
+                                           expectedSigmasVel, sigmaVelEps);
+    }
+    
 }
