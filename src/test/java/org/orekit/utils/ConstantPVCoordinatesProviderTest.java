42bb5f73287d46018da4162d48197f4eba3051a3
Joe Reed <joseph.reed@gmail.com>
2022-07-19 09:29:19 -0400

    updated copyright

--- src/test/java/org/orekit/utils/ConstantPVCoordinatesProviderTest.java
+++ src/test/java/org/orekit/utils/ConstantPVCoordinatesProviderTest.java
@@ -1,8 +1,8 @@
-/* Copyright 2002-2022 CS GROUP
+/* Copyright 2002-2022 Joseph Reed
  * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * CS licenses this file to You under the Apache License, Version 2.0
+ * Joseph Reed licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -16,7 +16,6 @@
  */
 package org.orekit.utils;
 
-import java.util.Iterator;
 import java.util.PrimitiveIterator;
 import java.util.Random;
 import java.util.stream.DoubleStream;
--- src/test/java/org/orekit/utils/WaypointPVBuilderTest.java
+++ src/test/java/org/orekit/utils/WaypointPVBuilderTest.java
@@ -1,8 +1,8 @@
-/* Contributed in the public domain.
+/* Copyright 2002-2022 Joseph Reed
  * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * CS licenses this file to You under the Apache License, Version 2.0
+ * Joseph Reed licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
