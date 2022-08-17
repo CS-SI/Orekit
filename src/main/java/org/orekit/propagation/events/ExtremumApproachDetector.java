0f305847f3394fdf6d64124152b62b87df36f9d2
Vincent CUCCHIETTI <vincent.cucchietti@csgroup.eu>
2022-08-17 15:31:55 +0000

    Update src/main/java/org/orekit/propagation/events/ExtremumApproachDetector.java

--- src/main/java/org/orekit/propagation/events/ExtremumApproachDetector.java
+++ src/main/java/org/orekit/propagation/events/ExtremumApproachDetector.java
@@ -1,12 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
+ * CS licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 package org.orekit.propagation.events;
 
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
@@ -80,7 +81,7 @@ public class ExtremumApproachDetector extends AbstractDetector<ExtremumApproachD
     /** {@inheritDoc} */
     public double g(final SpacecraftState s) {
         final PVCoordinates deltaPV = deltaPV(s);
-        return Vector3D.dotProduct(deltaPV.getPosition().normalize(), deltaPV.getVelocity());
+        return Vector3D.dotProduct(deltaPV.getPosition(), deltaPV.getVelocity());
     }
 
     /**
