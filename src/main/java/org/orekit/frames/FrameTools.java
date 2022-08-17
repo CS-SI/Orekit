983f2103c3dc76f99880ac8807c4602c6944aebb
Vincent Cucchietti <vincent.cucchietti@csgroup.eu>
2022-08-17 14:23:13 +0200

    Added FrameTools
    feat: Added the FrameTools class that provides transform from/to CCSDS frame facade/Orekit frame.
    
    test: Added two tests to check FrameTools.

---
+++ src/main/java/org/orekit/frames/FrameTools.java
@@ -0,0 +1,168 @@
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.frames;
+
+import org.hipparchus.linear.MatrixUtils;
+import org.hipparchus.linear.RealMatrix;
+import org.orekit.files.ccsds.definitions.FrameFacade;
+import org.orekit.files.ccsds.definitions.OrbitRelativeFrame;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.utils.PVCoordinates;
+import org.orekit.utils.PVCoordinatesProvider;
+
+/**
+ * Utility class providing tools to convert from/to Orekit frames and CCSDS frames.
+ *
+ * @author Pascal Parraud
+ * @author Vincent Cucchietti
+ */
+public class FrameTools {
+
+    /** Transform from %s to %s is not implemented. */
+    private static final String INVALID_TRANSFORM = "Transform from %s to %s is not implemented";
+
+    /** Private constructor for this utility class. */
+    private FrameTools() {
+        // Do nothing
+    }
+
+    /**
+     * Computes the transform from one CCSDS {@link FrameFacade frame} to another.
+     *
+     * @param fIn  the initial frame to convert from
+     * @param fOut the final frame to convert to
+     * @param date the date for the transform
+     * @param pv   the PV coordinates provider (required when one of the frames is a LOF)
+     * @return the transform from initial to final frame
+     */
+    public static Transform getTransform(final FrameFacade fIn, final FrameFacade fOut,
+                                         final AbsoluteDate date, final PVCoordinatesProvider pv) {
+        // Gets transform according to the types of the input frames
+        try {
+            if (fIn.asFrame() != null) {
+                return getTransform(fIn.asFrame(), fOut, date, pv);
+            } else if (fIn.asOrbitRelativeFrame() != null) {
+                return getTransform(fIn.asOrbitRelativeFrame(), fOut, date, pv);
+            }
+            // Transform cannot be get from these 2 frames
+            throw new IllegalArgumentException(String.format(INVALID_TRANSFORM, fIn.getName(), fOut.getName()));
+
+        } catch (IllegalArgumentException exception) {
+            // Transform cannot be get from these 2 frames
+            throw new IllegalArgumentException(String.format(INVALID_TRANSFORM, fIn.getName(), fOut.getName()));
+        }
+    }
+
+    /**
+     * Computes the transform from an Orekit {@link Frame} to a CCSDS {@link FrameFacade frame}.
+     *
+     * @param fIn  the initial frame to convert from
+     * @param fOut the final frame facade to convert to
+     * @param date the date for the transform
+     * @param pv   the PV coordinates provider (required when one of the frames is a LOF)
+     * @return the transform from initial to final frame
+     */
+    public static Transform getTransform(final Frame fIn, final FrameFacade fOut,
+                                         final AbsoluteDate date, final PVCoordinatesProvider pv) {
+
+        // Gets transform according to the types of the input frames
+        if (fOut.asFrame() != null) {
+            return fIn.getTransformTo(fOut.asFrame(), date);
+        } else if (fOut.asOrbitRelativeFrame() != null) {
+            final LOFType lofOut = fOut.asOrbitRelativeFrame().getLofType();
+            if (lofOut != null) {
+                return lofOut.transformFromInertial(date, pv.getPVCoordinates(date, fIn));
+            }
+        }
+        // Transform cannot be gotten from these 2 frames
+        throw new IllegalArgumentException(String.format(INVALID_TRANSFORM, fIn.getName(), fOut.getName()));
+    }
+
+    /**
+     * Computes the transform from an Orekit {@link OrbitRelativeFrame orbit relative frame} to a CCSDS
+     * {@link FrameFacade frame}.
+     *
+     * @param fIn  the initial frame to convert from
+     * @param fOut the final frame facade to convert to
+     * @param date the date for the transform
+     * @param pv   the PV coordinates provider (required when one of the frames is a LOF)
+     * @return the transform from initial to final frame
+     */
+    public static Transform getTransform(final OrbitRelativeFrame fIn, final FrameFacade fOut,
+                                         final AbsoluteDate date, final PVCoordinatesProvider pv) {
+
+        final LOFType lofIn = fIn.getLofType();
+
+        if (lofIn != null) {
+            if (fOut.asFrame() != null) {
+                return getTransform(lofIn, fOut.asFrame(), date, pv);
+
+            } else if (fOut.asOrbitRelativeFrame() != null) {
+                final LOFType lofOut = fOut.asOrbitRelativeFrame().getLofType();
+
+                if (lofOut != null) {
+                    // Uses a pivot pv in some arbitrary inertial frame
+                    final PVCoordinates pivot = pv.getPVCoordinates(date, FramesFactory.getGCRF());
+                    // First transform from input lof to inertial
+                    final Transform first = lofIn.transformFromInertial(date, pivot).getInverse();
+                    // Second transform from inertial to output lof
+                    final Transform second = lofOut.transformFromInertial(date, pivot);
+                    // Returns the composed transform
+                    return new Transform(date, first, second);
+                }
+            }
+        }
+        // Transform cannot be gotten from these 2 frames
+        throw new IllegalArgumentException(
+                String.format(INVALID_TRANSFORM, "Undefined orbit relative frame", fOut.getName()));
+    }
+
+    /**
+     * Computes the transform from an Orekit {@link LOFType local orbit frame} to an Orekit {@link Frame frame}.
+     *
+     * @param lofIn local orbital frame input
+     * @param fOut  the final frame facade to convert to
+     * @param date  the date for the transform
+     * @param pv    the PV coordinates provider (required when one of the frames is a LOF)
+     * @return the transform from initial to final frame
+     */
+    public static Transform getTransform(final LOFType lofIn, final Frame fOut,
+                                         final AbsoluteDate date, final PVCoordinatesProvider pv) {
+
+        return lofIn.transformFromInertial(date, pv.getPVCoordinates(date, fOut)).getInverse();
+    }
+
+    /**
+     * Converts a covariance matrix from one frame to another.
+     *
+     * @param covIn   covariance matrix in some initial frame to convert
+     * @param inToOut transform from the initial to a final frame
+     * @return the covariance matrix in the final frame
+     */
+    public static RealMatrix convertCovFrame(final RealMatrix covIn,
+                                             final Transform inToOut) {
+        // Gets the rotation from the transform
+        final double[][] rotInToOut = inToOut.getRotation().getMatrix();
+        // Builds the matrix to perform covariance transformation
+        final RealMatrix matInToOut = MatrixUtils.createRealMatrix(6, 6);
+        // Fills in the upper left and lower right blocks with the rotation
+        matInToOut.setSubMatrix(rotInToOut, 0, 0);
+        matInToOut.setSubMatrix(rotInToOut, 3, 3);
+        // Returns the covariance matrix converted to frameOut
+        return matInToOut.multiply(covIn.multiplyTransposed(matInToOut));
+    }
+}
---
+++ src/test/java/org/orekit/frames/FrameToolsTest.java
@@ -0,0 +1,207 @@
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.frames;
+
+import org.hipparchus.linear.DiagonalMatrix;
+import org.hipparchus.linear.RealMatrix;
+import org.hipparchus.util.FastMath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.orekit.Utils;
+import org.orekit.data.DataContext;
+import org.orekit.files.ccsds.definitions.CelestialBodyFrame;
+import org.orekit.files.ccsds.definitions.FrameFacade;
+import org.orekit.files.ccsds.definitions.OrbitRelativeFrame;
+import org.orekit.orbits.KeplerianOrbit;
+import org.orekit.orbits.Orbit;
+import org.orekit.orbits.OrbitType;
+import org.orekit.orbits.PositionAngle;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.Constants;
+import org.orekit.utils.IERSConventions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FrameToolsTest {
+
+    /**
+     * Configure access to Orekit data folder for simple unit tests.
+     */
+    @BeforeAll
+    static void configureOrekitDataAccess() {
+        Utils.setDataRoot("FrameTools-data");
+    }
+
+    /**
+     * Provides an orbit for the tests.
+     *
+     * @return a Keplerian orbit defined in EME2000.
+     */
+    private Orbit getOrbit() {
+        Orbit kOrb = new KeplerianOrbit(7.E6, 0.001, FastMath.toRadians(45.), 0., 0., 0.,
+                PositionAngle.TRUE, FramesFactory.getEME2000(),
+                new AbsoluteDate(2015, 1, 1, TimeScalesFactory.getUTC()),
+                Constants.GRIM5C1_EARTH_MU);
+        return OrbitType.CARTESIAN.convertType(kOrb);
+    }
+
+    @Test
+    void testGetTransform() {
+        final Orbit orbit = getOrbit();
+        // From celestial to celestial
+        for (CelestialBodyFrame in : CelestialBodyFrame.values()) {
+            FrameFacade from = new FrameFacade(in.getFrame(IERSConventions.IERS_2010, false, DataContext.getDefault()),
+                    in, null, null, in.name());
+            for (CelestialBodyFrame out : CelestialBodyFrame.values()) {
+                FrameFacade to = new FrameFacade(
+                        out.getFrame(IERSConventions.IERS_2010, false, DataContext.getDefault()),
+                        out, null, null, out.name());
+                try {
+                    Transform t = FrameTools.getTransform(from, to, orbit.getDate(), orbit);
+                    assertNotNull(t);
+                } catch (IllegalArgumentException iae) {
+                    assertTrue(iae.getMessage().contains("is not implemented"));
+                } catch (Exception e) {
+                    fail("Unexpected exception", e.getCause());
+                }
+            }
+        }
+        // From celestial to LOF
+        for (CelestialBodyFrame in : CelestialBodyFrame.values()) {
+            FrameFacade from = new FrameFacade(in.getFrame(IERSConventions.IERS_2010, false, DataContext.getDefault()),
+                    in, null, null, in.name());
+            for (OrbitRelativeFrame out : OrbitRelativeFrame.values()) {
+                FrameFacade to = new FrameFacade(null, null, out, null, out.name());
+                try {
+                    Transform t = FrameTools.getTransform(from, to, orbit.getDate(), orbit);
+                    assertNotNull(t);
+                } catch (IllegalArgumentException iae) {
+                    assertTrue(iae.getMessage().contains("is not implemented"));
+                } catch (Exception e) {
+                    fail("Unexpected exception", e.getCause());
+                }
+            }
+        }
+        // From LOF to celestial
+        for (OrbitRelativeFrame in : OrbitRelativeFrame.values()) {
+            FrameFacade from = new FrameFacade(null, null, in, null, in.name());
+            for (CelestialBodyFrame out : CelestialBodyFrame.values()) {
+                FrameFacade to = new FrameFacade(
+                        out.getFrame(IERSConventions.IERS_2010, false, DataContext.getDefault()),
+                        out, null, null, out.name());
+                try {
+                    Transform t = FrameTools.getTransform(from, to, orbit.getDate(), orbit);
+                    assertNotNull(t);
+                } catch (IllegalArgumentException iae) {
+                    assertTrue(iae.getMessage().contains("is not implemented"));
+                } catch (Exception e) {
+                    fail("Unexpected exception", e.getCause());
+                }
+            }
+        }
+        // From LOF to LOF
+        for (OrbitRelativeFrame in : OrbitRelativeFrame.values()) {
+            FrameFacade from = new FrameFacade(null, null, in, null, in.name());
+            for (OrbitRelativeFrame out : OrbitRelativeFrame.values()) {
+                FrameFacade to = new FrameFacade(null, null, out, null, out.name());
+                try {
+                    Transform t = FrameTools.getTransform(from, to, orbit.getDate(), orbit);
+                    assertNotNull(t);
+                } catch (IllegalArgumentException iae) {
+                    assertTrue(iae.getMessage().contains("is not implemented"));
+                } catch (Exception e) {
+                    fail("Unexpected exception", e.getCause());
+                }
+            }
+        }
+    }
+
+    @Test
+    void testConvertCovFrame() {
+
+        final Orbit pv = getOrbit();
+
+        double sig_sma = 1000;
+        double sig_ecc = 1000 / pv.getA();
+        double sig_inc = FastMath.toRadians(0.01);
+        double sig_pom = FastMath.toRadians(0.01);
+        double sig_gom = FastMath.toRadians(0.01);
+        double sig_anm = FastMath.toRadians(0.1);
+
+        sig_sma *= sig_sma;
+        sig_ecc *= sig_ecc;
+        sig_inc *= sig_inc;
+        sig_pom *= sig_pom;
+        sig_gom *= sig_gom;
+        sig_anm *= sig_anm;
+
+        final RealMatrix covMatrix = new DiagonalMatrix(new double[]{sig_sma, sig_ecc, sig_inc,
+                sig_pom, sig_gom, sig_anm});
+        //     System.out.println("Initial :");
+        //     printMatrix(covMatrix.getData());
+        //     System.out.println();
+        CelestialBodyFrame in = CelestialBodyFrame.EME2000;
+        FrameFacade from = new FrameFacade(in.getFrame(IERSConventions.IERS_2010, false, DataContext.getDefault()),
+                in, null, null, in.name());
+        OrbitRelativeFrame out = OrbitRelativeFrame.TNW;
+        FrameFacade to = new FrameFacade(null, null, out, null, out.name());
+        final Transform t = FrameTools.getTransform(from, to, pv.getDate(), pv);
+        final RealMatrix converted = FrameTools.convertCovFrame(covMatrix, t);
+
+        // Données de référence issues de CelestLab :
+        // https://sourceforge.isae.fr/svn/dcas-soft-espace/support/softs/CelestLab/trunk/help/en_US/scilab_en_US_help/jacobian%20matrices.html cas-3
+        // Attention : Les covariances CCSDS sont censées être exprimées en Cartésien.
+        //             Or, l'exemple Celestlab utilise des données kepleriennes.
+        //             Nous considérons doc ici que la covariance est cartésienne.
+        //             La variable celestlab dpv_dkep doit donc être surchargée pour être la matrice identité.
+        double[][] expected = {{2.543E-08, 0., 5.027E-09, 0., 0., 0.},
+                {0., 1000000., 0., 0., 0., 0.},
+                {5.027E-09, 0., 2.543E-08, 0., 0., 0.},
+                {0., 0., 0., 0.0000015, 0., 0.0000015},
+                {0., 0., 0., 0., 3.046E-08, 0.},
+                {0., 0., 0., 0.0000015, 0., 0.0000015}};
+        //
+        //   System.out.println("Expected :");
+        //   printMatrix(expected);
+        //   System.out.println("Converted :");
+        //   printMatrix(converted.getData());
+
+        // Les deux matrices sont identiques si l'erreur absolue sur chaque terme n'excède pas 1E-7 USI
+        validateMatrix(expected, converted.getData(), 4.e-8);
+    }
+
+    //   private void printMatrix(double[][] data) {
+    //      for (int i = 0; i < data.length; i++) {
+    //         for (int j = 0; j < data[0].length; j++) {
+    //            System.out.print(data[i][j] + " ");
+    //         }
+    //         System.out.println();
+    //      }
+    //   }
+
+    private void validateMatrix(double[][] data, double[][] expected, double threshold) {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                assertEquals(expected[i][j], data[i][j], threshold, String.format("KO for i = %d and j = %d", i, j));
+            }
+        }
+    }
+}
