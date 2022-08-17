10873c0d8321ce84b5cf88981ed0f5621844f70b
Vincent Cucchietti <vincent.cucchietti@csgroup.eu>
2022-08-17 14:28:05 +0200

    Documentation
    docs: Fixed documentation and added license header for ExtremumApproachDetector and its test class.

--- src/main/java/org/orekit/propagation/events/ExtremumApproachDetector.java
+++ src/main/java/org/orekit/propagation/events/ExtremumApproachDetector.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.orekit.propagation.events;
 
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
@@ -17,8 +33,8 @@ import org.orekit.utils.PVCoordinatesProvider;
  * <p>It is also possible to detect solely one type of event using an {@link EventSlopeFilter event slope filter}. For
  * example in order to only detect closest approach, one should type the following :
  * <pre>{@code
- * ExtremumApproachDetector rawDetector = new ExtremumApproachDetector(otherPVProvider);
- * EventDetector closeApproachDetector = new EventSlopeFilter<ExtremumApproachDetector>(rawDetector,FilterType.TRIGGER_ONLY_INCREASING_EVENTS);
+ * ExtremumApproachDetector extremumApproachDetector = new ExtremumApproachDetector(otherPVProvider);
+ * EventDetector closeApproachDetector = new EventSlopeFilter<ExtremumApproachDetector>(extremumApproachDetector,FilterType.TRIGGER_ONLY_INCREASING_EVENTS);
  *  } </pre>
  * </p>
  *
@@ -55,8 +71,8 @@ public class ExtremumApproachDetector extends AbstractDetector<ExtremumApproachD
      *                        approach.
      */
     public ExtremumApproachDetector(
-        final double maxCheck, final double threshold, final int maxIter,
-        final EventHandler<? super ExtremumApproachDetector> handler, final PVCoordinatesProvider otherPVProvider) {
+            final double maxCheck, final double threshold, final int maxIter,
+            final EventHandler<? super ExtremumApproachDetector> handler, final PVCoordinatesProvider otherPVProvider) {
         super(maxCheck, threshold, maxIter, handler);
         this.otherPVProvider = otherPVProvider;
     }
@@ -73,7 +89,7 @@ public class ExtremumApproachDetector extends AbstractDetector<ExtremumApproachD
      */
     protected PVCoordinates deltaPV(final SpacecraftState s) {
         return new PVCoordinates(s.getPVCoordinates(),
-            otherPVProvider.getPVCoordinates(s.getDate(), s.getFrame()));
+                otherPVProvider.getPVCoordinates(s.getDate(), s.getFrame()));
     }
 
     /** {@inheritDoc} */
--- src/test/java/org/orekit/propagation/events/ExtremumApproachDetectorTest.java
+++ src/test/java/org/orekit/propagation/events/ExtremumApproachDetectorTest.java
@@ -1,3 +1,19 @@
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.orekit.propagation.events;
 
 import org.assertj.core.api.Assertions;
@@ -42,7 +58,7 @@ public class ExtremumApproachDetectorTest {
         final double raan = 0; //rad
         final double anomaly = FastMath.toRadians(0); //rad
         final Orbit orbit =
-            new KeplerianOrbit(a, e, i, pa, raan, anomaly, PositionAngle.TRUE, frame, initialDate, mu);
+                new KeplerianOrbit(a, e, i, pa, raan, anomaly, PositionAngle.TRUE, frame, initialDate, mu);
 
         final PVCoordinatesProvider earthPVProvider = CelestialBodyFactory.getEarth();
 
@@ -53,11 +69,11 @@ public class ExtremumApproachDetectorTest {
 
         // When
         final SpacecraftState stateAtEvent =
-            propagator.propagate(initialDate.shiftedBy(orbit.getKeplerianPeriod() * 2));
+                propagator.propagate(initialDate.shiftedBy(orbit.getKeplerianPeriod() * 2));
 
         // Then
         Assertions.assertThat(stateAtEvent.getDate().durationFrom(initialDate))
-            .isEqualTo(orbit.getKeplerianPeriod(), withPrecision(2.0e-12));
+                .isEqualTo(orbit.getKeplerianPeriod(), withPrecision(2.0e-12));
 
     }
 
@@ -81,23 +97,23 @@ public class ExtremumApproachDetectorTest {
         final double raan = 0; //rad
         final double anomaly = FastMath.toRadians(0); //rad
         final Orbit orbit =
-            new KeplerianOrbit(a, e, i, pa, raan, anomaly, PositionAngle.TRUE, frame, initialDate, mu);
+                new KeplerianOrbit(a, e, i, pa, raan, anomaly, PositionAngle.TRUE, frame, initialDate, mu);
 
         final PVCoordinatesProvider earthPVProvider = CelestialBodyFactory.getEarth();
 
         final ExtremumApproachDetector detector =
-            new ExtremumApproachDetector(earthPVProvider).withHandler(new StopOnEvent<>());
+                new ExtremumApproachDetector(earthPVProvider).withHandler(new StopOnEvent<>());
 
         final Propagator propagator = new KeplerianPropagator(orbit);
         propagator.addEventDetector(detector);
 
         // When
         final SpacecraftState stateAtEvent =
-            propagator.propagate(initialDate.shiftedBy(orbit.getKeplerianPeriod() * 2));
+                propagator.propagate(initialDate.shiftedBy(orbit.getKeplerianPeriod() * 2));
 
         // Then
         Assertions.assertThat(stateAtEvent.getDate().durationFrom(initialDate))
-            .isEqualTo(orbit.getKeplerianPeriod() / 2, withPrecision(1.27798E-8));
+                .isEqualTo(orbit.getKeplerianPeriod() / 2, withPrecision(1.27798E-8));
 
     }
 }
