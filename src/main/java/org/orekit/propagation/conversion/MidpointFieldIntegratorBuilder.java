eb648ddfaa8f4d15608ba0e4af017c8022ab85b9
Vincent CUCCHIETTI <vincent.cucchietti@csgroup.eu>
2023-03-03 13:17:36 +0000

    Fixed issue #1006 : Added FieldODEIntegratorBuilder interface and its implementing classes

---
+++ src/main/java/org/orekit/propagation/conversion/MidpointFieldIntegratorBuilder.java
@@ -0,0 +1,51 @@
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.orekit.propagation.conversion;
+
+import org.hipparchus.CalculusFieldElement;
+import org.hipparchus.Field;
+import org.hipparchus.ode.AbstractFieldIntegrator;
+import org.hipparchus.ode.nonstiff.MidpointFieldIntegrator;
+import org.orekit.orbits.FieldOrbit;
+import org.orekit.orbits.OrbitType;
+
+/** Builder for MidpointFieldIntegrator.
+ * @author Pascal Parraud
+ * @author Vincent Cucchietti
+ * @since 12.0
+ */
+public class MidpointFieldIntegratorBuilder<T extends CalculusFieldElement<T>> implements FieldODEIntegratorBuilder<T> {
+
+    /** Step size (s). */
+    private final T step;
+
+    /** Build a new instance.
+     * @param step step size (s)
+     * @see MidpointFieldIntegrator
+     */
+    public MidpointFieldIntegratorBuilder(final T step) {
+        this.step = step;
+    }
+
+    /** {@inheritDoc} */
+    public AbstractFieldIntegrator<T> buildIntegrator(final Field<T> field,
+                                                      final FieldOrbit<T> orbit,
+                                                      final OrbitType orbitType) {
+        return new MidpointFieldIntegrator<>(field, step);
+    }
+
+}
