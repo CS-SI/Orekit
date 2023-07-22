d1e8c06416ffdfadad5237b08427330e7d39b7a0
Bryan Cazabonne <bryan.cazabonne@gmail.com>
2023-07-22 21:16:24 +0200

    Updated missing copyrights year.

--- src/test/java/org/orekit/bodies/FieldEllipseTest.java
+++ src/test/java/org/orekit/bodies/FieldEllipseTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2002-2022 Luc Maisonobe
+/* Copyright 2002-2023 Luc Maisonobe
  * Licensed to CS GROUP (CS) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
