c599fd7be2c48ef9e2240bd2372f9259462a06cc
Luc Maisonobe <luc@orekit.org>
2023-08-07 15:59:08 +0200

    Fixed failing test.

--- src/test/java/org/orekit/errors/OrekitMessagesTest.java
+++ src/test/java/org/orekit/errors/OrekitMessagesTest.java
@@ -30,7 +30,7 @@ public class OrekitMessagesTest {
 
     @Test
     public void testMessageNumber() {
-        Assertions.assertEquals(283, OrekitMessages.values().length);
+        Assertions.assertEquals(286, OrekitMessages.values().length);
     }
 
     @Test
