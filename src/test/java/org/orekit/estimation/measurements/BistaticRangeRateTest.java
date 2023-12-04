ac69eecd66d950e0afda613d5f4ece757991349f
Luc Maisonobe <luc@orekit.org>
2023-12-04 08:51:27 +0100

    Renamed SaastamoinenModel → ModifiedSaastamoinenModel

--- src/test/java/org/orekit/estimation/measurements/BistaticRangeRateTest.java
+++ src/test/java/org/orekit/estimation/measurements/BistaticRangeRateTest.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.Test;
 import org.orekit.estimation.Context;
 import org.orekit.estimation.EstimationTestUtils;
 import org.orekit.estimation.measurements.modifiers.BistaticRangeRateTroposphericDelayModifier;
-import org.orekit.models.earth.troposphere.SaastamoinenModel;
+import org.orekit.models.earth.troposphere.ModifiedSaastamoinenModel;
 import org.orekit.orbits.OrbitType;
 import org.orekit.orbits.PositionAngleType;
 import org.orekit.propagation.Propagator;
@@ -168,7 +168,7 @@ public class BistaticRangeRateTest {
                                                                1.0, 3.0, 300.0);
         propagator.clearStepHandlers();
 
-        final BistaticRangeRateTroposphericDelayModifier modifier = new BistaticRangeRateTroposphericDelayModifier(SaastamoinenModel.getStandardModel());
+        final BistaticRangeRateTroposphericDelayModifier modifier = new BistaticRangeRateTroposphericDelayModifier(ModifiedSaastamoinenModel.getStandardModel());
 
         double maxRelativeError = 0;
         for (final ObservedMeasurement<?> measurement : measurements) {
@@ -324,7 +324,7 @@ public class BistaticRangeRateTest {
                                                                1.0, 3.0, 300.0);
         propagator.clearStepHandlers();
 
-        final BistaticRangeRateTroposphericDelayModifier modifier = new BistaticRangeRateTroposphericDelayModifier(SaastamoinenModel.getStandardModel());
+        final BistaticRangeRateTroposphericDelayModifier modifier = new BistaticRangeRateTroposphericDelayModifier(ModifiedSaastamoinenModel.getStandardModel());
 
         double maxRelativeError = 0;
         for (final ObservedMeasurement<?> measurement : measurements) {
