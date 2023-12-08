be4e5d09a9cef308ab0469433400cfeb96f508c1
Luc Maisonobe <luc@orekit.org>
2023-12-08 18:08:14 +0100

    Fixed missing pressure conversion.

--- src/main/java/org/orekit/models/earth/troposphere/EstimatedTroposphericModel.java
+++ src/main/java/org/orekit/models/earth/troposphere/EstimatedTroposphericModel.java
@@ -77,7 +77,10 @@ public class EstimatedTroposphericModel implements DiscreteTroposphericModel {
      */
     public EstimatedTroposphericModel(final double t0, final double p0,
                                       final MappingFunction model, final double totalDelay) {
-        this(new ModifiedSaastamoinenModel(new ConstantPressureTemperatureHumidityProvider(new PressureTemperatureHumidity(p0, t0, 0.0, 0.0))),
+        this(new ModifiedSaastamoinenModel(new ConstantPressureTemperatureHumidityProvider(new PressureTemperatureHumidity(TropoUnit.HECTO_PASCAL.toSI(p0),
+                                                                                                                           t0,
+                                                                                                                           0.0,
+                                                                                                                           0.0))),
              model, totalDelay);
     }
 
