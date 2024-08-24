c1c6285e5fae3f32b24bf01b12b883c70e69cbb2
Luc Maisonobe <luc@orekit.org>
2024-08-24 16:18:55 +0200

    Removed non thread-safe use of DecimalFormat.
    Fixes #643

--- src/main/java/org/orekit/time/DateComponents.java
+++ src/main/java/org/orekit/time/DateComponents.java
@@ -17,7 +17,6 @@
 package org.orekit.time;
 
 import java.io.Serializable;
-import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -123,12 +122,6 @@ public class DateComponents implements Serializable, Comparable<DateComponents> 
 
     /** Formatting symbols used in {@link #toString()}. */
     private static final DecimalFormatSymbols US_SYMBOLS = new DecimalFormatSymbols(Locale.US);
-
-    /** Format for years. */
-    private static final DecimalFormat FOUR_DIGITS = new DecimalFormat("0000", US_SYMBOLS);
-
-    /** Format for months and days. */
-    private static final DecimalFormat TWO_DIGITS  = new DecimalFormat("00", US_SYMBOLS);
 
     /** Offset between J2000 epoch and modified julian day epoch. */
     private static final int MJD_TO_J2000 = 51544;
@@ -473,11 +466,7 @@ public class DateComponents implements Serializable, Comparable<DateComponents> 
      * @return string representation of the date.
      */
     public String toString() {
-        return new StringBuilder().
-               append(FOUR_DIGITS.format(year)).append('-').
-               append(TWO_DIGITS.format(month)).append('-').
-               append(TWO_DIGITS.format(day)).
-               toString();
+        return String.format(Locale.US, "%04d-%02d-%02d", year, month, day);
     }
 
     /** {@inheritDoc} */
