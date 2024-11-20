c0c904a75e72ffc0b049d5131decddc2b22470f6
Luc Maisonobe <luc@orekit.org>
2024-11-20 16:26:03 +0100

    Removed unused equations.

--- src/main/java/org/orekit/models/earth/troposphere/iturp834/ITURP834PathDelay.java
+++ src/main/java/org/orekit/models/earth/troposphere/iturp834/ITURP834PathDelay.java
@@ -17,10 +17,8 @@
 package org.orekit.models.earth.troposphere.iturp834;
 
 import org.hipparchus.CalculusFieldElement;
-import org.hipparchus.util.FastMath;
 import org.orekit.bodies.FieldGeodeticPoint;
 import org.orekit.bodies.GeodeticPoint;
-import org.orekit.bodies.OneAxisEllipsoid;
 import org.orekit.models.earth.ITURP834AtmosphericRefraction;
 import org.orekit.models.earth.troposphere.FieldTroposphericDelay;
 import org.orekit.models.earth.troposphere.TroposphereMappingFunction;
@@ -28,13 +26,13 @@ import org.orekit.models.earth.troposphere.TroposphericDelay;
 import org.orekit.models.earth.troposphere.TroposphericModel;
 import org.orekit.models.earth.weather.FieldPressureTemperatureHumidity;
 import org.orekit.models.earth.weather.PressureTemperatureHumidity;
-import org.orekit.models.earth.weather.water.WaterVaporPressureProvider;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.FieldAbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.utils.FieldTrackingCoordinates;
 import org.orekit.utils.ParameterDriver;
 import org.orekit.utils.TrackingCoordinates;
+import org.orekit.utils.units.Unit;
 
 import java.util.Collections;
 import java.util.List;
@@ -46,79 +44,34 @@ import java.util.List;
  * i.e. section 1 of the recommendation, is implemented in the
  * {@link ITURP834AtmosphericRefraction} class.
  * </p>
+ * @see ITURP834WeatherParameters
+ * @see ITURP834MappingFunction
  * @author Luc Maisonobe
  * @see <a href="https://www.itu.int/rec/R-REC-P.834/en">P.834 : Effects of tropospheric refraction on radiowave propagation</>
  * @since 13.0
  */
 public class ITURP834PathDelay implements TroposphericModel {
 
-    /** Average value of atmospheric refractivity extrapolated to sea level (from ITU-R P.453). */
-    private static final double NS = 315;
-
     /** Molar gas constant (J/mol K). */
     private static final double R = 8.314;
 
     /** Dry air molar mass (kg/mol). */
-    private static final double MD = 0.0289644;
+    private static final double MD = Unit.GRAM.toSI(28.9644);
 
-    /** Hydrostatic factor (K/hPa). */
-    private static final double K1 = 76.604;
+    /** Hydrostatic factor (K/Pa). */
+    private static final double K1 = Unit.parse("hPa⁻¹").toSI(76.604);
 
-    /** Wet factor (K²/hPa). */
-    private static final double K2 = 373900;
-
-    /** Gravity factor. */
-    private static final double G = 9.784;
-
-    /** Gravity latitude correction factor. */
-    private static final double GL = 0.00266;
-
-    /** Gravity altitude correction factor (rescaled for altitudes in meters). */
-    private static final double GH = 2.8e-7;
-
-    /** Computation engine for vertical excess path. */
-    private final VerticalExcessPath verticalExcessPathComputer;
+    /** Wet factor (K²/Pa). */
+    private static final double K2 = Unit.parse("hPa⁻¹").toSI(373900);
 
     /** Mapping function. */
     private final TroposphereMappingFunction mappingFunction;
 
-    /** Converter between pressure, temperature and humidity and relative humidity. */
-    private final WaterVaporPressureProvider waterVaporPressureProvider;
-
-    /** Earth. */
-    private final OneAxisEllipsoid earth;
-
     /** Simple constructor.
-     * <p>
-     * The model uses a fixed computation engine for vertical excess path, which should
-     * be chosen among the three possibilities in table 2 of ITU-R P.834
-     * </p>
-     * <ul>
-     *     <li>{@link VerticalExcessPath#COASTAL COASTAL}: coastal areas (islands and locations less than 10km from sea shore)</li>
-     *     <li>{@link VerticalExcessPath#NON_COASTAL_EQUATORIAL NON_COASTAL_EQUATORIAL}: non-coastal equatorial areas</li>
-     *     <li>{@link VerticalExcessPath#NON_COASTAL_NON_EQUATORIAL NON_COASTAL_NON_EQUATORIAL}: all other areas</li>
-     * </ul>
-     * <p>
-     * It is the responsibility of the caller to use the proper vertical excess path computation engine,
-     * no attempt is made in this class to associate the {@link GeodeticPoint geodetic point} passed as
-     * an argument to the {@link
-     * #pathDelay(TrackingCoordinates, GeodeticPoint, PressureTemperatureHumidity, double[], AbsoluteDate)
-     * pathDelay} method to a type of area. This means that if the model is intended to be used on sites
-     * belonging to different types of areas, then separate instances of the model should be built with
-     * different vertical excess path computation engines and associated to their respective sites.
-     * </p>
-     * @param verticalExcessPathComputer computation engine for vertical excess path
-     * @param waterVaporPressureProvider converter between pressure, temperature and humidity and relative humidity
-     * @param earth Earth model
      * @param utc UTC time scale
      */
-    public ITURP834PathDelay(final VerticalExcessPath verticalExcessPathComputer,
-                             final WaterVaporPressureProvider waterVaporPressureProvider,
-                             final OneAxisEllipsoid earth, final TimeScale utc) {
-        this.verticalExcessPathComputer = verticalExcessPathComputer;
-        this.mappingFunction            = new ITURP834MappingFunction(utc);
-        this.waterVaporPressureProvider = waterVaporPressureProvider;
-        this.earth                      = earth;
+    public ITURP834PathDelay(final TimeScale utc) {
+        this.mappingFunction = new ITURP834MappingFunction(utc);
     }
 
     /** {@inheritDoc} */
@@ -127,20 +80,8 @@ public class ITURP834PathDelay implements TroposphericModel {
                                        final PressureTemperatureHumidity weather,
                                        final double[] parameters, final AbsoluteDate date) {
 
-        // compute vertical excess path
-        final double deltaLv = verticalExcessPathComputer.verticalExcessPath(weather, waterVaporPressureProvider);
-
-        // corrective factor for exponential atmosphere, equations 21, 22, and 23
-        final double h0    = 1.0e6 * deltaLv / NS;
-        final double rs    = earth.transform(new GeodeticPoint(point.getLatitude(), point.getLongitude(), 0)).getNorm();
-        final double rh0   = earth.transform(new GeodeticPoint(point.getLatitude(), point.getLongitude(), h0)).getNorm();
-        final double ns    = Double.NaN; // TODO
-        final double nh0   = Double.NaN; // TODO
-        final double ratio = ns * rs / (nh0 * rh0);
-        final double k     = 1 - ratio * ratio;
-
         // calculate path delay
-        final double gm       = G * (1 - GL * FastMath.cos(2 * point.getLatitude()) - GH * point.getAltitude());
+        final double gm       = Gravity.getGravityAtAltitude(point).evaluate();
         final double deltaLvh = 1.0e-6 * R * K1 * weather.getPressure() / (MD * gm);
         final double deltaLvw = 1.0e-6 * R * K2 * weather.getWaterVaporPressure() /
                                 (MD * gm * (1 + weather.getLambda()) * weather.getTm());
@@ -158,24 +99,9 @@ public class ITURP834PathDelay implements TroposphericModel {
                                                                                    final FieldGeodeticPoint<T> point,
                                                                                    final FieldPressureTemperatureHumidity<T> weather,
                                                                                    final T[] parameters, final FieldAbsoluteDate<T> date) {
-        // compute vertical excess path
-        final T deltaLv = verticalExcessPathComputer.verticalExcessPath(weather, waterVaporPressureProvider);
-        final T zero    = date.getField().getZero();
-
-        // corrective factor for exponential atmosphere, equations 21, 22, and 23
-        final T h0    = deltaLv.multiply(1.0e6 / NS);
-        final T rs    = earth.transform(new FieldGeodeticPoint<>(point.getLatitude(), point.getLongitude(), zero)).getNorm();
-        final T rh0   = earth.transform(new FieldGeodeticPoint<>(point.getLatitude(), point.getLongitude(), h0)).getNorm();
-        final T ns    = zero.newInstance(Double.NaN); // TODO
-        final T nh0   = zero.newInstance(Double.NaN); // TODO
-        final T ratio = ns.multiply(rs).divide(nh0.multiply(rh0));
-        final T k     = ratio.square().subtract(1).negate();
 
         // calculate path delay
-        final T gm       = FastMath.cos(point.getLatitude().multiply(2)).multiply(-GL).
-                           add(1).
-                           subtract(point.getAltitude().multiply(GH)).
-                           multiply(G);
+        final T gm       = Gravity.getGravityAtAltitude(point).evaluate();
         final T deltaLvh = weather.getPressure().multiply(1.0e-6 * R * K1).
                            divide(gm.multiply(MD));
         final T deltaLvw = weather.getWaterVaporPressure().multiply(1.0e-6 * R * K2).
