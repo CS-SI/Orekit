0836b9dc41931124a9fa0dbef884168c5309cb54
Luc Maisonobe <luc@orekit.org>
2024-11-20 16:27:21 +0100

    Updated constructor.

--- src/test/java/org/orekit/models/earth/troposphere/iturp834/ITURP834PathDelayTest.java
+++ src/test/java/org/orekit/models/earth/troposphere/iturp834/ITURP834PathDelayTest.java
@@ -20,8 +20,6 @@ import org.hipparchus.util.Binary64Field;
 import org.hipparchus.util.FastMath;
 import org.junit.jupiter.api.Test;
 import org.orekit.bodies.GeodeticPoint;
-import org.orekit.bodies.OneAxisEllipsoid;
-import org.orekit.frames.FramesFactory;
 import org.orekit.models.earth.troposphere.AbstractPathDelayTest;
 import org.orekit.models.earth.troposphere.AskneNordiusModel;
 import org.orekit.models.earth.troposphere.CanonicalSaastamoinenModel;
@@ -29,26 +27,23 @@ import org.orekit.models.earth.troposphere.ChaoMappingFunction;
 import org.orekit.models.earth.troposphere.ConstantAzimuthalGradientProvider;
 import org.orekit.models.earth.troposphere.ConstantTroposphericModel;
 import org.orekit.models.earth.troposphere.ConstantViennaAProvider;
-import org.orekit.models.earth.troposphere.GlobalMappingFunctionModel;
+import org.orekit.models.earth.troposphere.FixedTroposphericDelay;
+import org.orekit.models.earth.troposphere.MariniMurray;
 import org.orekit.models.earth.troposphere.MendesPavlisModel;
+import org.orekit.models.earth.troposphere.ModifiedHopfieldModel;
 import org.orekit.models.earth.troposphere.ModifiedSaastamoinenModel;
-import org.orekit.models.earth.troposphere.NiellMappingFunctionModel;
-import org.orekit.models.earth.troposphere.RevisedChaoMappingFunction;
-import org.orekit.models.earth.troposphere.TroposphereMappingFunction;
 import org.orekit.models.earth.troposphere.TroposphericDelay;
 import org.orekit.models.earth.troposphere.TroposphericModel;
 import org.orekit.models.earth.troposphere.TroposphericModelUtils;
 import org.orekit.models.earth.troposphere.ViennaACoefficients;
 import org.orekit.models.earth.troposphere.ViennaOne;
 import org.orekit.models.earth.troposphere.ViennaThree;
-import org.orekit.models.earth.weather.ConstantPressureTemperatureHumidityProvider;
+import org.orekit.models.earth.weather.HeightDependentPressureTemperatureHumidityConverter;
 import org.orekit.models.earth.weather.PressureTemperatureHumidity;
 import org.orekit.models.earth.weather.water.CIPM2007;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScale;
 import org.orekit.time.TimeScalesFactory;
-import org.orekit.utils.Constants;
-import org.orekit.utils.IERSConventions;
 import org.orekit.utils.TrackingCoordinates;
 
 import java.io.IOException;
@@ -59,11 +54,7 @@ import java.util.Locale;
 public class ITURP834PathDelayTest extends AbstractPathDelayTest<ITURP834PathDelay> {
 
     protected ITURP834PathDelay buildTroposphericModel() {
-        return new ITURP834PathDelay(VerticalExcessPath.NON_COASTAL_NON_EQUATORIAL, new CIPM2007(),
-                                     new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
-                                                          Constants.WGS84_EARTH_FLATTENING,
-                                                          FramesFactory.getITRF(IERSConventions.IERS_2010, true)),
-                                     TimeScalesFactory.getUTC());
+        return new ITURP834PathDelay(TimeScalesFactory.getUTC());
     }
 
     @Test
@@ -83,14 +74,17 @@ public class ITURP834PathDelayTest extends AbstractPathDelayTest<ITURP834PathDel
 
     @Test
     public void testTmp() throws IOException {
-        final AbsoluteDate date = new AbsoluteDate(1994, 1, 1, TimeScalesFactory.getUTC());
+        final AbsoluteDate date = defaultDate;
 
-        final double latitude    = FastMath.toRadians(48.0);
-        final double longitude   = FastMath.toRadians(0.20);
-        final double height      = 68.0;
-        final GeodeticPoint point = new GeodeticPoint(latitude, longitude, height);
-
-        final TimeScale utc = TimeScalesFactory.getUTC();
+        final TimeScale     utc       = TimeScalesFactory.getUTC();
+        final GeodeticPoint point     = new GeodeticPoint(FastMath.toRadians(47.71675),
+                                                          FastMath.toRadians(6.12264),
+                                                          300);
+        final PressureTemperatureHumidity pth =
+            new HeightDependentPressureTemperatureHumidityConverter(new CIPM2007()).
+                convert(TroposphericModelUtils.STANDARD_ATMOSPHERE, defaultPoint.getAltitude());
+        final PressureTemperatureHumidity pthITU =
+            new ITURP834WeatherParameters(utc).getWeatherParameters(point, date);
 
             final ProcessBuilder pb = new ProcessBuilder("gnuplot").
                             redirectOutput(ProcessBuilder.Redirect.INHERIT).
@@ -99,43 +93,54 @@ public class ITURP834PathDelayTest extends AbstractPathDelayTest<ITURP834PathDel
             final Process gnuplot = pb.start();
             try (PrintStream out = new PrintStream(gnuplot.getOutputStream(), false, StandardCharsets.UTF_8.name())) {
                 out.format(Locale.US, "set terminal qt size %d, %d title 'path delay'%n", 1000, 1000);
-                out.format(Locale.US, "set xlabel 'elevation'%n");
-                out.format(Locale.US, "set ylabel 'path delay'%n");
-                out.format(Locale.US, "set title '%s'%n", "Path delays");
-                print(buildTroposphericModel(), out, "$itu", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
-                print(new CanonicalSaastamoinenModel(), out, "$canonical_saastamoinen", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
+                out.format(Locale.US, "set xlabel 'elevation (°)'%n");
+                out.format(Locale.US, "set ylabel 'path delay (m)'%n");
+                out.format(Locale.US, "set title '%s'%n", "ITU-R P.834 validation");
+                print(buildTroposphericModel(), out, "$itu", 0, point, pthITU, date);
+                print(new CanonicalSaastamoinenModel(), out, "$canonical_saastamoinen", 0, point, pth, date);
                 print(new ModifiedSaastamoinenModel(TroposphericModelUtils.STANDARD_ATMOSPHERE_PROVIDER), out, "$modified_saastamoinen", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
+                      pth, date);
                 print(new ViennaOne(new ConstantViennaAProvider(new ViennaACoefficients(0.00127683, 0.00060955)),
                                               new ConstantAzimuthalGradientProvider(null),
                                               new ConstantTroposphericModel(new TroposphericDelay(2.0966, 0.2140, 0, 0)),
                                               TimeScalesFactory.getUTC()), out, "$vienna_1", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
+                      pth, date);
                 print(new ViennaThree(new ConstantViennaAProvider(new ViennaACoefficients(0.00127683, 0.00060955)),
                                               new ConstantAzimuthalGradientProvider(null),
                                               new ConstantTroposphericModel(new TroposphericDelay(2.0966, 0.2140, 0, 0)),
                                               TimeScalesFactory.getUTC()), out, "$vienna_3", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
+                      pth, date);
                 print(new AskneNordiusModel(new ChaoMappingFunction()), out, "$askne_nordius", 0, point,
-                      TroposphericModelUtils.STANDARD_ATMOSPHERE, date);
-                out.format(Locale.US, "plot $itu with lines title 'ITU', \\%n");
-                out.format(Locale.US, "     $canonical_saastamoinen with lines title 'canonical Saastamoinen', \\%n");
-                out.format(Locale.US, "     $modified_saastamoinen with lines title 'modified Saastamoinen', \\%n");
-                out.format(Locale.US, "     $vienna_1 with lines title 'Vienna 1', \\%n");
-                out.format(Locale.US, "     $vienna_3 with lines title 'Vienna 3', \\%n");
-                out.format(Locale.US, "     $$askne_nordius with lines title 'Askne-Nordius'%n");
+                      pth, date);
+                print(FixedTroposphericDelay.getDefaultModel(), out, "$tabulated", 0, point,
+                      pth, date);
+                print(new ModifiedHopfieldModel(), out, "$modified_hopfield", 0, point,
+                      pth, date);
+                print(new MariniMurray(694.3, TroposphericModelUtils.NANO_M), out, "$marini_murray", 0, point,
+                      pth, date);
+                print(new MendesPavlisModel(TroposphericModelUtils.STANDARD_ATMOSPHERE_PROVIDER,
+                                            0.532, TroposphericModelUtils.MICRO_M), out, "$mendes_pavlis", 0, point,
+                      pth, date);
+                out.format(Locale.US, "plot $itu with linespoints pt 9 dt 3 title 'ITU-R P.834', \\%n");
+                out.format(Locale.US, "     $canonical_saastamoinen with lines dt 3 title 'canonical Saastamoinen', \\%n");
+                out.format(Locale.US, "     $modified_saastamoinen with lines dt 3 title 'modified Saastamoinen', \\%n");
+                out.format(Locale.US, "     $vienna_1 with lines dt 3 title 'Vienna 1', \\%n");
+                out.format(Locale.US, "     $vienna_3 with lines dt 3 title 'Vienna 3', \\%n");
+                out.format(Locale.US, "     $askne_nordius with lines dt 3 title 'Askne-Nordius', \\%n");
+                out.format(Locale.US, "     $tabulated with lines dt 3 title 'tabulated', \\%n");
+                out.format(Locale.US, "     $modified_hopfield with lines dt 3 title 'modified Hopfield', \\%n");
+                out.format(Locale.US, "     $marini_murray with lines dt 2 title 'Marini-Murray (optical at 694.3nm)', \\%n");
+                out.format(Locale.US, "     $mendes_pavlis with lines dt 2 title 'Mendes-Pavlis (optical at 532nm)'%n");
                 out.format(Locale.US, "pause mouse close%n");
             }
     }
     private void print(final TroposphericModel tm, final PrintStream out, final String name, final int index,
                        final GeodeticPoint point, final PressureTemperatureHumidity weather, final AbsoluteDate date) {
         out.format(Locale.US, "%s <<EOD%n", name);
-        for (double e = 0; e < FastMath.toRadians(20); e += 0.001) {
+        for (double e = 0; e <= 20; e += 0.25) {
             out.format(Locale.US, "%.6f %.6f%n",
-                       FastMath.toDegrees(e),
-                       tm.pathDelay(new TrackingCoordinates(0, e, 1.4e6), point, weather,
+                       e,
+                       tm.pathDelay(new TrackingCoordinates(0, FastMath.toRadians(e), 1.4e6), point, weather,
                                     tm.getParameters(date), date).getDelay());
         }
         out.format(Locale.US, "EOD%n");
