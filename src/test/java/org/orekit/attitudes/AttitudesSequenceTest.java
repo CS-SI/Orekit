370e7f561084b23429de4365178f8f3f17bc67e2
Serrof <serra.romain@gmail.com>
2024-12-02 23:13:02 +0100

    Made AttitudesSequence work without orbit

--- src/test/java/org/orekit/attitudes/AttitudesSequenceTest.java
+++ src/test/java/org/orekit/attitudes/AttitudesSequenceTest.java
@@ -64,6 +64,7 @@ import org.orekit.propagation.numerical.NumericalPropagator;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.FieldAbsoluteDate;
 import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.AbsolutePVCoordinates;
 import org.orekit.utils.AngularDerivativesFilter;
 import org.orekit.utils.Constants;
 import org.orekit.utils.ExtendedPositionProvider;
@@ -510,9 +511,7 @@ public class AttitudesSequenceTest {
         final AbsoluteDate initialDate = new AbsoluteDate(2004, 1, 1, 23, 30, 00.000, TimeScalesFactory.getUTC());
         final Vector3D position  = new Vector3D(-6142438.668, 3492467.560, -25767.25680);
         final Vector3D velocity  = new Vector3D(505.8479685, 942.7809215, 7435.922231);
-        final Orbit initialOrbit = new KeplerianOrbit(new PVCoordinates(position, velocity),
-                                                      FramesFactory.getEME2000(), initialDate,
-                                                      Constants.EIGEN5C_EARTH_MU);
+        final AbsolutePVCoordinates initialPV = new AbsolutePVCoordinates(FramesFactory.getEME2000(), initialDate, new PVCoordinates(position, velocity));
 
         final OneAxisEllipsoid earth = new OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
                                                             Constants.WGS84_EARTH_FLATTENING,
@@ -524,8 +523,8 @@ public class AttitudesSequenceTest {
                                                              "Волгоград");
         final AttitudesSequence attitudesSequence = new AttitudesSequence();
         final double            transitionTime    = 250.0;
-        final AttitudeProvider  nadirPointing     = new NadirPointing(initialOrbit.getFrame(), earth);
-        final AttitudeProvider  targetPointing    = new TargetPointing(initialOrbit.getFrame(), volgograd.getPoint(), earth);
+        final AttitudeProvider  nadirPointing     = new NadirPointing(initialPV.getFrame(), earth);
+        final AttitudeProvider  targetPointing    = new TargetPointing(initialPV.getFrame(), volgograd.getPoint(), earth);
         final ElevationDetector eventDetector     = new ElevationDetector(volgograd).
                                                     withConstantElevation(FastMath.toRadians(5.0)).
                                                     withHandler(new ContinueOnEvent());
@@ -533,28 +532,29 @@ public class AttitudesSequenceTest {
         attitudesSequence.addSwitchingCondition(nadirPointing, targetPointing, eventDetector,
                                                 true, false, transitionTime, AngularDerivativesFilter.USE_RR,
                                                 (previous, next, state) -> nadirToTarget.add(state.getDate()));
-        final double[][] tolerance = ToleranceProvider.getDefaultToleranceProvider(10.).getTolerances(initialOrbit, initialOrbit.getType());
+        final double[][] tolerance = ToleranceProvider.getDefaultToleranceProvider(10.).getTolerances(initialPV);
         final AdaptiveStepsizeIntegrator integrator = new DormandPrince853Integrator(0.001, 300.0, tolerance[0], tolerance[1]);
         final NumericalPropagator propagator = new NumericalPropagator(integrator);
         GravityFieldFactory.addPotentialCoefficientsReader(new ICGEMFormatReader("g007_eigen_05c_coef", false));
         propagator.addForceModel(new HolmesFeatherstoneAttractionModel(earth.getBodyFrame(),
                                                                        GravityFieldFactory.getNormalizedProvider(8, 8)));
-        propagator.setInitialState(new SpacecraftState(initialOrbit,
-                                                       nadirPointing.getAttitude(initialOrbit,
-                                                                                 initialOrbit.getDate(),
-                                                                                 initialOrbit.getFrame())));
+        propagator.setInitialState(new SpacecraftState(initialPV,
+                                                       nadirPointing.getAttitude(initialPV,
+                                                                                 initialPV.getDate(),
+                                                                                 initialPV.getFrame())));
         propagator.setAttitudeProvider(attitudesSequence);
+        propagator.setOrbitType(null);
         propagator.propagate(initialDate.shiftedBy(6000));
 
         // check that if we restart a backward propagation from an intermediate state
         // we properly get an interpolated attitude despite we missed the event trigger
         final AbsoluteDate midTransition = nadirToTarget.get(0).shiftedBy(0.5 * transitionTime);
         SpacecraftState state   = propagator.propagate(midTransition.shiftedBy(+60), midTransition);
-        Rotation nadirR  = nadirPointing.getAttitude(state.getOrbit(), state.getDate(), state.getFrame()).getRotation();
-        Rotation targetR = targetPointing.getAttitude(state.getOrbit(), state.getDate(), state.getFrame()).getRotation();
+        Rotation nadirR  = nadirPointing.getAttitude(state.getAbsPVA(), state.getDate(), state.getFrame()).getRotation();
+        Rotation targetR = targetPointing.getAttitude(state.getAbsPVA(), state.getDate(), state.getFrame()).getRotation();
         final double reorientationAngle = Rotation.distance(nadirR, targetR);
         Assertions.assertEquals(0.5 * reorientationAngle, Rotation.distance(state.getAttitude().getRotation(), targetR),
-                0.03 * reorientationAngle);
+                0.08 * reorientationAngle);
 
 
     }
