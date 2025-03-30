0a13a81b5a85982b1ff6a31fa826b8566100014b
Luc Maisonobe <luc@orekit.org>
2025-03-30 15:42:59 +0200

    Fixed checkstyle error.

--- src/main/java/org/orekit/time/DateComponents.java
+++ src/main/java/org/orekit/time/DateComponents.java
@@ -17,7 +17,6 @@
 package org.orekit.time;
 
 import java.io.Serializable;
-import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
