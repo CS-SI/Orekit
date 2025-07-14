23ac71f06839a3568386e5e59a8a11fa49944f05
Luc Maisonobe <luc@orekit.org>
2025-07-14 20:24:23 +0200

    Cleanup.

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -139,14 +139,14 @@ merge_date=$(TZ=UTC date +"%Y-%m-%dT%H:%M:%SZ")
 (cd $top ; glab auth login --hostname gitlab.orekit.org)
 if test -z "$(cd $top ; git branch -a --list origin/${release_branch})" ; then
   # create the release branch on origin, so we can merge into it
-  (cd $top ; glab api --method POST "projects/orekit%2Forekit/repository/branches?branch=${release_branch}&ref=develop")
+  (cd $top ; glab api --silent --method POST "projects/orekit%2Forekit/repository/branches?branch=${release_branch}&ref=develop")
 fi
 (cd $top ; glab mr create --fill --source-branch $rc_branch --target-branch $release_branch --yes)
 mr_id=$(cd $top ; glab mr list --source-branch $rc_branch | sed -n 's,^!\([0-9]*\).*,\1,p')
 (cd $top ; glab mr merge $mr_id --remove-source-branch --yes)
 
 # switch to release branch
-(cd $top ; git checkout $release_branch ; git branch --set-upstream-to origin/$release_branch; git pull)
+(cd $top ; git fetch ; git checkout $release_branch ; git branch --set-upstream-to origin/$release_branch; git pull)
 
 # delete release candidate branch
 request_confirmation "delete $rc_branch release candidate branch? (note that tag $rc_tag will be preserved)"
