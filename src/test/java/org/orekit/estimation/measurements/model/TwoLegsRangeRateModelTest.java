2e46ca44089cc3df4fad6548cef08bbd759e6041
Romain Serra <serra.romain@gmail.com>
2026-02-01 14:38:17 +0100

    fix range rate PV shifter

--- src/test/java/org/orekit/estimation/measurements/model/TwoLegsRangeRateModelTest.java
+++ src/test/java/org/orekit/estimation/measurements/model/TwoLegsRangeRateModelTest.java
@@ -18,8 +18,11 @@ package org.orekit.estimation.measurements.model;
 
 import org.hipparchus.analysis.differentiation.Gradient;
 import org.hipparchus.analysis.differentiation.GradientField;
+import org.hipparchus.geometry.euclidean.threed.FieldVector3D;
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.orekit.estimation.measurements.signal.SignalTravelTimeModel;
 import org.orekit.estimation.measurements.signal.TwoLegsSignalTravelTimer;
 import org.orekit.frames.Frame;
@@ -31,8 +34,63 @@ import org.orekit.utils.FieldAbsolutePVCoordinates;
 import org.orekit.utils.FieldPVCoordinates;
 import org.orekit.utils.PVCoordinates;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class TwoLegsRangeRateModelTest {
+
+    @Test
+    void testValueAgainstOneLeg() {
+        // GIVEN
+        final Frame frame = FramesFactory.getGCRF();
+        final AbsoluteDate receptionDate = AbsoluteDate.ARBITRARY_EPOCH;
+        final SignalTravelTimeModel signalTravelTimeModel = new SignalTravelTimeModel();
+        final TwoLegsSignalTravelTimer twoLegsSignalTravelTimer = new TwoLegsSignalTravelTimer(signalTravelTimeModel);
+        final TwoLegsRangeRateModel rangeRateModel = new TwoLegsRangeRateModel(twoLegsSignalTravelTimer);
+        final PVCoordinates pvRelay = new PVCoordinates(new Vector3D(1e3, 1e4,-1e2), new Vector3D(1, 2, 3),
+                Vector3D.MINUS_I);
+        final PVCoordinates pvObserver = new PVCoordinates(Vector3D.MINUS_J, Vector3D.MINUS_K);
+        final AbsolutePVCoordinates relay = new AbsolutePVCoordinates(frame, receptionDate, pvRelay);
+        final AbsolutePVCoordinates emitter = new AbsolutePVCoordinates(frame, receptionDate, pvObserver);
+        // WHEN
+        final double rangeRate = rangeRateModel.value(frame, pvObserver, receptionDate, relay, emitter);
+        // THEN
+        final OneLegRangeRateModel oneLegRangeRateModel = new OneLegRangeRateModel(signalTravelTimeModel);
+        final double firstLeg = oneLegRangeRateModel.value(frame, pvObserver, receptionDate, relay);
+        final double delay = signalTravelTimeModel.getAdjustableEmitterComputer(relay).computeDelay(pvObserver.getPosition(),
+                receptionDate, frame);
+        final double secondLeg = oneLegRangeRateModel.value(frame, relay.shiftedBy(-delay).getPVCoordinates(),
+                receptionDate.shiftedBy(-delay), emitter);
+        assertEquals(firstLeg + secondLeg, rangeRate);
+    }
+
+    @Test
+    void testFieldValueAgainstOneLeg() {
+        // GIVEN
+        final Frame frame = FramesFactory.getGCRF();
+        final GradientField field = GradientField.getField(1);
+        final FieldAbsoluteDate<Gradient> receptionDate = FieldAbsoluteDate.getArbitraryEpoch(field);
+        final SignalTravelTimeModel signalTravelTimeModel = new SignalTravelTimeModel();
+        final TwoLegsSignalTravelTimer twoLegsSignalTravelTimer = new TwoLegsSignalTravelTimer(signalTravelTimeModel);
+        final TwoLegsRangeRateModel rangeRateModel = new TwoLegsRangeRateModel(twoLegsSignalTravelTimer);
+        final FieldVector3D<Gradient> fieldVelocity = new FieldVector3D<>(field.getOne(), new Gradient(0., 1.), field.getZero());
+        final FieldPVCoordinates<Gradient> pvRelay = new FieldPVCoordinates<>(new FieldVector3D<>(field, new Vector3D(1e3, 1e4,-1e2)),
+                fieldVelocity);
+        final PVCoordinates pvObserver = new PVCoordinates(Vector3D.MINUS_J, Vector3D.MINUS_K);
+        final FieldAbsolutePVCoordinates<Gradient> relay = new FieldAbsolutePVCoordinates<>(frame, receptionDate, pvRelay);
+        final FieldAbsolutePVCoordinates<Gradient> emitter = new FieldAbsolutePVCoordinates<>(frame, receptionDate,
+                new FieldPVCoordinates<>(field, pvObserver));
+        // WHEN
+        final Gradient rangeRate = rangeRateModel.value(frame, emitter.getPVCoordinates(), receptionDate, relay, emitter);
+        // THEN
+        final OneLegRangeRateModel oneLegRangeRateModel = new OneLegRangeRateModel(signalTravelTimeModel);
+        final Gradient firstLeg = oneLegRangeRateModel.value(frame, emitter.getPVCoordinates(), receptionDate, relay);
+        final Gradient delay = signalTravelTimeModel.getFieldAdjustableEmitterComputer(field, relay)
+                .computeDelay(emitter.getPosition(), receptionDate, frame);
+        final Gradient secondLeg = oneLegRangeRateModel.value(frame, relay.shiftedBy(delay.negate()).getPVCoordinates(),
+                receptionDate.shiftedBy(delay.negate()), emitter);
+        assertEquals(firstLeg.add(secondLeg), rangeRate);
+        assertNotEquals(0., rangeRate.getGradient()[0]);
+    }
 
     @Test
     void testValueNoVelocity() {
@@ -51,14 +109,15 @@ class TwoLegsRangeRateModelTest {
         assertEquals(0., rangeRate);
     }
 
-    @Test
-    void testValueRadialVelocity() {
+    @ParameterizedTest
+    @ValueSource(doubles = {-1e3, 1e0, 1e1, 1e2, 1e4, 1e5})
+    void testValueRadialVelocity(final double radialSpeed) {
         // GIVEN
         final Frame frame = FramesFactory.getGCRF();
         final AbsoluteDate date = AbsoluteDate.ARBITRARY_EPOCH;
         final TwoLegsSignalTravelTimer twoLegsSignalTravelTimer = new TwoLegsSignalTravelTimer(new SignalTravelTimeModel());
         final TwoLegsRangeRateModel rangeRateModel = new TwoLegsRangeRateModel(twoLegsSignalTravelTimer);
-        final PVCoordinates pvRelay = new PVCoordinates(new Vector3D(1e5, 0., 0.), Vector3D.PLUS_I);
+        final PVCoordinates pvRelay = new PVCoordinates(new Vector3D(1e5, 0., 0.), Vector3D.PLUS_I.scalarMultiply(radialSpeed));
         final PVCoordinates pvEmitter = new PVCoordinates();
         final AbsolutePVCoordinates relay = new AbsolutePVCoordinates(frame, date, pvRelay);
         final AbsolutePVCoordinates emitter = new AbsolutePVCoordinates(frame, date, pvEmitter);
@@ -105,6 +164,6 @@ class TwoLegsRangeRateModelTest {
                 fieldDate, new FieldAbsolutePVCoordinates<>(field, relay), new FieldAbsolutePVCoordinates<>(field, emitter));
         // THEN
         final double rangeRate = rangeRateModel.value(frame, receiverPV, date, relay, date, emitter, date);
-        assertEquals(rangeRate, fieldRangeRate.getValue());
+        assertEquals(rangeRate, fieldRangeRate.getValue(), 1e-12);
     }
 }
