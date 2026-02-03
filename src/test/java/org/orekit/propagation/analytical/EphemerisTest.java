4119e5c1ffeb31febc6c190c40a99738f6140d8c
Luc Maisonobe <luc@orekit.org>
2026-02-03 15:26:22 +0100

    Removed star imports.

--- src/test/java/org/orekit/propagation/analytical/EphemerisTest.java
+++ src/test/java/org/orekit/propagation/analytical/EphemerisTest.java
@@ -33,19 +33,36 @@ import org.mockito.Mockito;
 import org.orekit.OrekitMatchers;
 import org.orekit.TestUtils;
 import org.orekit.Utils;
-import org.orekit.attitudes.*;
+import org.orekit.attitudes.AttitudeInterpolator;
+import org.orekit.attitudes.AttitudeProvider;
+import org.orekit.attitudes.AttitudesSequence;
+import org.orekit.attitudes.CelestialBodyPointed;
+import org.orekit.attitudes.LofOffset;
 import org.orekit.bodies.CelestialBodyFactory;
 import org.orekit.bodies.GeodeticPoint;
 import org.orekit.bodies.OneAxisEllipsoid;
-import org.orekit.errors.*;
+import org.orekit.errors.OrekitException;
+import org.orekit.errors.OrekitIllegalArgumentException;
+import org.orekit.errors.OrekitIllegalStateException;
+import org.orekit.errors.OrekitMessages;
+import org.orekit.errors.TimeStampedCacheException;
 import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.frames.LOFType;
 import org.orekit.frames.TopocentricFrame;
 import org.orekit.models.earth.EarthStandardAtmosphereRefraction;
 import org.orekit.models.earth.ReferenceEllipsoid;
-import org.orekit.orbits.*;
-import org.orekit.propagation.*;
+import org.orekit.orbits.CartesianOrbit;
+import org.orekit.orbits.KeplerianOrbit;
+import org.orekit.orbits.Orbit;
+import org.orekit.orbits.OrbitHermiteInterpolator;
+import org.orekit.orbits.OrbitType;
+import org.orekit.orbits.PositionAngleType;
+import org.orekit.propagation.AdditionalDataProvider;
+import org.orekit.propagation.OrbitBlender;
+import org.orekit.propagation.Propagator;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.SpacecraftStateInterpolator;
 import org.orekit.propagation.covariance.StateCovariance;
 import org.orekit.propagation.covariance.StateCovarianceBlender;
 import org.orekit.propagation.covariance.StateCovarianceKeplerianHermiteInterpolator;
@@ -55,8 +72,21 @@ import org.orekit.propagation.events.ElevationDetector;
 import org.orekit.propagation.events.handlers.ContinueOnEvent;
 import org.orekit.propagation.events.handlers.RecordAndContinue;
 import org.orekit.propagation.numerical.NumericalPropagator;
-import org.orekit.time.*;
-import org.orekit.utils.*;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.DateComponents;
+import org.orekit.time.TimeComponents;
+import org.orekit.time.TimeInterpolator;
+import org.orekit.time.TimeScalesFactory;
+import org.orekit.time.TimeStampedDoubleHermiteInterpolator;
+import org.orekit.time.TimeStampedPair;
+import org.orekit.utils.AbsolutePVCoordinates;
+import org.orekit.utils.AbsolutePVCoordinatesTest;
+import org.orekit.utils.AngularDerivativesFilter;
+import org.orekit.utils.CartesianDerivativesFilter;
+import org.orekit.utils.Constants;
+import org.orekit.utils.IERSConventions;
+import org.orekit.utils.PVCoordinates;
+import org.orekit.utils.TimeStampedPVCoordinates;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -1149,11 +1179,11 @@ class EphemerisTest {
     public void setUp() throws IllegalArgumentException, OrekitException {
         Utils.setDataRoot("regular-data");
 
-        initDate = new AbsoluteDate(new DateComponents(2004, 01, 01),
+        initDate = new AbsoluteDate(new DateComponents(2004, 1, 1),
                                     TimeComponents.H00,
                                     TimeScalesFactory.getUTC());
 
-        finalDate = new AbsoluteDate(new DateComponents(2004, 01, 02),
+        finalDate = new AbsoluteDate(new DateComponents(2004, 1, 2),
                                      TimeComponents.H00,
                                      TimeScalesFactory.getUTC());
 
