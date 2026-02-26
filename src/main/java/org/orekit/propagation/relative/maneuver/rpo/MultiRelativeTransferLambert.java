a4808dd5f2e1251843b045468ec4fa16b67c211f
rcuvillon <romain.cuvillon@cs-soprasteria.com>
2026-02-26 18:16:51 +0100

    fix: - Adapt RPO waypoints calculator and associated tests - Displace TeatDrop class - add convertToImpulseManeuver (to be tested)

--- src/main/java/org/orekit/propagation/relative/maneuver/rpo/MultiRelativeTransferLambert.java
+++
@@ -1,4 +0,0 @@
-package org.orekit.propagation.relative.maneuver.rpo;
-
-public class MultiRelativeTransferLambert extends AbstractMultiRelativeTransfers {
-}
--- src/main/java/org/orekit/propagation/relative/maneuver/rpo/MultiRelativeTransfersYA.java
+++
@@ -1,4 +0,0 @@
-package org.orekit.propagation.relative.maneuver.rpo;
-
-public class MultiRelativeTransfersYA extends AbstractMultiRelativeTransfers {
-}
