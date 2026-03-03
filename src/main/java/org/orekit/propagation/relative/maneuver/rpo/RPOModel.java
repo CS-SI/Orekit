13ef95e581c081d13c270644d22ebbb3806720b2
rcuvillon <romain.cuvillon@cs-soprasteria.com>
2026-03-03 10:59:53 +0100

    fix: add field methods.

--- src/main/java/org/orekit/propagation/relative/maneuver/rpo/RPOModel.java
+++ src/main/java/org/orekit/propagation/relative/maneuver/rpo/RPOModel.java
@@ -17,31 +17,48 @@
 package org.orekit.propagation.relative.maneuver.rpo;
 
 import org.hipparchus.CalculusFieldElement;
+import org.hipparchus.geometry.euclidean.threed.FieldVector3D;
 import org.hipparchus.geometry.euclidean.threed.Vector3D;
 import org.hipparchus.linear.MatrixUtils;
 import org.orekit.frames.LOFType;
 import org.orekit.frames.LocalOrbitalFrame;
+import org.orekit.orbits.FieldKeplerianOrbit;
 import org.orekit.orbits.FieldOrbit;
 import org.orekit.orbits.KeplerianOrbit;
 import org.orekit.orbits.Orbit;
 import org.orekit.orbits.OrbitType;
 import org.orekit.orbits.PositionAngleType;
+import org.orekit.propagation.FieldSpacecraftState;
 import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.analytical.FieldKeplerianPropagator;
 import org.orekit.propagation.analytical.KeplerianPropagator;
 import org.orekit.propagation.events.DateDetector;
 import org.orekit.propagation.events.EventDetector;
+import org.orekit.propagation.events.FieldDateDetector;
+import org.orekit.propagation.events.FieldEventDetector;
+import org.orekit.propagation.relative.FieldRelativeProvider;
 import org.orekit.propagation.relative.RelativeProvider;
 import org.orekit.propagation.relative.clohessywiltshire.ClohessyWiltshireEquations;
 import org.orekit.propagation.relative.clohessywiltshire.ClohessyWiltshireMatrices;
 import org.orekit.propagation.relative.clohessywiltshire.ClohessyWiltshireProvider;
 import org.orekit.propagation.relative.clohessywiltshire.ClohessyWiltshireRendezVous;
+import org.orekit.propagation.relative.clohessywiltshire.FieldClohessyWiltshireEquations;
+import org.orekit.propagation.relative.clohessywiltshire.FieldClohessyWiltshireMatrices;
+import org.orekit.propagation.relative.clohessywiltshire.FieldClohessyWiltshireProvider;
+import org.orekit.propagation.relative.clohessywiltshire.FieldClohessyWiltshireRendezVous;
 import org.orekit.propagation.relative.maneuver.ClohessyWiltshireManeuver;
+import org.orekit.propagation.relative.maneuver.FieldClohessyWiltshireManeuver;
+import org.orekit.propagation.relative.maneuver.FieldRelativeManeuver;
+import org.orekit.propagation.relative.maneuver.FieldYamanakaAnkersenManeuver;
 import org.orekit.propagation.relative.maneuver.RelativeManeuver;
 import org.orekit.propagation.relative.maneuver.YamanakaAnkersenManeuver;
+import org.orekit.propagation.relative.yamanakaankersen.FieldYamanakaAnkersenProvider;
+import org.orekit.propagation.relative.yamanakaankersen.FieldYamanakaAnkersenRendezVous;
 import org.orekit.propagation.relative.yamanakaankersen.YamanakaAnkersenProvider;
 import org.orekit.propagation.relative.yamanakaankersen.YamanakaAnkersenRendezVous;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.FieldAbsoluteDate;
+import org.orekit.utils.FieldPVCoordinates;
 import org.orekit.utils.TimeStampedFieldPVCoordinates;
 import org.orekit.utils.TimeStampedPVCoordinates;
 
@@ -148,6 +165,44 @@ public enum RPOModel implements RPO {
             return new ArrayList<>(maneuvers);
         }
 
+        /**
+         * Compute relative maneuvers to realize a forced trajectory defined by the waypoints (ForcedLinear/ForcedCircular)
+         * using Clohessy-Wiltshire model.
+         * @param waypoints Waypoints of the trajectory in QSW frame.
+         * @param initialVelocity Initial velocity in the Clohessy-Wiltshire (QSW) frame.
+         * @param targetOrbit Orbit of the target.
+         * @param cwProvider Clohessy-Wiltshire provider.
+         * @param <T> field.
+         * @return list of relative maneuvers in Clohessy-Wiltshire frame (QSW).
+         */
+        public <T extends CalculusFieldElement<T>> List<FieldRelativeManeuver<T>> computeForcedManeuvers(
+                final List<TimeStampedFieldPVCoordinates<T>> waypoints, final FieldVector3D<T> initialVelocity,
+                final FieldOrbit<T> targetOrbit, final FieldRelativeProvider<T> cwProvider) {
+            final List<FieldClohessyWiltshireManeuver<T>> maneuvers = new ArrayList<>();
+            FieldVector3D<T> velocityBeforeManeuver = initialVelocity;
+            final LocalOrbitalFrame targetLof = new LocalOrbitalFrame(targetOrbit.getFrame(), LOFType.QSW, targetOrbit.toOrbit(), LOFType.QSW.getName());
+            for (int i = 0; i < waypoints.size() - 1; i++) {
+                // Define Current waypoint and next waypoint in QSW
+                final TimeStampedFieldPVCoordinates<T> currentWaypoint = waypoints.get(i);
+                final TimeStampedFieldPVCoordinates<T> nextWaypoint = waypoints.get(i + 1);
+                // Define Date Detector to trigger the maneuver
+                final FieldEventDetector<T> firstImpulseTrigger = new FieldDateDetector<>(currentWaypoint.getDate());
+                // Compute the velocity after the maneuver to arrive at the following waypoint at the correct date using RendezVous.
+                final FieldVector3D<T> velocityAfterManeuver = (new FieldClohessyWiltshireRendezVous<T>()).computeRendezVous(currentWaypoint, nextWaypoint, targetLof, targetOrbit).getPvt1().getVelocity();
+                // Compute the impulsion at the current waypoint to reach the next waypoint.
+                final FieldVector3D<T> deltaV = velocityAfterManeuver.subtract(velocityBeforeManeuver);
+                // Create the Clohessy-Wiltshire maneuver and add it to the list.
+                final FieldClohessyWiltshireManeuver<T> maneuver = new FieldClohessyWiltshireManeuver<>(firstImpulseTrigger, deltaV, (FieldClohessyWiltshireProvider<T>) cwProvider);
+                maneuvers.add(maneuver);
+                // Compute the velocity before the maneuver at the next waypoint.
+                final T transferDuration = nextWaypoint.getDate().durationFrom(currentWaypoint.getDate());
+                final FieldClohessyWiltshireMatrices<T> matrices = (new FieldClohessyWiltshireEquations<T>()).computeMatrices(transferDuration, targetOrbit.getKeplerianMeanMotion());
+                velocityBeforeManeuver = new FieldVector3D<>(matrices.getPhiVR().operate(MatrixUtils.createFieldVector(currentWaypoint.getPosition().toArray()))
+                        .add(matrices.getPhiVV().operate(MatrixUtils.createFieldVector(velocityAfterManeuver.toArray()))).toArray());
+            }
+            return new ArrayList<>(maneuvers);
+        }
+
 //        // TODO: Tester la fonction --> Pas sûr de la transformation du vecteur deltaV.
 //        /**
 //         * Convert the relative maneuvers into Impulse maneuvers in the targetOrbit frame.
@@ -191,6 +246,33 @@ public enum RPOModel implements RPO {
                         new Vector3D(-maneuverVelocity.getX(), maneuverVelocity.getY(), maneuverVelocity.getZ()));
                 final Vector3D deltaV =  maneuverVelocity.subtract(pvtBeforeMan.getVelocity());
                 final ClohessyWiltshireManeuver maneuver = new ClohessyWiltshireManeuver(maneuverDate, deltaV, (ClohessyWiltshireProvider) cwProvider);
+                maneuvers.add(maneuver);
+            }
+            return new ArrayList<>(maneuvers);
+        }
+
+        /**
+         * Computes the Clohessy-Wilstshire based maneuvers of the teardrop relative orbit in QSW Local Orbital Frame.
+         * <p>The injection point is the turn-around point of the teardrop (the round end).</p>
+         * <p>All maneuvers happen at the pointy end of the teardrop.</p>
+         * @param waypoints List of the successive waypoints of the target.
+         * @param cwProvider Clohessy-Wiltshire provider.
+         * @param <T> field.
+         * @return list of Clohessy-Wiltshire maneuvers.
+         */
+        public <T extends CalculusFieldElement<T>> List<FieldRelativeManeuver<T>> computeTeardropManeuvers(
+                final List<TimeStampedFieldPVCoordinates<T>> waypoints, final FieldRelativeProvider<T> cwProvider) {
+            final List<FieldClohessyWiltshireManeuver<T>> maneuvers = new ArrayList<>();
+            // Creation of the maneuvers at the maneuver point of the teardrop.
+            for (int i = 1; i < waypoints.size(); i++) {
+                final TimeStampedFieldPVCoordinates<T> maneuverWaypoint = waypoints.get(i);
+                final FieldVector3D<T> maneuverVelocity = maneuverWaypoint.getVelocity();
+                final FieldEventDetector<T> maneuverDate = new FieldDateDetector<>(maneuverWaypoint.getDate());
+                final TimeStampedFieldPVCoordinates<T> pvtBeforeMan = new TimeStampedFieldPVCoordinates<>(maneuverWaypoint.getDate(),
+                        new FieldPVCoordinates<>(maneuverWaypoint.getPosition(),
+                        new FieldVector3D<>(maneuverVelocity.getX().negate(), maneuverVelocity.getY(), maneuverVelocity.getZ())));
+                final FieldVector3D<T> deltaV =  maneuverVelocity.subtract(pvtBeforeMan.getVelocity());
+                final FieldClohessyWiltshireManeuver<T> maneuver = new FieldClohessyWiltshireManeuver<>(maneuverDate, deltaV, (FieldClohessyWiltshireProvider<T>) cwProvider);
                 maneuvers.add(maneuver);
             }
             return new ArrayList<>(maneuvers);
@@ -309,7 +391,7 @@ public enum RPOModel implements RPO {
                 final YamanakaAnkersenManeuver maneuver = new YamanakaAnkersenManeuver(firstImpulseTrigger, deltaV, (YamanakaAnkersenProvider) yaProvider);
                 maneuvers.add(maneuver);
                 // Create a Yamanaka-Ankersen provider to propagate to the next waypoint.
-                final YamanakaAnkersenProvider yaProviderManeuver = new YamanakaAnkersenProvider(orbit, new TimeStampedPVCoordinates(currentWaypoint.getDate(), currentWaypoint.getPosition(), velocityAfterManeuver), "Provider_" + i);
+                final YamanakaAnkersenProvider yaProviderManeuver = new YamanakaAnkersenProvider(orbit, new TimeStampedPVCoordinates(currentWaypoint.getDate(), currentWaypoint.getPosition(), velocityAfterManeuver), "FieldProvider_" + i);
                 targetPropagator.addAdditionalDataProvider(yaProviderManeuver);
                 // Propagate the waypoint.
                 final SpacecraftState propagated = targetPropagator.propagate(nextWaypoint.getDate());
@@ -317,6 +399,52 @@ public enum RPOModel implements RPO {
                 final double trueAnomaly = ((KeplerianOrbit) OrbitType.KEPLERIAN.convertType(propagated.getOrbit())).getTrueAnomaly();
                 orbit = new KeplerianOrbit(orbit.getA(), orbit.getE(), orbit.getI(), orbit.getPerigeeArgument(), orbit.getRightAscensionOfAscendingNode(), trueAnomaly, PositionAngleType.TRUE, orbit.getFrame(), nextWaypoint.getDate(), orbit.getMu());
                 velocityBeforeManeuver = new Vector3D(Arrays.copyOfRange(yaProviderManeuver.getAdditionalData(propagated), 3, 6));
+            }
+            return new ArrayList<>(maneuvers);
+        }
+
+        /**
+         * Compute relative maneuvers to realize a forced trajectory defined by the waypoints (ForcedLinear/ForcedCircular)
+         * using Yamanaka-Ankersen model.
+         * @param waypoints Waypoints of the trajectory in LVLH_CCSDS frame.
+         * @param initialVelocity Initial velocity in the Yamanaka-Ankersen (LVLH_CCSDS) frame.
+         * @param targetOrbit Orbit of the target.
+         * @param yaProvider Yamanaka-Ankersen provider.
+         * @param <T> field.
+         * @return list of relative maneuvers in Yamanaka-Ankersen model's frame (LVLH_CCSDS).
+         */
+        public <T extends CalculusFieldElement<T>> List<FieldRelativeManeuver<T>> computeForcedManeuvers(
+                final List<TimeStampedFieldPVCoordinates<T>> waypoints, final FieldVector3D<T> initialVelocity,
+                final FieldOrbit<T> targetOrbit, final FieldRelativeProvider<T> yaProvider) {
+            final List<FieldYamanakaAnkersenManeuver<T>> maneuvers = new ArrayList<>();
+            FieldVector3D<T> velocityBeforeManeuver = initialVelocity;
+            FieldKeplerianOrbit<T> orbit = (FieldKeplerianOrbit<T>) OrbitType.KEPLERIAN.convertType(targetOrbit);
+            yaProvider.setTargetOrbit(orbit);
+            final FieldKeplerianPropagator<T> targetPropagator = new FieldKeplerianPropagator<>(targetOrbit);
+            for (int i = 0; i < waypoints.size() - 1; i++) {
+                final LocalOrbitalFrame lofUpdated = new LocalOrbitalFrame(targetOrbit.getFrame(), LOFType.LVLH_CCSDS, orbit.toOrbit(), LOFType.LVLH_CCSDS.getName());
+                // Define Current waypoint and next waypoint in QSW
+                final TimeStampedFieldPVCoordinates<T> currentWaypoint = waypoints.get(i);
+                final TimeStampedFieldPVCoordinates<T> nextWaypoint = waypoints.get(i + 1);
+                // Define Date Detector to trigger the maneuver
+                final FieldEventDetector<T> firstImpulseTrigger = new FieldDateDetector<>(currentWaypoint.getDate());
+                // Compute the velocity after the maneuver to arrive at the following waypoint at the correct date using RendezVous.
+                final FieldVector3D<T> velocityAfterManeuver = (new FieldYamanakaAnkersenRendezVous<T>()).computeRendezVous(currentWaypoint, nextWaypoint, lofUpdated, orbit, targetPropagator).
+                        getPvt1().getVelocity();
+                // Compute the impulsion at the current waypoint to reach the next waypoint.
+                final FieldVector3D<T> deltaV = velocityAfterManeuver.subtract(velocityBeforeManeuver);
+                // Create the Yamanaka-Ankersen maneuver and add it to the list.
+                final FieldYamanakaAnkersenManeuver<T> maneuver = new FieldYamanakaAnkersenManeuver<>(firstImpulseTrigger, deltaV, (FieldYamanakaAnkersenProvider<T>) yaProvider);
+                maneuvers.add(maneuver);
+                // Create a Yamanaka-Ankersen provider to propagate to the next waypoint.
+                final FieldYamanakaAnkersenProvider<T> yaProviderManeuver = new FieldYamanakaAnkersenProvider<>(orbit, new TimeStampedFieldPVCoordinates<>(currentWaypoint.getDate(), new FieldPVCoordinates<>(currentWaypoint.getPosition(), velocityAfterManeuver)), "Provider_" + i);
+                targetPropagator.addAdditionalDataProvider(yaProviderManeuver);
+                // Propagate the waypoint.
+                final FieldSpacecraftState<T> propagated = targetPropagator.propagate(nextWaypoint.getDate());
+                // Update the target orbit and the velocity before the next maneuver.
+                final T trueAnomaly = ((FieldKeplerianOrbit<T>) OrbitType.KEPLERIAN.convertType(propagated.getOrbit())).getTrueAnomaly();
+                orbit = new FieldKeplerianOrbit<>(orbit.getA(), orbit.getE(), orbit.getI(), orbit.getPerigeeArgument(), orbit.getRightAscensionOfAscendingNode(), trueAnomaly, PositionAngleType.TRUE, orbit.getFrame(), nextWaypoint.getDate(), orbit.getMu());
+                velocityBeforeManeuver = new FieldVector3D<>(Arrays.copyOfRange(yaProviderManeuver.getAdditionalData(propagated), 3, 6));
             }
             return new ArrayList<>(maneuvers);
         }
@@ -364,6 +492,33 @@ public enum RPOModel implements RPO {
                         new Vector3D(maneuverVelocity.getX(), -maneuverVelocity.getY(), -maneuverVelocity.getZ()));
                 final Vector3D deltaV =  maneuverVelocity.subtract(pvtBeforeMan.getVelocity());
                 final YamanakaAnkersenManeuver maneuver = new YamanakaAnkersenManeuver(maneuverDate, deltaV, (YamanakaAnkersenProvider) yaProvider);
+                maneuvers.add(maneuver);
+            }
+            return new ArrayList<>(maneuvers);
+        }
+
+        /**
+         * Computes the Yamanaka-Ankersen based maneuvers of the teardrop relative orbit in LVLH_CCSDS Local Orbital Frame.
+         * <p>The injection point is the turn-around point of the teardrop (the round end).</p>
+         * <p>All maneuvers happen at the pointy end of the teardrop.</p>
+         * @param waypoints List of the successive waypoints of the target.
+         * @param yaProvider Clohessy-Wiltshire provider.
+         * @param <T> field.
+         * @return list of Clohessy-Wiltshire maneuvers.
+         */
+        public <T extends CalculusFieldElement<T>> List<FieldRelativeManeuver<T>> computeTeardropManeuvers(
+                final List<TimeStampedFieldPVCoordinates<T>> waypoints, final FieldRelativeProvider<T> yaProvider) {
+            final List<FieldYamanakaAnkersenManeuver<T>> maneuvers = new ArrayList<>();
+            // Creation of the maneuvers at the maneuver point of the teardrop.
+            for (int i = 1; i < waypoints.size(); i++) {
+                final TimeStampedFieldPVCoordinates<T> maneuverWaypoint = waypoints.get(i);
+                final FieldVector3D<T> maneuverVelocity = maneuverWaypoint.getVelocity();
+                final FieldEventDetector<T> maneuverDate = new FieldDateDetector<>(maneuverWaypoint.getDate());
+                final TimeStampedFieldPVCoordinates<T> pvtBeforeMan = new TimeStampedFieldPVCoordinates<>(maneuverWaypoint.getDate(),
+                        new FieldPVCoordinates<>(maneuverWaypoint.getPosition(),
+                        new FieldVector3D<>(maneuverVelocity.getX(), maneuverVelocity.getY().negate(), maneuverVelocity.getZ().negate())));
+                final FieldVector3D<T> deltaV =  maneuverVelocity.subtract(pvtBeforeMan.getVelocity());
+                final FieldYamanakaAnkersenManeuver<T> maneuver = new FieldYamanakaAnkersenManeuver<>(maneuverDate, deltaV, (FieldYamanakaAnkersenProvider<T>) yaProvider);
                 maneuvers.add(maneuver);
             }
             return new ArrayList<>(maneuvers);
