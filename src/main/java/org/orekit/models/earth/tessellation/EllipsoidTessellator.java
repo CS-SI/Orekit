f6815f766602ade3b50bc4d624f395eeb7bff978
Serrof <serra.romain@gmail.com>
2026-05-09 23:07:45 +0200

    use of record type instead of some private static classes

--- src/main/java/org/orekit/models/earth/tessellation/EllipsoidTessellator.java
+++ src/main/java/org/orekit/models/earth/tessellation/EllipsoidTessellator.java
@@ -847,44 +847,22 @@ public class EllipsoidTessellator {
 
     }
 
-    /** Local class for a range of indices to be used for building a tile. */
-    private static class Range {
-
-        /** Lower index. */
-        private final int lower;
-
-        /** Upper index. */
-        private final int upper;
-
-        /** Simple constructor.
-         * @param lower lower index
-         * @param upper upper index
-         */
-        Range(final int lower, final int upper) {
-            this.lower = lower;
-            this.upper = upper;
-        }
-
+    /**
+     * Local class for a range of indices to be used for building a tile.
+     *
+     * @param lower Lower index.
+     * @param upper Upper index.
+     */
+    private record Range(int lower, int upper) {
     }
 
-    /** Local class for a pair of ranges of indices to be used for building a tile. */
-    private static class RangePair {
-
-        /** Across range. */
-        private final Range across;
-
-        /** Along range. */
-        private final Range along;
-
-        /** Simple constructor.
-         * @param across across range
-         * @param along along range
-         */
-        RangePair(final Range across, final Range along) {
-            this.across = across;
-            this.along  = along;
-        }
-
+    /**
+     * Local class for a pair of ranges of indices to be used for building a tile.
+     *
+     * @param across Across range.
+     * @param along  Along range.
+     */
+    private record RangePair(Range across, Range along) {
     }
 
 }
