b9fa0872ad24c095751de3b70b08675fd6a37860
Luc Maisonobe <luc@orekit.org>
2026-06-13 22:10:00 +0200

    Attempt to fix state transition matrix parameter types.
    (cherry picked from commit 2838d72b3721d8dc88954ff08dcad7f3906ee226)
    
    # Conflicts:
    #	src/main/java/org/orekit/propagation/analytical/gnss/FieldGnssPropagator.java
    #	src/main/java/org/orekit/propagation/analytical/gnss/GNSSPropagator.java
    #	src/test/java/org/orekit/propagation/analytical/gnss/GPSPropagatorTest.java

--- src/test/java/org/orekit/propagation/analytical/gnss/GPSPropagatorTest.java
+++ src/test/java/org/orekit/propagation/analytical/gnss/GPSPropagatorTest.java
@@ -94,10 +94,9 @@ class GPSPropagatorTest {
 
     @Test
     void testClockCorrections() {
-        final GNSSPropagator<GPSAlmanac>
-            propagator =
-            new GNSSPropagator<>(almanacs.getFirst().factory(context.getFrames().getEME2000(), context.getFrames()
-                .getITRF(IERSConventions.IERS_2010, false)));
+        final GNSSPropagator<GPSAlmanac> propagator =
+            new GNSSPropagator<>(almanacs.getFirst().factory(context.getFrames().getEME2000(),
+                                                             context.getFrames().getITRF(IERSConventions.IERS_2010, false)));
         propagator.addAdditionalDataProvider(
             new ClockCorrectionsProvider(almanacs.getFirst(), almanacs.getFirst().getCycleDuration()));
         // Propagate at the GPS date and one GPS cycle later
@@ -540,8 +539,7 @@ class GPSPropagatorTest {
             propagator =
             new GNSSPropagator<>(factory.createFromDrivers(), eci, ecef, attitudeProvider, mass);
 
-        final GNSSPropagator<GPSLegacyNavigationMessage>
-            rebuilt =
+        final GNSSPropagator<GPSLegacyNavigationMessage> rebuilt =
             new GNSSPropagator<>(propagator.getInitialState(), factory.createFromDrivers(), ecef, attitudeProvider,
                                  mass);
         final GNSSOrbitalElements<?> oe2 = rebuilt.getOrbitalElements();
